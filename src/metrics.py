596a379527309ad609c842664ee5bdcf0eed95a1
lee212 <hroe.lee@gmail.com>
2013-02-13 17:13:29 -0500

    init metrics

---
+++ src/metrics.py
@@ -0,0 +1,19 @@
+"""This module is an application development using FutureGrid Cloud Metrics framework.
+    
+Our first approach is emailing cloud users who have long-running instances on FutureGrid.
+For example,
+$ cm vm:5
+sends emails to whom running vm instances more than 5 days.
+
+"""
+
+class FGMetric:
+    def list_users(self):
+        # current active users
+        return
+    def alert_users(self):
+        # 
+        return
+    def email_users(self):
+        return
+
