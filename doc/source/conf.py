207916dc0bd00f4e2eb3f7f69007bf88768d9bc8
Gregor von Laszewski <laszewski@gmail.com>
2013-03-02 23:01:37 -0500

    layout improfement

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -128,7 +128,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -154,10 +154,10 @@ html_static_path = ['_static']
 #html_show_sourcelink = True
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
--- doc/source/index.rst
+++ doc/source/index.rst
@@ -1,12 +1,8 @@
-.. cloudmesh documentation master file, created by
-   sphinx-quickstart on Sat Feb  9 19:32:34 2013.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to cloudmesh's documentation!
-=====================================
+Documentation for  cloudmesh
+============================
 
 Contents:
+   intro
 
 .. toctree::
    :maxdepth: 2
