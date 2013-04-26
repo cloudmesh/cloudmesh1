dfe8360a54b11451ee260464328ab70be9e8a631
Gregor von Laszewski <laszewski@gmail.com>
2013-04-26 15:34:19 -0400

    completed the cm_keys class

--- cloudmesh/cm_keys.py
+++ cloudmesh/cm_keys.py
@@ -1,4 +1,34 @@
 from cm_config import cm_config
+import os
+from string import Template
+import base64
+import hashlib
+
+def key_fingerprint(key_string):
+    key = base64.decodestring(key_string)
+    fp_plain = hashlib.md5(key).hexdigest()
+    return ':'.join(a+b for a,b in zip(fp_plain[::2], fp_plain[1::2]))
+
+def key_validate(type,filename):
+    if type.lower() == "file":
+        try :
+            keystring = open(filename, "r").read()
+        except :
+            return False
+    else :
+        keystring = file
+    
+    try :
+        type, key_string, comment = keystring.split()
+        data = base64.decodestring(key_string)
+        int_len = 4
+        str_len = struct.unpack('>I', data[:int_len])[0] # this should return 7
+
+        if data[int_len:int_len+str_len] == type:
+            return True
+    except Exception, e:
+        print e
+        return False
 
 class cm_keys:
 
@@ -10,7 +40,55 @@ class cm_keys:
         else:
             self.config = cm_config(self.filename)
 
-    def set(self, name):
+    def type(self, name):
+        try:
+            value = self._getvalue(name)
+            if value.startswith("ssh"):
+                return "string"
+            else:
+                return "file"
+        except:
+            return "keys"
+        
+    def _getvalue (self,name):
+        if name == 'keys':
+            return self.config.data["cloudmesh"]["keys"]
+        elif name == 'default':
+            key = self.config.data["cloudmesh"]["keys"]["default"]
+        else:
+            key = name
+        return self.config.data["cloudmesh"]["keys"]["keylist"][key]
+
+    def __getitem__(self,name):
+
+        key_type = self.type(name)
+        print "TYPE", key_type
+        if key_type == "file":
+            filename = self.config.data["cloudmesh"]["keys"]["keylist"][name]
+            print "FILENAME", filename
+            value = self._get_key_from_file(filename)
+        else:
+            value = self._getvalue(name)
+        print "VALUE", value
+        return value
+            
+    def __setitem__(self, name, value):
+        if name == 'default':
+            key = self.config.data["cloudmesh"]["keys"]["default"]
+        else:
+            key = name
+        self.config.data["cloudmesh"]["keys"]["keylist"][key] = value
+
+    def _path_expand(self,text):
+        """ returns a string with expanded variavble """
+        template = Template(text)
+        result = template.substitute(os.environ)
+        return result
+
+    def _get_key_from_file(self, filename):
+        return open(self._path_expand(os.path.expanduser(filename)), "r").read()
+                
+    def setdefault(self, name):
         """sets the default key"""
         self.config.data["cloudmesh"]["keys"]["default"] = name
 
@@ -37,3 +115,10 @@ class cm_keys:
 
     def update(self):
         """writes the updated dict to the config"""
+    
+    def fingerprint(self, name):
+        value = self.__getitem__(name)
+        print "XXXXXX", value
+        t, keystring, comment = value.split()
+        print keystring
+        return key_fingerprint(keystring)
---
+++ cloudmesh/test_cm_keys.py
@@ -0,0 +1,97 @@
+""" run with
+
+nosetests -v --nocapture
+
+or
+
+nosetests -v
+
+"""
+import sys
+#sys.path.insert(0, '..')
+
+from cm_keys import cm_keys
+import json
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+header = True
+
+def HEADING(txt):
+    if header:
+        print
+        print "#", 70 * '#'
+        print "#", txt
+        print "#", 70 * '#'
+
+class Test_cloudmesh:
+
+
+    filename = "credentials-example.yaml"
+    #filename = None
+
+    def setup(self):
+        self.keys = cm_keys()
+
+    def tearDown(self):
+        pass
+
+    def test01_print(self):
+        #print self.keys
+        pass
+    
+    def test01_names(self):
+        HEADING("NAMES")
+        print self.keys.names()
+        
+    def test02_default(self):
+        HEADING("DEFAULT")
+        print self.keys.default()
+
+    def test03_getvalue(self):
+        HEADING("GET VALUE")
+        print self.keys._getvalue("gregor")
+        print self.keys._getvalue("default")
+        print self.keys._getvalue("keys")
+
+    def test03_set(self):
+        HEADING("SET DEFAULT")
+        self.keys.setdefault("gregor")
+        print self.keys.default()
+
+    def test04_get(self):
+        HEADING("GET GREGOR")
+        print self.keys["gregor"]
+
+    def test05_get(self):
+        HEADING("GET")
+        print self.keys["default"]
+        print self.keys["gregor"]
+        
+    def test06_set(self):
+        HEADING("SET HELLO WORLD")
+        print self.keys["keys"]
+        
+        self.keys["default"] = "hello"
+        self.keys["gregor"] = "world"
+
+        print self.keys["keys"]
+        
+        assert (self.keys._getvalue("default") == "hello") and (self.keys._getvalue("gregor") == "world")
+
+    def test07_type(self):
+        HEADING("TYPE")
+        print "LLL", self.keys.type("gregor") 
+        for name in self.keys.names():
+            print name
+            value =  self.keys[name]
+            print self.keys.type(name), name, value
+            
+        assert (self.keys.type("gregor") == "file")
+
+    def test08_fingerprint(self):
+        HEADING("FINGERPRINT")
+        print "LLL", self.keys.fingerprint("gregor") 
+
+
+    
