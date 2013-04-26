eb144d008cedba79a4d171e9d8fab4d4ffa4f759
Gregor von Laszewski <laszewski@gmail.com>
2013-04-26 12:46:14 -0400

    improving cm_keys

--- cloudmesh/cm_keys.py
+++ cloudmesh/cm_keys.py
@@ -1,22 +1,39 @@
+from cm_config import cm_config
+
 class cm_keys:
 
-    def __init__(self):
+    def __init__(self, filename=None):
         """initializes based on cm_config and returns pointer to the keys dict."""
-        
-    def default(self, name)
+        self.filename = filename
+        if self.filename == None:
+            self.config = cm_config()
+        else:
+            self.config = cm_config(self.filename)
+
+    def set(self, name):
         """sets the default key"""
+        self.config.data["cloudmesh"]["keys"]["default"] = name
+
+    def default(self):
+        """sets the default key"""
+        return self.config.userkeys('default')
         
-    def add(self, line, name=None)
+    def addkey(self, line, name=None):
+        """adds a key with a given name. If no name can be derived from line a unique name is chosen automatically"""
+
+    def addkeyfile(self, filename):
         """adds a key with a given name. If no name can be derived from line a unique name is chosen automatically"""
 
     def names(self):
         """returns all key names in an array"""
+        return self.config.data["cloudmesh"]["keys"]["keylist"].keys()
 
     def validate(self, line):
         """validates if a default key os ok and follows 'keyencryptiontype keystring keyname'"""
 
     def __str__(self):
         """returns the dict in a string representing the project"""
+        return str(self.config)
 
     def update(self):
         """writes the updated dict to the config"""
