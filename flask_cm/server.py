3c92d304113991ae7ae76624b474a088d6e3e7e0
Gregor von Laszewski <laszewski@gmail.com>
2013-04-29 13:41:11 -0400

    added warning for another bug

--- flask_cm/server.py
+++ flask_cm/server.py
@@ -373,6 +373,7 @@ def vm_login(cloud=None,server=None):
     else :
         message = 'Logged in Successfully'
         ip = server['addresses'][server['addresses'].keys()[0]][1]['addr'] 
+        # THIS IS A BUG AND MUST BE SET PER VM, E.G. sometimesvm type probably decides that?
         print "ssh",'ubuntu@'+ip
         xterm('-e','ssh','ubuntu@'+ip)
         
