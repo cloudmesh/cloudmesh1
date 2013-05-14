2fa700f808fe27fffeb64f31d8f3d455f2781233
Gregor von Laszewski <laszewski@gmail.com>
2013-05-14 03:14:30 -0400

    added second version number, there should only be one

--- cloudmesh/cm_rc.py
+++ cloudmesh/cm_rc.py
@@ -75,7 +75,7 @@ def DEBUG(label, var):
 
 def main():
     default_path = '.futuregrid/novarc'
-    arguments = docopt(__doc__, version='1.0.1')
+    arguments = docopt(__doc__, version='0.8')
 
     DEBUG("arguments", arguments)
     
