bf35a1f9bc22a64516489a73f727db817e4ee64b
Gregor von Laszewski <laszewski@gmail.com>
2013-05-14 10:48:07 -0400

    added getter and setter mockup for cm_project

--- cloudmesh/cm_projects.py
+++ cloudmesh/cm_projects.py
@@ -1,22 +1,35 @@
 class cm_projects:
 
-    def __init__(self):
+    def __init__(self, filename=None):
         """initializes based on cm_config and returns pointer to the keys dict."""
+        # Check if the file exists
+        if filename == None:
+            self.config = cm_config()
+        else:
+            self.filename = self._path_expand(filename)
+            try:
+                with open(self.filename): pass
+            except IOError:
+                print 'ERROR: cm_keys, file "%s" does not exist' % self.filename
+                sys.exit()
+            self.config = cm_config(self.filename)
+
+    @property
+    def default(self):
+        """returns the default project"""
         
-    def default(self, name)
+    @default.setter
+    def default(self, name):
         """sets the default project"""
-        
+
     def add(self, name, status="active")
         """adds a project with given status"""
 
     def names(self, status="active"):
-        """returns all projects in an array whi it is in a specified status"""
-
-    def validate(self, line):
-        """validates if a default project is set"""
+        """returns all projects in an array with a specified status"""
 
     def __str__(self):
         """returns the dict in a string representing the project"""
 
-    def update(self):
-        """writes the updated dict to the config"""
+    def save(self):
+        """writes the updated dict to the config yaml file"""
