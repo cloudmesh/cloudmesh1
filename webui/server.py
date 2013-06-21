f55b03dae328ca0bed66fa13bbc1bdc6cee7e653
Gregor von Laszewski <laszewski@gmail.com>
2013-06-21 06:11:34 -0400

    mergerd the inventroy and flask server

--- webui/server.py
+++ webui/server.py
@@ -27,6 +27,13 @@ import base64,struct,hashlib
 from datetime import datetime
 import yaml
 
+try:
+    from sh import xterm
+except:
+    print "xterm not suppported"
+    #TODO: THERE SHOULD BE A VARIABLE SET HERE SO THAT THE ARROW START UP BUTTON 
+    #      CAN RETURN MEANINGFULL MESSAGE IF NOT SUPPORTED
+
 def table_printer(the_dict, header_info=None):
     # header_info ["attribute", "value"]
     if header_info != None or (header_info == "") :
@@ -136,6 +143,48 @@ redhat = FabricImage(
 print inventory.pprint()
 
 ######################################################################
+# CLOUDMESH
+######################################################################
+
+
+
+clouds = cloudmesh()
+# refresh, misses the search for display
+                
+clouds.refresh()
+clouds.refresh_user_id()
+config = cm_config()
+configuration = config.get()
+prefix = config.prefix
+index = config.index
+
+#clouds.load()
+#clouds.refresh("openstack")
+#clouds.clouds
+
+# DEFINING A STATE FOR THE CHECKMARKS IN THE TABLE
+
+"""
+for name in clouds.active():
+
+        config.data['cloudmesh']['clouds']
+
+for name in clouds.active():
+    try:
+        a = config.data['cloudmesh']['clouds'][name]['default']['filter']['state']
+        print "- filter exist for cloud", name
+    except:
+        config.create_filter(name, clouds.states(name))
+        config.write()
+"""
+
+print config
+
+clouds.all_filter()
+
+
+
+######################################################################
 # PROVISINOR
 ######################################################################
 provisionerImpl = ProvisionerSimulator
@@ -155,16 +204,83 @@ pages = FlatPages(app)
 # ACTIVATE STRUCTURE
 ######################################################################
 
-
 def make_active(name):
     active = {'home': "",
               'inventory': "",
-              "images":"",
+              'table': "",
               'contact': "",
-              'profile': ""}
+              'flavors': "",
+              'images': "",
+              'metric': "",
+              'profile': "",
+              'vm_info': "",
+              'projects': "",
+              'security': "",
+              'keys': "",
+              'clouds':""}
     
     active[name] = 'active'
     return active
+
+######################################################################
+# ROUTE: KEYS
+######################################################################
+
+@app.route('/keys/',methods=['GET','POST'])
+def managekeys():
+    
+    active = make_active('keys')
+    keys = cm_keys()
+
+    msg = ''
+    error = False
+    """
+    keys:
+      default: name 1 
+      keylist:
+         name 1: $HOME/.ssh/id_rsa.pub # this is automatically replaced with the key
+         name 2: $HOME/.ssh/id_rsa2.pub # this is automatically replaced with the key
+         bla: key ssh-rsa AAAAB3.....zzzz keyname
+    """
+    if request.method == 'POST' and request.form.has_key('keyname'):
+        keyname = request.form['keyname']
+        fileorstring = request.form['keyorpath']
+
+        if keys.defined(keyname):
+            
+            msg = "Key name already exists. Please delete the key '%s' before proceeding." % keyname           
+        else:
+            try:
+                keys.set(keyname, fileorstring, expand=True)
+                msg = 'Key %s added successfully' % keyname
+                keys.write()
+            except Exception, e:
+                keys.delete(keyname)
+                msg = e
+            
+    elif request.method == 'POST' :
+            keys['default'] = request.form['selectkeys']
+            keys.write()
+
+    return render_template('keys.html',
+                           keys=keys,
+                           active=active,
+                           show=msg)
+                        
+
+@app.route('/keys/delete/<name>/')
+def deletekey(name):
+
+    active = make_active('keys')
+    keys = cm_keys()
+
+    try:
+        keys.delete(name)
+        keys.write()
+    except:
+        print "Error: deleting the key %s" % name
+    return redirect("/keys/")
+
 
 ######################################################################
 # ROUTE: /
@@ -178,6 +294,254 @@ def index():
                            pages=pages,
                            active=active,
                            version=version)
+
+
+
+
+
+
+######################################################################
+# ROUTE: REFRESH
+######################################################################
+
+
+@app.route('/cm/refresh/')
+@app.route('/cm/refresh/<cloud>/')
+def refresh(cloud=None, server=None):
+    #print "-> refresh", cloud, server
+    clouds.refresh()
+    clouds.all_filter()
+    return table()
+
+######################################################################
+# ROUTE: Filter
+######################################################################
+
+    
+@app.route('/cm/filter/<cloud>/',methods=['GET','POST'])
+def filter(cloud=None):
+    #print "-> filter", cloud
+
+    #
+    # BUG: when cloud is none
+    #
+    name = cloud
+    if request.method == 'POST':
+        query_states = []
+        state_table = {}
+        for state in clouds.states(name):
+            state_name = "%s:%s" % (name,state)
+            state_table[state] = state_name in request.form
+            if state_table[state]:
+                query_states.append(state)
+        config.set_filter(name, state_table, 'state')
+
+        clouds.state_filter(name, query_states)
+
+        
+    return redirect("/table/")
+
+
+
+######################################################################
+# ROUTE: KILL
+######################################################################
+
+
+@app.route('/cm/kill/')
+def kill_vms():
+    print "-> kill all"
+    r = cm("--set", "quiet", "kill", _tty_in=True)
+    return table()
+
+######################################################################
+# ROUTE: DELETE
+######################################################################
+
+
+@app.route('/cm/delete/<cloud>/<server>/')
+def delete_vm(cloud=None, server=None):
+    print "-> delete", cloud, server
+    # if (cloud == 'india'):
+    #  r = cm("--set", "quiet", "delete:1", _tty_in=True)
+    clouds.delete(cloud, server)
+    time.sleep(5)
+    #    clouds.refresh()
+    return redirect("/table/")
+#    return table()
+
+######################################################################
+# ROUTE: DELETE GROUP
+######################################################################
+@app.route('/cm/delete/<cloud>/')
+def delete_vms(cloud=None):
+# donot do refresh before delete, this will cause all the vms to get deleted  
+    f_cloud = clouds.clouds[cloud]
+    for id, server in f_cloud['servers'].iteritems():   
+        print "-> delete", cloud, id
+        clouds.delete(cloud, id)
+    time.sleep(7)
+    f_cloud['servers'] = {}
+    return redirect("/table/")
+
+
+######################################################################
+# ROUTE: ASSIGN PUBLIC IP
+######################################################################
+
+
+@app.route('/cm/assignpubip/<cloud>/<server>/')
+def assign_public_ip(cloud=None, server=None):
+    try :
+        if configuration['clouds'][cloud]['cm_automatic_ip'] == False:
+            clouds.assign_public_ip(cloud,server)
+            clouds.refresh(names = [cloud])
+            return redirect("/table/")
+        else:
+            return "Manual public ip assignment is not allowed for %s cloud" % cloud
+    except Exception, e:
+         return str(e) + "Manual public ip assignment is not allowed for %s cloud" % cloud
+     
+######################################################################
+# ROUTE: START
+######################################################################
+
+#
+# WHY NOT USE cm_keys as suggested?
+#
+@app.route('/cm/start/<cloud>/')
+def start_vm(cloud=None, server=None):
+    print "*********** STARTVM", cloud
+    print "-> start", cloud
+    # if (cloud == 'india'):
+    #  r = cm("--set", "quiet", "start:1", _tty_in=True)
+    key = None
+
+    if configuration.has_key('keys'):
+        key = configuration['keys']['default']
+
+    # THIS IS A BUG
+    vm_flavor = clouds.default(cloud)['flavor']
+    vm_image = clouds.default(cloud)['image']
+
+    print "STARTING", config.prefix, config.index
+    result = clouds.create(cloud, config.prefix, config.index, vm_image, vm_flavor, key)
+    #print ">>>>>>>>>>>>>>", result
+    clouds.vm_set_meta(cloud, result['id'], {'cm_owner': config.prefix})
+    config.incr()
+    config.write()
+    
+    
+    return table()
+
+'''
+#gregors test
+@app.route('/cm/metric/<startdate>/<enddate>/<host>')
+def list_metric(cloud=None, server=None):
+    print "-> generate metric", startdate, endadte
+    #r = fg-metric(startdate, enddate, host, _tty_in=True)
+    return render_template('metric1.html',
+                           startdate=startdate,
+                           active=active,
+                           version=version,
+                           endate=enddate)
+    #return table()
+'''
+
+######################################################################
+# ROUTE: SAVE
+######################################################################
+
+
+@app.route('/save/')
+def save():
+    print "Saving the cloud status"
+    clouds.save()
+    return table()
+
+######################################################################
+# ROUTE: LOAD
+######################################################################
+
+
+@app.route('/load/')
+def load():
+    print "Loading the cloud status"
+    clouds.load()
+    return table()
+
+######################################################################
+# ROUTE: TABLE
+######################################################################
+
+
+@app.route('/table/')
+def table():
+    active = make_active('table')
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    filter()
+    return render_template('table.html',
+                           updated=time_now,
+                           keys="",  # ",".join(clouds.get_keys()),
+			   cloudmesh=clouds,
+                           clouds=clouds.clouds,
+                           pages=pages,
+                           active=active,
+                           config=config,
+                           version=version)
+
+
+
+
+######################################################################
+# ROUTE: VM Login
+######################################################################
+
+
+@app.route('/cm/login/<cloud>/<server>/')
+def vm_login(cloud=None,server=None):
+    global clouds
+    message = ''
+    active = make_active('vm_login')
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    
+    server=clouds.clouds[cloud]['servers'][server]
+     
+    if len(server['addresses'][server['addresses'].keys()[0]]) < 2:  
+        mesage = 'Cannot Login Now, Public IP not assigned'
+        print message
+        
+    else :
+        message = 'Logged in Successfully'
+        ip = server['addresses'][server['addresses'].keys()[0]][1]['addr'] 
+        # THIS IS A BUG AND MUST BE SET PER VM, E.G. sometimesvm type probably decides that?
+        print "ssh",'ubuntu@'+ip
+        xterm('-e','ssh','ubuntu@'+ip,_bg=True)
+        
+    return redirect("/table/")
+######################################################################
+# ROUTE: VM INFO
+######################################################################
+
+
+@app.route('/cm/info/<cloud>/<server>/')
+def vm_info(cloud=None,server=None):
+
+    active = make_active('vm_info')
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    clouds.clouds[cloud]['servers'][server]['cm_vm_id'] = server
+    clouds.clouds[cloud]['servers'][server]['cm_cloudname'] = cloud
+    
+    return render_template('vm_info.html',
+                           updated=time_now,
+                           keys="", 
+                           server=clouds.clouds[cloud]['servers'][server],
+                           id = server,
+                           cloudname = cloud, 
+                           active=active,
+                           version=version,
+                           table_printer=table_printer )
 
 ######################################################################
 # ROUTE: SAVE
@@ -198,6 +562,105 @@ def save():
 def load():
     print "Loading the inventory"
     return display_inventory()
+
+
+######################################################################
+# ROUTE: FLAVOR
+######################################################################
+
+#@app.route('/flavors/<cloud>/' )
+@app.route('/flavors/', methods=['GET','POST'])
+def display_flavors(cloud=None):
+
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")    
+    active = make_active('flavors')
+
+    if request.method == 'POST':
+        for cloud in config.active():
+            configuration['clouds'][cloud]['default']['flavor']=request.form[cloud] 
+            config.write()
+
+    return render_template(
+        'flavor.html',
+        updated=time_now,
+        cloudmesh=clouds,
+        clouds=clouds.clouds,
+        config=config,
+        active=active,
+        version=version)
+
+
+
+######################################################################
+# ROUTE: IMAGES
+######################################################################
+
+#@app.route('/images/<cloud>/')
+@app.route('/images/', methods=['GET','POST'])
+def display_images():
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")    
+    active = make_active('images')
+
+    if request.method == 'POST':
+        for cloud in config.active():
+            configuration['clouds'][cloud]['default']['image']=request.form[cloud] 
+            config.write()
+
+    return render_template(
+        'images.html',
+        active=active,
+        updated=time_now,
+        clouds=clouds.clouds,
+        cloudmesh=clouds,
+        config=config,
+        version=version)
+
+
+######################################################################
+# ROUTE: PROFILE
+######################################################################
+
+@app.route('/profile/', methods=['GET','POST'])
+def profile():
+    # bug the global var of the ditc should be used
+
+    active = make_active('profile')
+
+    projects = cm_projects()
+    person = configuration['profile']
+    keys = cm_keys()
+
+    
+    if request.method == 'POST':
+        projects.default = request.form['field-selected-project']
+        configuration['security']['default']=request.form['field-selected-securityGroup']
+        config.index = request.form['field-index']
+        config.prefix = request.form['field-prefix']
+        config.firstname = request.form['field-firstname']
+        config.lastname = request.form['field-lastname']
+        config.phone = request.form['field-phone']
+        config.email = request.form['field-email']
+        config.default_cloud = request.form['field-default-cloud']
+
+        #print request.form["field-cloud-activated-" + value]
+        config.write()
+
+    
+    time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
+    
+    address = '<br>'.join(str(x) for x in person['address']) 
+    return render_template('profile.html',
+                           updated=time_now,
+                           keys=keys,
+                           projects=projects,
+                           person=person,
+                           address=address,
+                           clouds=clouds,
+                           active=active,
+                           configuration=configuration,
+                           version=version,
+        )
+
 
 ######################################################################
 # ROUTE: INVENTORY TABLE
@@ -318,6 +781,31 @@ def page(path):
                            pages=pages,
                            active=active,
                            version=version)
+
+######################################################################
+# ROUTE: METRIC
+######################################################################
+#@app.route('/metric/<s_date>/<e_date>/<user>/<cloud>/<host>/<period>/<metric>')
+
+
+@app.route('/metric/main', methods=['POST', 'GET'])
+def metric():
+    global clouds
+    args = {"s_date": request.args.get('s_date', ''),
+            "e_date": request.args.get('e_date', ''),
+            "user": request.args.get('user', ''),
+            "cloud": request.args.get('cloud', ''),
+            "host": request.args.get('host', ''),
+            "period": request.args.get('period', ''),
+            "metric": request.args.get('metric', '')}
+
+    return render_template('metric.html',
+                           clouds=clouds.get(),
+                           metrics=clouds.get_metrics(args),
+                           pages=pages,
+                           active=make_active('metric'),
+                           version=version)
+
 
 
     
