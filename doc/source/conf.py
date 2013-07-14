f8dab86316211d99d38b2582105621fc9634ba5e
Gregor von Laszewski <laszewski@gmail.com>
2013-07-14 15:58:15 -0400

    switch off bootswatch

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -167,7 +167,7 @@ html_theme_options = {
     # such as "amelia" or "cosmo".
     #
     # Note that this is served off CDN, so won't be available offline.
-    'bootswatch_theme': "cosmo",
+    #'bootswatch_theme': "cosmo",
 }
 
 
