490b366e7617d4f77bfc597a49b865a32420b7a0
Gregor von Laszewski <laszewski@gmail.com>
2013-08-23 12:31:58 -0400

    clenup doc

---
+++ cloudmesh/provisioner/system_info.py
@@ -0,0 +1,35 @@
+from fabric.api import *
+from fabric.context_managers import settings
+
+@task
+def memory_usage():
+    run('free -m')
+
+def main():
+    hosts = 'localhost,india.futuregrid.org'    
+    with settings(host_string=hosts):
+        memory_usage()
+
+if __name__ == '__main__':
+   memory_usage()
+
+'''
+class SystemInfo:
+    
+    
+    def __init__(self):
+        pass
+    
+    def get(self, ips, refresh=True):
+        """gets the system info from the host with the given ips. 
+        if the info is not there it is loaded from the machine.
+        the result is a dict with attribute tuples. the first element in the tuple is the valu, the second is a unit. 
+        if no unit is avalable or needed None is used as unit."""
+        pass
+    
+    def _get(self,ip):
+        """gets the systme information from the machine with the given ip"""
+        pass
+'''    
+    
+    
---
+++ doc/source/support.rst
@@ -0,0 +1,5 @@
+Support
+=======
+
+If you run into problems when using the  framework, please use our 
+help form at `https://portal.futuregrid.org/help <https://portal.futuregrid.org/help>`_.
