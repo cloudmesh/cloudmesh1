71a3faebcaebcd595e9e528b34580f30cc5e2ce1
Gregor von Laszewski <laszewski@gmail.com>
2013-08-25 09:15:10 -0400

    moving from wrong branch

--- fabfile/install.py
+++ fabfile/install.py
@@ -2,32 +2,34 @@ from fabric.api import task, local
 
 @task
 def deploy():
-    """deploys the system on ubuntu""
+    """deploys the system on ubuntu"""
     # download()
     ubuntu()
-
     install()
 
 @task
 def download():
-    """downloads cloudmesh"""
+    '''downloads cloudmesh'''
     local("git clone git@github.com:cloudmesh/cloudmesh.git")
 
 @task
-def install()
+def install():
     local("pip install -r Requirements.txt")
     local("pip setup.py install")
 
 @task
 def ubuntu():
-    """prepares an ubuntu system and installs all needed packages before we install cloudmesch""
+    '''prepares an ubuntu system and installs all 
+    needed packages before we install cloudmesch'''
     packages = ["git", 
     	        "curl", 
 		"python-virtualenv", 
 		"python-dev", 
 		"libldap2-dev", 
 		"libsasl2-dev", 
-		"ldap-user"]
+		"ldap-user",
+        "mongodb-10gen",
+        "rabbitmq-server"]
     for package in packages:
     	local ("sudo apt-get install {0}".format(package)) 
 
