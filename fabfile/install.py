41383d44bd6a3bb0b00df7406612583b4899cd69
Gregor von Laszewski <laszewski@gmail.com>
2013-08-25 10:08:45 -0400

    improved install

--- fabfile/install.py
+++ fabfile/install.py
@@ -1,4 +1,9 @@
 from fabric.api import task, local
+import sys
+import platform
+
+def is_ubuntu():
+    return platform.dist()[0] == 'Ubuntu'
 
 @task
 def deploy():
@@ -17,19 +22,52 @@ def install():
     local("pip install -r Requirements.txt")
     local("pip setup.py install")
 
-@task
-def ubuntu():
-    '''prepares an ubuntu system and installs all 
-    needed packages before we install cloudmesch'''
-    packages = ["git", 
-    	        "curl", 
-		"python-virtualenv", 
-		"python-dev", 
-		"libldap2-dev", 
-		"libsasl2-dev", 
-		"ldap-user",
-        "mongodb-10gen",
-        "rabbitmq-server"]
+
+def install_mongodb():
+    if is_ubuntu():
+        local('sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10')
+        local(
+              'sudo sh -c "echo \'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen\' > /etc/apt/sources.list.d/10gen.list"')
+        local('sudo apt-get update')
+        local('sudo apt-get install mongodb-10gen')
+    elif sys.platform == "darwin":
+        local('ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"')
+        local('brew update')
+        local('brew install mongodb')
+
+
+def install_package(package):
+    if is_ubuntu():
+        local ("sudo apt-get install {0}".format(package)) 
+    elif sys.platform == "darwin":
+        print "Not yet supported"
+        sys.exit()
+    elif sys.platform == "win32":
+        print "Windows is not supported"
+        print "Use Linux instead"
+        sys.exit()
+
+@task 
+def install_packages(packages):
     for package in packages:
-    	local ("sudo apt-get install {0}".format(package)) 
+        install_package (package) 
+
+@task
+def ubntu():
+    '''prepares an system and installs all 
+    needed packages before we install cloudmesch'''
+    
+    install_packages(["git", 
+                      "curl", 
+                      "python-virtualenv", 
+                      "python-dev", 
+                      "libldap2-dev", 
+                      "libsasl2-dev", 
+                      "ldap-user"])
+    install_mongodb()
+    install_packages(["rabbitmq-server"])
+
+
+
+
 
