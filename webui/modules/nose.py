1252427ed7c12566cb2491859f472736036449cc
Gregor von Laszewski <laszewski@gmail.com>
2013-08-29 11:25:56 -0400

    introducing a run franework for running the nosetest

--- webui/modules/nose.py
+++ webui/modules/nose.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, redirect
 from cloudmesh.config.cm_config import cm_config
 from cloudmesh.pbs.pbs import PBS
 
 nose_module = Blueprint('nose_module', __name__)
 from ast import literal_eval
-
+from sh import pwd
 from cloudmesh.util.ping import ping
 #
 # ROUTE: PROFILE
@@ -15,7 +15,7 @@ from cloudmesh.util.ping import ping
 from sh import nosetests
 
 
-@nose_module.route('/ping')
+@nose_module.route('/test/ping')
 def display_pingtest():
 
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
@@ -36,24 +36,49 @@ def display_pingtest():
                            results=results)
 
 
-@nose_module.route('/nose/<test>')
-def display_nosetest(test):
+@nose_module.route('/test/nose')
+@nose_module.route('/test/nose/')
+@nose_module.route('/test/nose/<test>')
+def display_nosetest(test=None):
 
     time_now = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-    filename = "/tmp/nosetest.json"
+    
 
-    testname = "../tests/test_{0}.py".format(test)
-    result = nosetests("--with-json",
-                       "--json-file='{0}' {1}".format(filename, testname))
-
-    with open(filename, "r") as myfile:
-        data = literal_eval(myfile.read())
-
+    if test is None:
+        filename = "/tmp/nosetests_all.json"    
+    else:
+        testname = "../tests/test_{0}.py".format(test)
+        filename = "/tmp/nosetests_{0}.json".format(test)
+    try:
+        with open(filename, "r") as myfile:
+            tests = literal_eval(myfile.read())
+    except:
+        tests = {'name': 'please run test'}
+        
     return render_template('nosetest.html',
                            updated=time_now,
-                           tests=data,
-                           filename=testname,
-                           resultname=filename,
+                           tests=tests,
+                           filename=filename,
                            name=test)
+
+@nose_module.route('/test/run')
+@nose_module.route('/test/run/<test>')
+def run_nosetest(test=None):
+
+    if test is None:
+        filename = "/tmp/nosetests_all.json"    
+        testname = ""
+    else:
+        filename = "/tmp/nosetests_{0}.json".format(test)
+        testname = "../tests/test_{0}.py".format(test)
+
+    print "PWD", pwd()
+    try:
+        result = nosetests("--with-json", "-w", "../tests",
+                           "--json-file='{0}' {1}".format(filename, testname))
+    except:
+        pass
+    print "RRRR", result
+    return redirect("/test/nose")
 
