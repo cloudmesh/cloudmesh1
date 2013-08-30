3db0e331d08d8a2bf2bb6b1fac8f929331dc1be3
Gregor von Laszewski <laszewski@gmail.com>
2013-08-30 18:49:08 -0400

    is git confused? looks like these files aloversudden showed up as missing/modified

--- cloudmesh/user/cm_user.py
+++ cloudmesh/user/cm_user.py
@@ -1,6 +1,7 @@
-from config.cm_config import cm_config_server
+from cloudmesh.config.cm_config import cm_config_server
+from cloudmesh.util.logger import LOGGER
 
-from util.logger import LOGGER
+from pymongo import MongoClient
 
 # ----------------------------------------------------------------------
 # SETTING UP A LOGGER
@@ -8,56 +9,33 @@ from util.logger import LOGGER
 
 log = LOGGER('cm_user')
 
-class cm_userLDAP (CMUserProviderBaseType):
+class cm_user(object):
     
-    
-    provider = {}
-    
-    def __init__(self, collection="user"):
-        super( cm_mongo, self ).__init__()
+    def __init__(self):
+        db_name = cm_config_server().config["mongo"]["db"]
+        client = MongoClient()
+        db = client[db_name]
+        ldapcollection = 'user'
+        cloudcollection = 'cloudmesh'
+        self.db_clouds = db[cloudcollection]
+        self.db_users = db[ldapcollection]
+        
+    def info(self, portalname, cloudnames = []):
+        ldapret = self.db_users.find({"cm_user_id": portalname})
+        cloudret = self.db_clouds.find({"name": portalname, "cm_kind": "users"})
+        userinfo = {}
+        # username is unique in ldap
+        if ldapret.count() > 0:
+            ldapuser = ldapret[0]
+            del ldapuser['_id']
+            userinfo = ldapuser
+        userinfo['cloud_ids'] = []
+        for arec in cloudret:
+            del arec['_id']
+            if len(cloudnames) > 0:
+                if arec['cm_cloud'] in cloudnames:
+                    userinfo['cloud_ids'].append(arec)
+            else:
+                userinfo['cloud_ids'].append(arec)
+        return userinfo
 
-        
-    def refresh(self):
-        '''
-        refreshes the userdatabase from the user provider
-        '''
-        users = self.list()
-        for user in users:
-            data = self.get(username)
-            self.update(username, data)
-
-
-    def register(self, name, type, **kwargs):
-        '''
-        registers a provider with som parameters specified in the dict params
-        
-        :param name: the name of the provider
-        :param type: the type of the provider, overwrites a possibly given type in params
-        :param params: a dictionary describing wht needs to be poassed to the service that provides user information
-        '''
-        provider = {'type': type, 
-                    'name': name}
-        for k,v in kwargs.iteritems():
-            provider[k] = v
-            
-    def get(self,username):
-        '''
-        returns the dict associated with the username
-        :param username:
-        '''
-        return {}
-        
-    def list(self):
-        '''
-        returns a list with all usernames
-        '''
-        return []
-    
-        
-        
-        
-            
-        
-        
-        
-        
