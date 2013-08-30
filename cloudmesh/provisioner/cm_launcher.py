ad7c7d6a6b3c6f8a8b4d9fa93de87e7479f12405
Sridhar Rajagopalan <sridhar.ashk@gmail.com>
2013-08-30 13:15:09 -0400

    adding the simulatorLauncher and nose-tests for the same. This launcher only prints stuff. Config files not used

--- cloudmesh/provisioner/cm_launcher.py
+++ cloudmesh/provisioner/cm_launcher.py
@@ -1,6 +1,9 @@
+from cloudmesh.config.cm_config import cm_config_server 
+
+
 class BaseClassLauncher:
 
-    states = ["running", "deploy", "active", "error"]
+    states_list = ["running", "deploy", "active", "error"]
 
     def states(self):
         """array of all valid status msges"""
@@ -25,8 +28,7 @@ class BaseClassLauncher:
         """installs the chef stuff"""
         raise NotImplementedError()
 
-class ChefLauncher(BaseclassLauncher):
-
+class ChefLauncher(BaseClassLauncher):
     def register(self, yamlfilewithchefserverinfo):
         """
         will be in cludmesh_server.yaml in .futuregrid. reade available as
@@ -34,21 +36,59 @@ class ChefLauncher(BaseclassLauncher):
         """
         raise NotImplementedError()
 
-class SimulatorLauncher(BaseclassLauncher):
-
+class SimulatorLauncher(BaseClassLauncher):
     def register(self, yamlfilewithchefserverinfo):
-        """
-        will be in cludmesh_server.yaml in .futuregrid. reade available as
-        cm_config_server (If I am not mistaken )
-        """
-        raise NotImplementedError()
-
+        #registering fake data
+        self.recipies = {"india-openstack-essex":
+                                    [
+                                     {"name": "cluster",
+                                      "description": "blabla"},
+                                     {"name": "hadoop",
+                                      "description": "blabla"},
+                                     {"name": "whatever",
+                                      "description": "blabla"},
+                                     ],
+                    "sierra-openstack-grizzly": 
+                                    [
+                                     {"name": "cluster",
+                                      "description": "blabla"},
+                                     {"name": "hadoop",
+                                      "description": "blabla"},
+                                     {"name": "whatever",
+                                      "description": "blabla"},
+                                     ],
+                     }
+        self.columns = {"india-openstack-essex" :
+                       ["name", "description"] ,
+                       "sierra-openstack-grizzly":
+                        ["name", "description"] 
+                        }
+        print "SimulatorLuncher: register "
+        for host in self.recipies:
+            print "host : {}".format(host)
+            for recipie in self.recipies[host]:
+                for col in self.columns[host]:
+                    print col,": ",recipie[col]
+        return True        
     
     def run(self, host, recipie):
         """installs the chef stuff"""
-        print "SimulatorLauncher: run {0}, {1}".format(host, recepie)
+        print "SimulatorLauncher: run {0}, {1}".format(host, recipie)
         return True
-
+    
+    def states(self):
+        """array of all valid status msges"""
+        for state in self.states_list:
+            print "SimulatorLauncher: states {0}".format(state)
+        return self.states_list
+    
+    @property
+    def status(self):
+        """ returns string of status - at the moment returning the 0th position value"""
+        return_state = self.states_list[0]
+        print "Simulalator launcher: status - returning {0}".format(return_state)
+        return return_state
+    
 """
 debug = True
 
