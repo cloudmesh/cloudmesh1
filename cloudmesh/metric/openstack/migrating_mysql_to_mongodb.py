f34e9736fd7aa5c8234992f4857a42dc97fc1ea8
Gregor von Laszewski <laszewski@gmail.com>
2013-09-03 18:43:53 -0400

    adding simple error handeling to qstat

--- cloudmesh/metric/openstack/migrating_mysql_to_mongodb.py
+++ cloudmesh/metric/openstack/migrating_mysql_to_mongodb.py
@@ -31,23 +31,29 @@ class Migrate_MySQL_to_Mongo:
     def export_mysql_in_csv(self, table):
         """Export mysql database tables using 'mysql' command line tools"""
         query = "select * from %s.%s" % (self.dbinfo["mysqldb_dbname"], table)
-        res_mysql = mysql("-u", "{0}".format(self.dbinfo["mysqldb_userid"]),
-                          "-p{0}".format(self.dbinfo["mysqldb_passwd"]),
-                          "-h", "{0}".format(self.dbinfo["mysqldb_hostname"]),
-                          "-e", "\"{0}\"".format(query),
-                          "--batch")
+        cmd1 = "mysql -u %s -p%s -h %s -e \"%s\" --batch" % \
+        (self.dbinfo["mysqldb_userid"],
+         self.dbinfo["mysqldb_passwd"],
+         self.dbinfo["mysqldb_hostname"],
+         query)
 
-        # Add quote(") at first
-        # Then replace tab to ","
-        # , add quote(") between new line
-        output = "\"" + str(res_mysql) \
-                .replace("\t","\",\"") \
-                .replace("\n","\"\n\"")
+        cmd2 = "sed 's/\t/\",\"/g;s/^/\"/;s/$/\"/;s/\n//g'" 
 
-        #DEBUGGING
+        #p1 = subprocess.Popen(cmd1.split(), stdout=subprocess.PIPE)
+        res = mysql("-u", "{0}".format(self.dbinfo["mysqldb_userid"]),
+                    "-p{0}".format(self.dbinfo["mysqldb_passwd"]),
+                    "-h", "{0}".format(self.dbinfo["mysqldb_hostname"]),
+                    "-e", "\"{0}\"".format(query),
+                    "--batch")
+
+        p2 = subprocess.Popen(cmd2.split(" "), stdin=p1.stdout,
+                              stdout=subprocess.PIPE)
+
+        output = p2.communicate()[0]
         print output
 
-        self.csv_data = output
+        p1.stdout.close()
+        p2.stdout.close()
 
     def mongoimport_csv(self, table):
         # mongoimport -u cmetrics_user -p -d cloudmetrics -c cloudplatform
