1714eb23bcab3f9a64698c976da9ac7991932a10
Gregor von Laszewski <laszewski@gmail.com>
2013-09-04 03:24:14 -0400

    extension to the launch webui

--- webui/modules/launch.py
+++ webui/modules/launch.py
@@ -8,6 +8,48 @@ launch_module = Blueprint('launch  _module', __name__)
 # ROUTE: launch  
 #
 
+# fake list of recipies which we need to get from cm_launcher
+
+launch_recipies = {"india-openstack-essex":
+                   [
+                    {"name": "Slurm Cluster",
+                     "types": ["VM", "baremetal"],
+                     "description": "Deploys a Slurm cluster. One of the Vms is the Master, "
+                                    "while the others register with the master as worker nodes"
+                                    " The master will be the first node in the list"},
+                    {"name": "Hadoop",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a haddop cluster on the VMs specified"},
+                    {"name": "Ganglia",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a Ganglia service for the vms specified. "
+                                    "The ganglia server will be the first node in the list"},
+                    {"name": "Nagios",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a Nagios service for the vms specified. "
+                                    "The ganglia server will be the first node in the list"},
+                    ],
+                   "sierra-openstack-grizzly": 
+                   [
+                    {"name": "Slurm Cluster",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a Slurm cluster. One of the Vms is the Master, "
+                                    "while the others register with the master as worker nodes"
+                                    " The master will be the first node in the list"},
+                    {"name": "Hadoop",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a haddop cluster on the VMs specified"},
+                    {"name": "Ganglia",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a Ganglia service for the vms specified. "
+                                    "The ganglia server will be the first node in the list"},
+                    {"name": "Nagios",
+                     "types": ["VM", "baremetal"],                     
+                     "description": "Deploys a Nagios service for the vms specified. "
+                                    "The ganglia server will be the first node in the list"},
+                    ],
+                   }
+
 
 @launch_module.route('/cm/launch/<host>/<recipie>')
 def launch_run ():
@@ -16,35 +58,16 @@ def launch_run ():
 
 @launch_module.route('/cm/launch/launch_servers', methods = ["POST"])
 def launch_servers():
-    # fake list of recipies which we need to get from cm_launcher
+    
     columns = {"india-openstack-essex" :
                ["name", "description"] ,
                "sierra-openstack-grizzly":
                 ["name", "description"] 
     }
-    recipies = {"india-openstack-essex":
-                                [
-                                 {"name": "cluster",
-                                  "description": "blabla"},
-                                 {"name": "hadoop",
-                                  "description": "blabla"},
-                                 {"name": "whatever",
-                                  "description": "blabla"},
-                                 ],
-                "sierra-openstack-grizzly": 
-                                [
-                                 {"name": "cluster",
-                                  "description": "blabla"},
-                                 {"name": "hadoop",
-                                  "description": "blabla"},
-                                 {"name": "whatever",
-                                  "description": "blabla"},
-                                 ],
-                 }
     server = request.form.get("server")
     resources = request.form.getlist("resource")
     return_string = "in server " + server + "<br>"
-    for r in recipies[server]:
+    for r in launch_recipies[server]:
         if r["name"] in resources:
             return_string += " start " + str(r["name"]) + "<br>"
     return return_string
@@ -59,29 +82,9 @@ def display_launch_table():
                "sierra-openstack-grizzly":
                 ["name", "description"] 
     }
-    recipies = {"india-openstack-essex":
-                                [
-                                 {"name": "cluster",
-                                  "description": "blabla"},
-                                 {"name": "hadoop",
-                                  "description": "blabla"},
-                                 {"name": "whatever",
-                                  "description": "blabla"},
-                                 ],
-                "sierra-openstack-grizzly": 
-                                [
-                                 {"name": "cluster",
-                                  "description": "blabla"},
-                                 {"name": "hadoop",
-                                  "description": "blabla"},
-                                 {"name": "whatever",
-                                  "description": "blabla"},
-                                 ],
-                 }
-               
-    
+        
     return render_template('mesh_launch.html',
-                           recipies=recipies,
+                           recipies=launch_recipies,
                            columns=columns,
                            )
 
