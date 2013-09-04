6b6fc47fd00a9aa7c22321cc16972bf0e05c35fb
Gregor von Laszewski <laszewski@gmail.com>
2013-09-04 11:31:42 -0400

    faking the flag for cloudmesh.with_login

--- cloudmesh/__init__.py
+++ cloudmesh/__init__.py
@@ -1,4 +1,4 @@
-from cloudmesh.config.cm_config import cm_config_server
+#from cloudmesh.config.cm_config import cm_config_server
 from pbr import version
 
 __version__ = version.VersionInfo('cloudmesh')
@@ -13,8 +13,17 @@ __version__ = version.VersionInfo('cloudmesh')
 with_login = True
 
 
+
 try:
-     with_login = cm_config_server().get("ldap.with_ldap")
+     with_login = config.cm_config_server().get("ldap.with_ldap")
+     print "WWWW", with_login
+     
 except:
     with_login = False
-    print "WARGING: not using user login"
+    print "WARGING: not using user login", with_login
+
+
+with_login = False
+
+print "XXXXOOOO", with_login
+
