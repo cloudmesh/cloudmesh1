7204638f32d8adaedab7eb7e405551b195111109
Sridhar Rajagopalan <sridhar.ashk@gmail.com>
2013-09-06 11:37:12 -0400

    Launcher with celery. To do integrate with mongo and make fab file

---
+++ cloudmesh/launcher/queue/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from celery import Celery
+
+celery = Celery('cloudmesh.provisioner.queue.celery',
+                broker='amqp://guest@localhost',
+                backend='amqp://guest@localhost',
+                include=['cloudmesh.launcher.queue.tasks'])
+
+# Optional configuration, see the application user guide.
+celery.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_DISABLE_RATE_LIMITS = True
+    )
+
+if __name__ == '__main__':
+    celery.start()
+
