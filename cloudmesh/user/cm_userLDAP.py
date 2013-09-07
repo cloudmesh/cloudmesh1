1d598f667d2140fa0b8c136953d1898ac9f5239b
Gregor von Laszewski <laszewski@gmail.com>
2013-09-07 06:35:11 -0400

    added base dn in preparation for reading it from the yaml file

--- cloudmesh/user/cm_userLDAP.py
+++ cloudmesh/user/cm_userLDAP.py
@@ -37,7 +37,8 @@ class cm_userLDAP (CMUserProviderBaseType):
         # print "'" + userId + "':'" + authProvider + "':'" + authCred + "'"
         
         # print adminuser, adminpass
-        userdn = "uid=" + userId + ",ou=People,dc=futuregrid,dc=org"
+        basedn = "ou=People,dc=futuregrid,dc=org"
+        userdn = "uid={0},{1}".format(uid,basedn)
         # print userdn
         ldapconn = ldap.initialize("ldap://{0}".format(self.host))
         log.info("Initializing the LDAP connection to server: " + self.host)
