26060b3ca2d45dc74081f626e3f5e1762029a54b
Allan Streib <astreib@indiana.edu>
2013-09-11 16:12:59 -0400

    Fixing config reference for ConfigDict

--- webui/modules/mesh_hpc.py
+++ webui/modules/mesh_hpc.py
@@ -23,7 +23,7 @@ mesh_hpc_module = Blueprint('mesh_hpc_module', __name__)
 def display_mongo_qstat_refresh(host=None):
     
     config = cm_config()
-    user = config.config["cloudmesh"]["hpc"]["username"]
+    user = config["cloudmesh"]["hpc"]["username"]
     pbs = pbs_mongo()
     
     if host is None:
