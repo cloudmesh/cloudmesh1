2bd5cfb8c419f89bed4ff36634d527e81a5c1f50
Gregor von Laszewski <laszewski@gmail.com>
2013-09-15 16:06:55 -0400

    use a class to reduce code duplication

--- cloudmesh/launcher/cm_launcher_db.py
+++ cloudmesh/launcher/cm_launcher_db.py
@@ -12,53 +12,11 @@ import pprint
 
 log = LOGGER(__file__)
 
-class cm_launcher_db(object):
+class cm_launcher_db(cm_MongoBase):
 
     def __init__(self):
         self.cm_type = "launcher"
-        self.db = get_mongo_db(self.cm_type)
+        self.connect()
 
-    def set(self, username, d):
-        element = dict (d)
-        element["cm_id"] = username
-        element["cm_type"] = "userauth"
-        self.update({"cm_id": username, "cm_type": "userauth"}, element)
-
-
-    def update(self, query, values=None):
-        '''
-        executes a query and updates the results from mongo db.
-        :param query:
-        '''
-        if values is None:
-            return self.db.update(query, upsert=True)
-        else:
-           # print "query: ",query
-           # print "values: ",values
-            return self.db.update(query, {"$set":values}, upsert=True)
-
-
-    def insert(self, element):
-        self.db.insert(element)
-
-    def clear(self):
-        self.db.drop()
-
-
-    def find(self, query=None):
-        '''
-        executes a query and returns the results from mongo db.
-        :param query:
-        '''
-        if query == None:
-            return self.db.find()
-        return self.db.find(query)
-
-    def find_one(self, query):
-        '''
-        executes a query and returns the results from mongo db.
-        :param query:
-        '''
-        return self.db.find_one(query)
 
 
