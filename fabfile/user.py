5c7e05f10f57e278db6fb4d0507b47b05f88a11a
Gregor von Laszewski <laszewski@gmail.com>
2013-09-21 10:39:45 -0400

    adding a cloudmesh.yaml generator

---
+++ fabfile/user.py
@@ -0,0 +1,29 @@
+from fabric.api import task, local, settings, hide
+from pprint import pprint
+from cloudmesh.cm_mongo import cm_mongo
+from cloudmesh.config.cm_config import cm_config, cm_config_server
+from cloudmesh.user.cm_userLDAP import cm_userLDAP
+from cloudmesh.pbs.pbs_mongo import pbs_mongo
+from cloudmesh.util.util import path_expand as cm_path_expand
+from cloudmesh.util.util import banner
+from cloudmesh.inventory import Inventory
+from cloudmesh.user.cm_template import cm_template
+from cloudmesh.config.ConfigDict import ConfigDict
+
+from  yaml import dump as yaml_dump
+import sys
+import os.path
+
+@task
+def yaml():
+    """
+    creates a user from tempalte and a simplified version of the yaml file and
+    puts it into mongo as a profile
+    
+    """
+
+    with hide('status'):
+        user_config = ConfigDict(filename="~/.futuregrid/me.yaml")
+        t = cm_template("~/.futuregrid/etc/cloudmesh.yaml")
+        result = t.replace(kind="dict", values=user_config)
+        print yaml_dump(result, default_flow_style=False)
