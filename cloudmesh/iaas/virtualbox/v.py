4139e95c5c079ee72057d1319e69a7f30babfb3b
Gregor von Laszewski <laszewski@gmail.com>
2013-09-21 23:59:55 -0400

    refactoring dict management and adding get_state

--- cloudmesh/iaas/virtualbox/v.py
+++ cloudmesh/iaas/virtualbox/v.py
@@ -2,31 +2,23 @@ from sh import VBoxManage
 from collections import OrderedDict
 from pprint import pprint
 
-vbox_list = VBoxManage.bake("list","vms","-l")
-
+vbox_list = VBoxManage.bake("list", "vms", "-l")
+vbox_vminfo = VBoxManage.bake("showvminfo")
 #
 # LIST
 #
-def get_vms_info():
-    result = vbox_list()
-    d = {}
-    for info in result.split("\n\n\n"):
-        lines = info.split("\n\n")[0].split("\n")
-        m = {}
-        for line in lines:
-            try:
-                attribute, value =  line.split(':',1)
-                if attribute == "State":
-                    value = value.split("(")[0]
-                m.update ({attribute : value.strip()})
-            except:
-                print line
-                pass
-        try: 
-            d[m["Name"]] = m
+
+def convert_to_dict(lines):
+    m = {}
+    for line in lines:
+        try:
+            attribute, value = line.split(':', 1)
+            if attribute == "State":
+                value = value.split("(")[0]
+            m.update ({attribute : value.strip()})
         except:
             pass
-    return d
+    return m
 
 def filter_attributes(d, filterkeys=["Name", "UUID", "State"]):
     f = {}
@@ -35,22 +27,68 @@ def filter_attributes(d, filterkeys=["Name", "UUID", "State"]):
 
         f[m["Name"]] = dict(zip(filterkeys, [m[k] for k in filterkeys]))
     return f
-        
-def get_vms():
-    vms = get_vms_info()
-    f = filter_attributes(vms)
+
+def get_vms(filter=None):
+    result = vbox_list()
+    d = {}
+    for info in result.split("\n\n\n"):
+        lines = info.split("\n\n")[0].split("\n")
+        m = convert_to_dict(lines)
+        try:
+            d[m["Name"]] = m
+        except:
+            pass
+
+    if filter is None:
+        return d
+
+    if filter == "simple":
+        filterkeys = ["Name", "UUID", "State"]
+    else:
+        filterkeys = filter
+
+    f = filter_attributes(d, filterkeys)
     return f
 
 print "ALL INFO"
 print 70 * "-"
-vms = get_vms_info()
+vms = get_vms()
 
 pprint (vms)
 
 print "SMALL INFO"
 print 70 * "-"
 
-vms = get_vms()
+vms = get_vms("simple")
 
 pprint (vms)
+
+def get_vminfo(name, filter=None):
+    data = vbox_vminfo(name).split("\n\n")[0].split("\n")
+    m = convert_to_dict(data)
+    if filter is None:
+        return m
+
+    if filter == "simple":
+        filterkeys = ["Name", "UUID", "State"]
+    else:
+        filterkeys = filter
+
+    f = dict(zip(filterkeys, [m[k] for k in filterkeys]))
+    return f
+
+def get_state(name):
+    info = get_vminfo(name, filter=["State"])
+    return info["State"]
+
+
+print "OOOO"
+name = "compute1.puppetlabs.lan"
+pprint (get_vminfo(name))
+
+print "YYYYY"
+pprint (get_vminfo(name, "simple"))
+
+print "STATE"
+print get_state(name)
 
