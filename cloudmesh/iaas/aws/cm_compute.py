9286c304bf01305005238779d3041f09294eb77c
Hyungro Lee <hroe.lee@gmail.com>
2013-09-25 16:42:44 -0400

    started aws

---
+++ cloudmesh/iaas/aws/cm_compute.py
@@ -0,0 +1,22 @@
+import boto
+
+class aws:
+    """ Amazon Cloud service with the boto interface
+    With boto interface, cloudmesh supports Amazon Web Services such as EC2, S3,
+    EBS, etc.
+    """
+
+    def __init__(self):
+        return
+
+    def vm_create(self):
+        self.create_vm()
+
+    def create_vm(self):
+        return
+
+    def vm_delete(self):
+        self.delete_vm()
+
+    def delete_vm(self):
+        return
