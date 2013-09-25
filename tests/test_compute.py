aa775aefdb9a7274980a5e8df3ef2859706d1db4
Hyungro Lee <hroe.lee@gmail.com>
2013-09-24 20:14:08 -0400

    removed azure test from test_compute.py

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -67,11 +67,6 @@ class Test:
             print "LOADED CLOUDS"
         """
 
-    def setup_azure(self):
-        """setup azure"""
-        from cloudmesh.iaas.azure.cm_compute import azure
-        self.azure_cloud = azure("windows_azure")
-
     def test_get_extensions(self):
         """test geting the cloud extensions"""
         HEADING()
@@ -413,40 +408,6 @@ class Test:
         print "RESULT", result
         meta = self.cloud.get_meta(id)
         print meta
-
-    def test_14_azure(self):
-        """test azure"""
-        HEADING()
-        self.setup_azure()
-        self.azure_cloud.refresh("images")
-        print json.dumps(self.azure_cloud.dump('images'), indent=4)
-
-    def test_azure_services(self):
-        """test azure services"""
-        HEADING()
-        self.setup_azure()
-        self.azure_cloud.refresh("services")
-        print json.dumps(self.azure_cloud.dump('services'), indent=4)
-
-    def test_create_azure_vm(self):
-        """test to create azure vm"""
-        HEADING()
-        self.setup_azure()
-        self.azure_cloud.vm_create()
-        print json.dumps(self.azure_cloud.get_deployment(), indent=4)
-
-    def test_delete_azure_vm(self, name):
-        """Test to tear down azure vm"""
-        HEADING()
-        self.setup_azure()
-        self.azure_cloud.vm_delete(name)
-
-    def test_azure_deployments(self):
-        """Test to list deployments"""
-        HEADING()
-        self.setup_azure()
-        res = self.azure_cloud.list_deployments()
-        print json.dumps(res, indent=4)
 
     def test_15_list_secgroup(self):
         """list security group"""
