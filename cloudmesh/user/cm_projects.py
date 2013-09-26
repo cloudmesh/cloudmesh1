bccb70b660194730ee7757847e0f7d54ea28b863
Gregor von Laszewski <laszewski@gmail.com>
2013-09-26 06:27:23 -0400

    starting a class that pulls project information from the portal

---
+++ cloudmesh/user/cm_projects.py
@@ -0,0 +1,53 @@
+class cm_projects():
+
+    cm_kind = "fg_project"
+    project_url_basename = "https://portal.futuregrid.org/projects/"
+
+    def __init__(self):
+        raise NotImplementedError()
+
+    def update(self):
+        """retrieves the information from the portal into mongo"""
+        raise NotImplementedError()
+
+    def id(self, identifier):
+        """
+        returns the number of a probect. Input can be. 
+        Assume number is 82
+        
+        82, fg82, fg-82, FG82, FG-82
+        fg can aslo be uppercase 
+        """
+        raise NotImplementedError()
+        _id = itentifier.lower()
+        if _id.startswith("fg-"):
+            # fg-82
+            number = identifier.replace("fg-", "")
+        elif _id.startswith("fg"):
+            # fg82
+            number = identifier.replace("fg", "")
+        else:
+            # we just assume a number
+            number = _id
+        return str(number)
+
+    def url(self, identifier):
+        return project_url_basename + id(identifier)
+
+    def info(self, identifier):
+        """
+        returns a dict with the information about the project.
+        This includes
+           title
+           description
+
+            other information may be added in future
+        """
+        number = id(identifier)
+        raise NotImplementedError()
+
+    def add(self, identifier, **kwargs):
+        """
+        adds the named attributes to the mongo db for the given project
+        """
+        raise NotImplementedError()
