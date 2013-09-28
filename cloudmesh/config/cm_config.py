73044bcece582052c62877ef702abeb38331c4d9
Gregor von Laszewski <laszewski@gmail.com>
2013-09-28 07:27:16 -0400

    making cm_config bare bones

--- cloudmesh/config/cm_config.py
+++ cloudmesh/config/cm_config.py
@@ -41,7 +41,6 @@ def get_mongo_db(mongo_collection):
     return db_clouds
 
 
-
 class cm_config_server(ConfigDict):
     """
     reads the information contained in the file
@@ -58,7 +57,7 @@ class cm_config_server(ConfigDict):
 class cm_config_launcher(ConfigDict):
     """
     reads the information contained in the file
-    ~/.futuregrid/cloudmesh_server.yaml
+    ~/.futuregrid/cloudmesh_launcher.yaml
     """
     filename = os.path.expanduser("~/.futuregrid/cloudmesh_launcher.yaml")
 
@@ -86,129 +85,6 @@ class cm_config(ConfigDict):
         if filename is None:
             filename = self.filename
         ConfigDict.__init__(self, filename=filename)
-
-
-        self._userdata_handler = None
-        self._serverdata = None
-
-
-    # ----------------------------------------------------------------------
-    # Internal helper methods
-    # ----------------------------------------------------------------------
-    @deprecated
-    def _get_cloud_handler(self, cloud, as_admin=False):
-        """This gets a class that knows how to handle the specific type of
-        cloud (how to provision users, etc)"""
-        handler_args = { 'username': self.init_config['cloudmesh']['profile']['username'],
-                         'email':  self.init_config['cloudmesh']['profile']['e_mail'],
-                         'defaultproj': self.init_config['cloudmesh']['projects']['default'],
-                         'projectlist': self.init_config['cloudmesh']['projects']['active'],
-                         'cloudname': cloud,
-                         'cloudcreds': self.get_data(cloud),
-                         'cloudadmincreds': self.serverdata['keystone'][cloud] }
-        cloud_handler_class = cloudmesh_cloud_handler(cloud)
-        cloud_handler = cloud_handler_class(**handler_args)
-        return cloud_handler
-
-
-    # ----------------------------------------------------------------------
-    # Methods to initialize (create) the config config
-    # ----------------------------------------------------------------------
-    @deprecated
-    def _initialize_user(self, username):
-        """Loads user config, including profile, projects, and credentials"""
-        user = self.userdata_handler(username)
-
-        self.init_config['cloudmesh']['prefix'] = username
-        self.init_config['cloudmesh']['index'] = "001"
-
-        self.init_config['cloudmesh']['profile'] = {
-            'username': username,
-            'uid': user.uid,
-            'gid': user.gid,
-            'firstname': user.firstname,
-            'lastname': user.lastname,
-            'phone': user.phone,
-            'e_mail': user.email,
-            'address': user.address
-        }
-
-        keys = {'default': None, 'keylist': {}}
-        if user.keys:
-            for key in user.keys.keys():
-                if keys['default'] is None:
-                    keys['default'] = key
-                keys['keylist'][key] = user.keys[key]
-        self.init_config['cloudmesh']['keys'] = keys
-
-        self.init_config['cloudmesh']['projects'] = {
-            'active': user.activeprojects,
-            'completed': user.completedprojects,
-            'default': user.defaultproject
-        }
-
-        self.init_config['cloudmesh']['active'] = user.activeclouds
-        self.init_config['cloudmesh']['default'] = user.defaultcloud
-
-    @deprecated
-    def _initialize_clouds(self):
-        """Creates cloud credentials for the user"""
-        self.init_config['cloudmesh']['clouds'] = {}
-        cloudlist = self.init_config['cloudmesh']['active']
-        for cloud in cloudlist:
-            cloud_handler = self._get_cloud_handler(cloud, as_admin=True)
-            cloud_handler.initialize_cloud_user()
-            self.init_config['cloudmesh']['clouds'][cloud] = copy.deepcopy(cloud_handler.credentials)
-
-    @deprecated
-    def initialize(self, username):
-        """Creates or resets the config for a user.  Note that the
-        userdata_handler property must be set with appropriate handler class."""
-        self.init_config = collections.OrderedDict()
-        self.init_config['cloudmesh'] = collections.OrderedDict()
-        self._initialize_user(username)
-        self._initialize_clouds()
-
-    @deprecated
-    def change_own_password(self, cloudname, oldpass, newpass):
-        cloud_handler = self._get_cloud_handler(cloudname)
-        cloud_handler.change_own_password(oldpass, newpass)
-        # Save the yaml file so the new password is saved
-        self.write()
-
-    @deprecated
-    def get_own_passwords(self):
-        cloudlist = self.active()
-        passwords = {}
-        for cloud in cloudlist:
-            cloud_handler = self._get_cloud_handler(cloud)
-            passwords[cloud] = cloud_handler.get_own_password()
-        return passwords
-
-
-    # ----------------------------------------------------------------------
-    # print methods
-    # ----------------------------------------------------------------------
-
-
-    # ======================================================================
-    # NOT SURE WHY THESE METHODS ARE NEEDED
-    # ======================================================================
-
-    @property
-    def userdata_handler(self):
-        """Plug-in class that knows how to get all the user/project config"""
-        return self._userdata_handler
-
-    @userdata_handler.setter
-    def userdata_handler(self, value):
-        self._userdata_handler = value
-
-    @property
-    def serverdata(self):
-        if self._serverdata is None:
-            self._serverdata = yaml.safe_load(open(self.cloudmesh_server_path, "r"))
-        return self._serverdata
 
     # ======================================================================
     # Properties
@@ -433,7 +309,8 @@ class cm_config(ConfigDict):
             avalue = value
         return 'export %s="%s"\n' % (attribute, avalue)
 
-    def rc(self, name):
+
+    def rc_openstack(self, name):
         """returns the lines that can be put in an rc file"""
         result = self.cloud(name)
         lines = ""
@@ -444,6 +321,13 @@ class cm_config(ConfigDict):
                 for key in value:
                     lines += self.export_line(key, value[key])
         return lines
+
+    def rc (self, name):
+        kind = self.cloud(name)['cm_type']
+        if kind == "openstack":
+            return self.rc_openstack(name)
+        else:
+            print "CLOUDTYPE not supported:", kind
 
     @deprecated
     def rc_euca(self, name, project):
