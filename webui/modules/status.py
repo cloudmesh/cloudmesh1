fc8cea3b1739c9c97890cc2ef221efed78a096c0
Gregor von Laszewski <laszewski@gmail.com>
2013-09-29 13:45:41 -0400

    passing parameters via dict

--- webui/modules/status.py
+++ webui/modules/status.py
@@ -17,12 +17,22 @@ def display_status():
     msg = ""
     status = ""
 
+    values = {
+              'india' : { 'jobs' : 3, 'users' : 50},
+              'bravo' : { 'jobs' : 13, 'users' : 40},
+              'echo' : { 'jobs' : 23, 'users' : 30},
+              'hotel' : { 'jobs' : 33, 'users' : 20},
+              'sierra' : { 'jobs' : 43, 'users' : 10},
+              'alamo' : { 'jobs' : 53, 'users' : 1},
+              }
+
     categories = ['India', 'Bravo', 'Echo', 'Hotel', 'Sierra', 'Alamo']
     jobs = [43000, 19000, 60000, 35000, 50000, 70000]
     users = [50000, 39000, 42000, 31000, 50000, 70000]
 
 
     return render_template('status.html',
+                           values=values,
                            categories=categories,
                            jobs=jobs,
                            users=users,
