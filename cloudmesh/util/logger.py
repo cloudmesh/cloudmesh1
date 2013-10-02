33971095a0b5a702b4763c96836ff0a9d84034c9
Gregor von Laszewski <laszewski@gmail.com>
2013-10-02 02:20:18 -0400

    shorter log messages

--- cloudmesh/util/logger.py
+++ cloudmesh/util/logger.py
@@ -3,8 +3,10 @@ A simple class to set up a custom logger for a class
 '''
 
 import logging
+import sys
+import os
 
-def LOGGER(name):
+def LOGGER(filename):
     """creates a logger with the given name.
 
     You can use it as follows::
@@ -15,6 +17,14 @@ def LOGGER(name):
        log.warning("this is a warning")
 
     """
+    pwd = os.getcwd()
+    name = filename.replace(pwd, "$PWD")
+    try:
+        (first, name) = name.split("site-packages")
+        name = "... site" + name
+    except:
+        pass
+
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
