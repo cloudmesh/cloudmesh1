0f3460d7780d284eaf36806b99d6b6cf219918be
Gregor von Laszewski <laszewski@gmail.com>
2013-10-02 19:48:18 -0400

    adding a moog fab file

---
+++ fabfile/mooc.py
@@ -0,0 +1,12 @@
+from fabric.api import task, local, execute, hide, settings
+from fabric.contrib.console import confirm
+
+
+@task
+def start():
+    local("fab server.start:server=mooc,mooc")
+
+def stop():
+    local("fab server.stop")
+
+    
