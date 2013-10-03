54fbefb9ce8b035e5b1f68f7411daffa36dd282d
Allan Streib <astreib@indiana.edu>
2013-10-03 12:48:46 -0400

    Use keystone to check for users already existing

--- cmd3local/plugins/cm_shell_admin.py
+++ cmd3local/plugins/cm_shell_admin.py
@@ -11,7 +11,7 @@ from docopt import docopt
 from cmd3.shell import command
 from cloudmesh.util.util import path_expand, yn_choice
 from cloudmesh.user.cm_user import cm_user
-
+from cloudmesh.iaas.openstack.cm_idm import keystone
 
 
 from cloudmesh.util.logger import LOGGER
@@ -25,23 +25,34 @@ def generate_password():
     length = 12
     return ''.join([choice(chars) for _ in range(length)])
 
-def create_user_in_cloud(username, password, cloudname):
-    print "Create user {0} in {1}".format(username, cloudname)
-
-def user_exists_in_cloud(username, cloudname):
-    return False
-
-def get_user_profile(username):
-    cmu = cm_user()
-    profile = cmu.info('astreib')
-    return profile
-
 class cm_shell_admin:
 
     """Administrative class"""
 
+    def __init__(self):
+        self._keystones = { }
+
     def activate_cm_shell_admin(self):
         pass
+
+    def get_keystone(self, cloudname):
+        if cloudname not in self._keystones:
+            self._keystones[cloudname] = keystone(cloudname)
+        return self._keystones[cloudname]
+
+    def user_exists_in_cloud(self, username, cloudname):
+        k = self.get_keystone(cloudname)
+        k_id = k.get_user_by_name(username)
+        return not k_id is None
+
+    def get_user_profile(self, username):
+        cmu = cm_user()
+        profile = cmu.info(username)
+        return profile
+
+    def create_user_in_keystone(self, username, password, cloudname):
+        print "Create user {0} in {1}".format(username, cloudname)
+
 
     @command
     def do_admin(self, args, arguments):
@@ -82,12 +93,12 @@ class cm_shell_admin:
             etc_yaml = path_expand("~/.futuregrid/etc/cloudmesh.yaml")
             me_yaml = path_expand("~/.futuregrid/etc/me.yaml")
 
-            if user_exists_in_cloud(new_username, cloudname):
+            if self.user_exists_in_cloud(new_username, cloudname):
                 print "User {0} is already defined in cloud {1}".format(new_username, cloudname)
             else:
-                me_values = get_user_profile(new_username)
+                me_values = self.get_user_profile(new_username)
                 new_password = generate_password()
-                create_user_in_cloud(new_username, new_password, cloudname)
+                self.create_user_in_keystone(new_username, new_password, cloudname)
 
                 me_content = open(me_yaml, 'r').read()
                 me_values["passwords"] = { cloudname: new_password }
@@ -95,8 +106,3 @@ class cm_shell_admin:
                 print t.render(**me_values)
 
             return
-
-
-
-
-
