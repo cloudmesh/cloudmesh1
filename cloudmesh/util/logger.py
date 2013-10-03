5270e0f5d303d14a10c048a56e3e595165437e48
Gregor von Laszewski <laszewski@gmail.com>
2013-10-03 07:08:29 -0400

    better log formatting

--- cloudmesh/util/logger.py
+++ cloudmesh/util/logger.py
@@ -28,7 +28,7 @@ def LOGGER(filename):
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter(
-        'CM {0}: [%(levelname)s] %(message)s'.format(name))
+        'CM {0:>50}: %(levelname)6s - %(message)s'.format(name))
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
     log.addHandler(handler)
