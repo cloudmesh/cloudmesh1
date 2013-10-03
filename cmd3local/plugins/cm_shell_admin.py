317834507c0af9cffe0cf316d772e021a673523e
Allan Streib <astreib@indiana.edu>
2013-10-03 14:59:02 -0400

    Getting passwords from db to genereate me.yaml
    Adding creation of roles and tenants

--- cmd3local/plugins/cm_shell_admin.py
+++ cmd3local/plugins/cm_shell_admin.py
@@ -1,6 +1,8 @@
 import types
 import textwrap
 import inspect
+import os
+import stat
 import sys
 import importlib
 import string
@@ -26,11 +28,11 @@ def generate_password():
     return ''.join([choice(chars) for _ in range(length)])
 
 class cm_shell_admin:
-
     """Administrative class"""
 
     def __init__(self):
         self._keystones = { }
+        self._cmu = None
 
     def activate_cm_shell_admin(self):
         pass
@@ -40,28 +42,73 @@ class cm_shell_admin:
             self._keystones[cloudname] = keystone(cloudname)
         return self._keystones[cloudname]
 
+    def get_cm_user(self):
+        if self._cmu is None:
+            self._cmu = cm_user()
+        return self._cmu
+
     def user_exists_in_cloud(self, username, cloudname):
         k = self.get_keystone(cloudname)
         k_id = k.get_user_by_name(username)
+        return False
         return not k_id is None
 
     def get_user_profile(self, username):
-        cmu = cm_user()
+        cmu = self.get_cm_user()
         profile = cmu.info(username)
         return profile
 
+    def save_user_password(self, username, password, cloudname):
+        cmu = self.get_cm_user()
+        cmu.set_password(username, password, cloudname)
+
     def create_user_in_keystone(self, username, password, cloudname):
+        k = self.get_keystone(cloudname)
+        # k.create_new_user(username, password)
         print "Create user {0} in {1}".format(username, cloudname)
+
+    def create_user_project_tenants(self, username, cloudname, projects):
+        k = self.get_keystone(cloudname)
+        uid = k.get_user_by_name(username)
+        member_rid = k.get_role_by_name('_member_')
+        for p in projects:
+            tenant = k.get_tenant_by_name(p)
+            if tenant is None:
+                k.create_new_tenant(p)
+                tenant = k.get_tenant_by_name(p)
+            add_role_to_user_tenant(tenant, uid, member_rid)
+
+    def create_user_in_cloud(self, username, password, cloudname, projects):
+        self.create_user_in_keystone(username, password, cloudname)
+        self.create_user_project_tenants(username, cloudname, projects)
+        
+    def generate_me_yaml(self, username, values, configdir):
+        cmu = self.get_cm_user()
+        cm_passwords = cmu.get_passwords(username)
+        me_template_file = path_expand("~/.futuregrid/etc/me.yaml")
+        me_template = open(me_template_file, 'r').read()
+        t = Template(me_template)
+        render_result = t.render(passwords=cm_passwords, **values)
+        if configdir:
+            # print to file
+            location = path_expand('{0}/me.yaml'.format(configdir))
+            f = os.open(location, os.O_CREAT | os.O_TRUNC |
+                        os.O_WRONLY, stat.S_IRUSR | stat.S_IWUSR)
+            os.write(f, render_result)
+            os.close(f)
+        else:
+            print render_result
 
 
     @command
     def do_admin(self, args, arguments):
         """
         Usage:
-               admin [force] newuser USERNAME CLOUDNAME CONFIGDIR
+               admin [force] newuser USERNAME CLOUDNAME [CONFIGDIR]
                
-        Initializes new user in the CLOUDNAME cloud. A cloudmesh.yaml
-        file will be generated and placed in CONFIGDIR.
+        Initializes new user in the CLOUDNAME cloud. A me.yaml
+        file will be generated and placed in CONFIGDIR, or printed
+        to stdout if CONDIGDIR is None.
 
         Arguments:
 
@@ -71,7 +118,7 @@ class cm_shell_admin:
 
           CLOUDNAME  the name of the cloud
           
-          CONFIGDIR  where to place the cloudmesh.yaml file
+          CONFIGDIR  where to place the me.yaml file
 
           force      force mode does not ask. This may be dangerous.
 
@@ -89,20 +136,11 @@ class cm_shell_admin:
             cloudname = arguments["CLOUDNAME"]
             configdir = arguments["CONFIGDIR"]
 
-            cm_yaml = path_expand('{0}/cloudmesh.yaml'.format(configdir))
-            etc_yaml = path_expand("~/.futuregrid/etc/cloudmesh.yaml")
-            me_yaml = path_expand("~/.futuregrid/etc/me.yaml")
-
             if self.user_exists_in_cloud(new_username, cloudname):
-                print "User {0} is already defined in cloud {1}".format(new_username, cloudname)
+                print "User {0} is already created in cloud {1}".format(new_username, cloudname)
             else:
                 me_values = self.get_user_profile(new_username)
                 new_password = generate_password()
-                self.create_user_in_keystone(new_username, new_password, cloudname)
-
-                me_content = open(me_yaml, 'r').read()
-                me_values["passwords"] = { cloudname: new_password }
-                t = Template(me_content)
-                print t.render(**me_values)
-
-            return
+                self.create_user_in_cloud(new_username, new_password, cloudname, me_values['projects'])
+                self.save_user_password(new_username, new_password, cloudname)
+                self.generate_me_yaml(new_username, me_values, configdir)
