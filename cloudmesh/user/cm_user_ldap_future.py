69ff1b52e100c42a1119749299781ae8f3340ad8
Gregor von Laszewski <laszewski@gmail.com>
2013-10-04 17:58:15 -0400

    fixing the conflict based on tab changes

---
+++ cloudmesh/user/cm_user_ldap_future.py
@@ -0,0 +1,240 @@
+from cloudmesh.config.cm_config import cm_config_server
+from cloudmesh.util.logger import LOGGER
+from CMUserProviderBaseType import CMUserProviderBaseType
+import ldap
+import sys
+
+# ----------------------------------------------------------------------
+# SETTING UP A LOGGER
+# ----------------------------------------------------------------------
+
+log = LOGGER(__file__)
+
+
+
+class cm_user_ldap (CMUserProviderBaseType):
+
+    providers = {}
+    host = None
+    cert = None
+
+    def get_config(self, **kwargs):
+
+        if not kwargs.has_key('host'):  # if kwargs['host'] is None:
+            self.host = cm_config_server().get("cloudmesh.server.ldap.hostname")
+
+        if not kwargs.has_key('ldapcert'):  # if kwargs['ldapcert'] is None:
+            self.cert = cm_config_server().get("cloudmesh.server.ldap.cert")
+
+
+    def authenticate(self, userId, password, **kwargs):
+        ret = False
+
+        # bug pass **kwargs
+        self.get_config()
+
+
+        # print "'" + userId + "':'" + authProvider + "':'" + authCred + "'"
+
+        # print adminuser, adminpass
+        basedn = "ou=People,dc=futuregrid,dc=org"
+        userdn = "uid={0},{1}".format(userId, basedn)
+        # print userdn
+        ldapconn = ldap.initialize("ldap://{0}".format(self.host))
+        log.info("Initializing the LDAP connection to server: " + self.host)
+        try:
+            ldapconn.start_tls_s()
+            log.info("tls started...")
+            ldapconn.bind_s(userdn, password)
+            ret = True
+        except ldap.INVALID_CREDENTIALS:
+            log.info("Your username or password is incorrect. Cannot bind.")
+            ret = False
+        except ldap.LDAPError:
+            log.info("User '" + userId + "' failed to authenticate due to LDAP error. The user may not exist." + str(sys.exc_info()))
+            ret = False
+        except:
+            ret = False
+            log.info("User '" + userId + "' failed to authenticate due to possible password encryption error." + str(sys.exc_info()))
+        finally:
+            log.info("Unbinding from the LDAP.")
+            ldapconn.unbind()
+        return ret
+
+    def __init__(self, collection="user"):
+        super(cm_userLDAP, self).__init__()
+        self.ldapconn = None
+
+    def refresh(self):
+        '''
+        Refresh the user database from the user provider
+        '''
+        self._refresh()
+        users = self.list()
+        for user in users:
+            data = self.get(user)
+            self.updates(user, data)
+
+    def connect(self, name, type, **kwargs):
+        '''
+        Register a provider with som parameters specified in the dict params
+        
+        :param name: the name of the provider
+        :param type: the type of the provider, overwrites a possibly given type in params
+        :param params: a dictionary describing wht needs to be poassed to the service that provides user information
+        '''
+        # kwargs host=None, ldapcert=None)
+        provider = {'type': type,
+                    'name': name}
+        for k, v in kwargs.iteritems():
+            provider[k] = v
+
+        if not kwargs.has_key('host'):  # if kwargs['host'] is None:
+            self.host = cm_config_server().get("cloudmesh.server.ldap.hostname")
+
+        if not kwargs.has_key('ldapcert'):  # if kwargs['ldapcert'] is None:
+            self.cert = cm_config_server().get("cloudmesh.server.ldap.cert")
+
+
+        ldap.set_option(ldap.OPT_X_TLS_CACERTFILE, self.cert)
+        # BUG IN FINAL VERSION MAKE SURE WE CHECK
+        ldap.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_NEVER)
+
+        self.ldapconn = ldap.initialize("ldap://" + self.host)
+        self.ldapconn.start_tls_s()
+        self.ldapconn.bind_s('', '')
+        self.users = {}
+
+    def disconnect(self):
+        if self.ldapconn:
+            self.ldapconn.unbind()
+
+    def __del__(self):
+        self.disconnect()
+
+
+    def get(self, username):
+        '''
+        Return the dict associated with the username
+        :param username:
+        '''
+        return self.users[username]
+
+    def list(self):
+        '''
+        Return a list with all usernames
+        '''
+        return self.users.keys()
+
+    def _refresh(self):
+        self._getUsers()
+        self._getProjects()
+        return self.users
+
+
+    def _getUsers(self, kind="flat"):
+        # print "LDAP query..."
+        try:
+            # ldap constant
+            ldapbasedn = "dc=futuregrid,dc=org"
+            ldapscope = ldap.SCOPE_SUBTREE
+            ldapfilter = "(objectclass=inetorgperson)"
+            # info to be retrieved from ldap
+            ldapattribs = ['uid',
+                           'uidNumber',
+                           'gidNumber',
+                           'mail',
+                           'givenName',
+                           'sn',
+                           'telephoneNumber',
+                           'address',
+                           'sshPublicKey']
+
+            # retrieve all ldap users
+            ldapresults = list(self.ldapconn.search_s(ldapbasedn, ldapscope, ldapfilter, ldapattribs))
+            for ldapresult in ldapresults:
+                # print ldapresult[1]
+                # a result may not have 'mail' attribute, but valid account must have
+                if ldapresult[1].has_key('mail'):
+                    ldapmail = ldapresult[1]['mail'][0]
+                    ldapuid = ldapresult[1]['uid'][0]
+                    ldapuidNumber = ldapresult[1]['uidNumber'][0]
+                    ldapgidNumber = ldapresult[1]['gidNumber'][0]
+                    firstname = ldapresult[1]['givenName'][0]
+                    lastname = ldapresult[1]['sn'][0]
+                    phone = ldapresult[1]['telephoneNumber'][0] if 'telephoneNumber' in ldapresult[1] else None
+                    address = 'TBD'  # not currently in LDAP
+
+                    keys = {}
+                    if ldapresult[1].has_key('sshPublicKey'):
+                        for sshkey in ldapresult[1]['sshPublicKey']:
+                            if sshkey.strip():
+                                (keytype, key, nickname) = (
+                                    sshkey.strip().split(None, 2) + [''])[0:3]
+                                keys[nickname.translate(None, '.$')] = "key %s" % sshkey
+
+                    if kind == "flat":
+                        self.users[ldapuid] = {"firstname":firstname,
+                                               "lastname":lastname,
+                                               "uidNumber":ldapuidNumber,
+                                               "gidNumber":ldapgidNumber,
+                                               "phone":phone,
+                                               "email":ldapmail,
+                                               "address":address,
+                                               "projects":{"active":[]},
+                                               "keys":keys }
+                    elif kind == "yaml":
+                        self.users[ldapuid] = {"profile":{
+                                                          "firstname":firstname,
+                                                          "lastname":lastname,
+                                                          "uidNumber":ldapuidNumber,
+                                                          "gidNumber":ldapgidNumber,
+                                                          "phone":phone,
+                                                          "email":ldapmail,
+                                                          "address":address,
+                                               },
+                                               "projects":{"active":[]},
+                                               "keys":keys }
+
+        except:
+            print "WRONG" + str(sys.exc_info())
+
+    def _getProjects(self):
+        try:
+            ldapbasedn = "dc=futuregrid,dc=org"
+            ldapscope = ldap.SCOPE_SUBTREE
+            ldapfilter = "(&(objectclass=posixGroup)(cn=fg*))"
+            ldapattribs = ['cn', 'memberUid']
+            ldapresults = list(self.ldapconn.search_s(ldapbasedn, ldapscope, ldapfilter, ldapattribs))
+            for ldapresult in ldapresults:
+                if ldapresult[1].has_key('cn'):
+                    cn = ldapresult[1]['cn'][0]
+                    projid = cn[2:]
+                    if ldapresult[1].has_key('memberUid'):
+                        for auid in ldapresult[1]['memberUid']:
+                            if self.users.has_key(auid):
+                                if self.users[auid]["projects"]["active"] is not None:
+                                    self.users[auid]["projects"]["active"].append(int(projid))
+                                else:
+                                    self.users[auid]["projects"]["active"] = [int(projid)]
+        except:
+            print "WRONG" + str(sys.exc_info())
+
+
+
+def main():
+    idp = cm_userLDAP (CMUserProviderBaseType)
+    idp.connect("fg-ldap", "ldap")
+    idp.refresh()
+    users = idp.list()
+
+    from pprint import pprint
+
+    pprint(users)
+
+    pprint(idp.users)
+
+
+
+if __name__ == "__main__":
+    main()
--- webui/modules/rack.py
+++ webui/modules/rack.py
@@ -1,31 +1,22 @@
 from flask import Blueprint
 from flask import render_template, request, redirect
-<<<<<<< HEAD
-#from cloudmesh.util.util import path_expand
-=======
 # from cloudmesh.util.util import path_expand
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
+
 from cloudmesh.config.cm_config import cm_config_server
 
 # from cloudmesh.config.cm_rack import cm_keys
 
 from cloudmesh.rack.cluster_map_heat import HeatClusterMap
 from cloudmesh.rack.cluster_map_service import ServiceClusterMap
-<<<<<<< HEAD
 from cloudmesh.rack.fetch_cluster_info import FetchClusterInfo
-=======
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
 
 from sh import pwd  # @UnresolvedImport
 from flask.ext.wtf import Form  # @UnresolvedImport
 from wtforms import SelectField
 from flask.ext.login import login_required  # @UnresolvedImport
-
-<<<<<<< HEAD
 from pprint import pprint
 
-=======
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
+
 rack_module = Blueprint('rack_module', __name__)
 
 #
@@ -33,29 +24,28 @@ rack_module = Blueprint('rack_module', __name__)
 #
 
 class RackForm(Form):
-<<<<<<< HEAD
     select_rack = SelectField()
     select_service = SelectField()
-    
-    rack_list = [ ('all',   'All Clusters'),
+
+    rack_list = [ ('all', 'All Clusters'),
                   ('india', 'India Cluster'),
-                  ('echo',  'Echo Cluster'),
-                  ('delta', 'Delta Cluster'), 
+                  ('echo', 'Echo Cluster'),
+                  ('delta', 'Delta Cluster'),
                   ('bravo', 'Bravo Cluster')
                  ]
-    
-    service_list = [ 
+
+    service_list = [
         ('service', 'Service Map'),
         ('temperature', 'Heat Map')
     ]
-    
+
     def initForm(self):
-        self.select_rack.choices=self.rack_list
-        self.select_service.choices=self.service_list
-        
+        self.select_rack.choices = self.rack_list
+        self.select_service.choices = self.service_list
+
     def validate_on_submit(self):
         return True
-    
+
 
 @rack_module.route('/inventory/rack')
 def display_rack_home():
@@ -63,66 +53,33 @@ def display_rack_home():
     if rack_form.validate_on_submit():
         rack_form.initForm()
         return render_template("rack.html", form=rack_form, flag_home=True)
-=======
-	select_rack = SelectField()
-	select_service = SelectField()
 
-	rack_list = [ ('india', 'India Cluster'),
-				  ('echo', 'Echo Cluster'),
-				  ('delta', 'Delta Cluster'),
-				  ('bravo', 'Bravo Cluster'),
-				  ('all', 'All Clusters')
-				]
-
-	service_list = [ ('temperature', 'Heat Map'),
-				     ('service', 'Service Map')
-				   ]
-
-	def initForm(self):
-		self.select_rack.choices = self.rack_list
-		self.select_rack.data = "bravo"
-
-		self.select_service.choices = self.service_list
-		self.select_service.data = 'temperature'
-
-	def validate_on_submit(self):
-		return True
-
-
-@rack_module.route('/inventory/rack')
-def display_rack_home():
-	rack_form = RackForm()
-	if rack_form.validate_on_submit():
-		rack_form.initForm()
-		return render_template("rack.html", form=rack_form, flag_home=True)
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
 
 
 @rack_module.route('/inventory/rack/map', methods=['POST'])
 def display_rack_map():
-<<<<<<< HEAD
-    
+
     ####
     #
     #  Flag of debug
     #
     ####
     flag_debug = False
-    
+
     rack = request.form['select_rack']
     service = request.form['select_service']
     # get location of configuration file, input diag, output image
     dir_base = "~/.futuregrid"
     server_config = cm_config_server()
-    relative_dir_diag = server_config.get("rack.input")
-    relative_dir_image = server_config.get("rack.diagrams." + service)
+    relative_dir_diag = server_config.get("cloudmesh.rack.input")
+    relative_dir_image = server_config.get("cloudmesh.rack.diagrams." + service)
     print "relative dir image, ", relative_dir_image
     flask_dir = "static"
     # guess absolute path of webui
     rack_py_dir = pwd().strip().split("/")
-    #print "rack_py_dir dir,,,,", rack_py_dir
-    webui_dir = rack_py_dir    #[:-1]
-    #print "webui dir,,,,", webui_dir
+    # print "rack_py_dir dir,,,,", rack_py_dir
+    webui_dir = rack_py_dir  # [:-1]
+    # print "webui dir,,,,", webui_dir
     list_image_dir = [ flask_dir ] + relative_dir_image.strip().split("/");
     abs_dir_image = "/".join(webui_dir + list_image_dir)
     abs_dir_diag = dir_base + "/" + relative_dir_diag
@@ -148,81 +105,22 @@ def display_rack_map():
     map_class.update(dict_data)
     # plot map
     map_class.plot()
-    
+
     # get image names
     filename_image = map_class.getImageFilename()
     filename_legend = map_class.getLegendFilename()
     abs_web_path_image = "/".join([""] + list_image_dir + [filename_image])
     abs_web_path_legend = "/".join([""] + list_image_dir + [filename_legend])
-    
-    return render_template( "rack.html", 
-                            flag_home=False, 
+
+    return render_template("rack.html",
+                            flag_home=False,
                             rack=rack,
-                            imageWidth=300 * map_class.getRackCount(), 
+                            imageWidth=300 * map_class.getRackCount(),
                             service=service,
                             imageFilename=abs_web_path_image,
                             legendFilename=abs_web_path_legend
                             )
-=======
 
-	####
-	#
-	#  Flag of debug
-	#
-	####
-	flag_debug = True
-
-	rack = request.form['select_rack']
-	service = request.form['select_service']
-	# get location of configuration file, input diag, output image
-	dir_base = "~/.futuregrid"
-	server_config = cm_config_server()
-	relative_dir_diag = server_config.get("cloudmesh.server.rack.input")
-	relative_dir_image = server_config.get("cloudmesh.server.rack.diagrams." + service)
-	print "relative dir image, ", relative_dir_image
-	flask_dir = "static"
-	# guess absolute path of webui
-	rack_py_dir = pwd().strip().split("/")
-	# print "rack_py_dir dir,,,,", rack_py_dir
-	webui_dir = rack_py_dir  # [:-1]
-	# print "webui dir,,,,", webui_dir
-	list_image_dir = [ flask_dir ] + relative_dir_image.strip().split("/");
-	abs_dir_image = "/".join(webui_dir + list_image_dir)
-	abs_dir_diag = dir_base + "/" + relative_dir_diag
-	# dynamic generate image
-	map_class = None
-	if service == "temperature":
-		map_class = HeatClusterMap(rack, dir_base, abs_dir_diag, abs_dir_image)
-	elif service == "service":
-		map_class = ServiceClusterMap(rack, dir_base, abs_dir_diag, abs_dir_image)
-	# get cluster server data
-	dict_data = None
-	if flag_debug:
-		dict_data = map_class.genRandomValues()
-	else:
-		# fetch the real data ....
-		# to do ...
-		pass
-	# update data
-	map_class.update(dict_data)
-	# plot map
-	map_class.plot()
-
-	# get image names
-	filename_image = map_class.getImageFilename()
-	filename_legend = map_class.getLegendFilename()
-	abs_web_path_image = "/".join([""] + list_image_dir + [filename_image])
-	abs_web_path_legend = "/".join([""] + list_image_dir + [filename_legend])
-
-	return render_template("rack.html",
-							flag_home=False,
-							rack=rack,
-							imageWidth=300 * map_class.getRackCount(),
-							service=service,
-							imageFilename=abs_web_path_image,
-							legendFilename=abs_web_path_legend
-							)
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
 
 
 
@@ -230,7 +128,7 @@ def display_rack_map():
 def display_all_racks():
 
 
-<<<<<<< HEAD
+
     # dir = path_expand(cm_config_server().get("rack.path"))
 
     # not so nice cludge, ask for location of statcic instead
@@ -243,20 +141,6 @@ def display_all_racks():
     return render_template('rack.html',
                            name="india",
                            rack=rack)
-=======
-	# dir = path_expand(cm_config_server().get("cloudmesh.server.rack.path"))
-
-	# not so nice cludge, ask for location of statcic instead
-
-	# web_pwd = pwd().strip()
-	# basename = "/static/{0}/{1}".format(dir, filename)
-
-	rack = None
-
-	return render_template('rack.html',
-						   name="india",
-						   rack=rack)
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
 
 
 @rack_module.route('/inventory/rack/<name>')
@@ -264,14 +148,13 @@ def display_all_racks():
 def display_rack(name, service=None):
 
 
-<<<<<<< HEAD
     if service is None:
         service = "temperature"
     print "test begin/...."
     basename = None
     # diag_dir = path_expand(cm_config_server().get("rack.input"))
     # output_dir = path_expand(cm_config_server().get("rack.diagramms.{0}".format(service)))
-    
+
 
     # not so nice cludge, ask for location of statcic instead
 
@@ -300,40 +183,3 @@ def display_rack(name, service=None):
                            basename=basename,
                            name=name,
                            rack=rack)
-=======
-	if service is None:
-		service = "temperature"
-	print "test begin/...."
-	basename = None
-	# diag_dir = path_expand(cm_config_server().get("cloudmesh.server.rack.input"))
-	# output_dir = path_expand(cm_config_server().get("rack.diagramms.{0}".format(service)))
-
-
-	# not so nice cludge, ask for location of statcic instead
-
-	# web_pwd = pwd().strip()
-	# basename = "/static/{0}/{1}".format(output_dir, name)
-
-	#  /static/racks/diagrams/india
-	# .svg
-	# .png
-	# -legend.png
-
-
-	#
-	# CREATE YOU IMAGES NOW
-	#
-
-	# if service == "temperature":
-	# 	do this
-	# else:
-	# 	do that
-
-	rack = name
-
-	return render_template('rack.html',
-						   service=service,
-						   basename=basename,
-						   name=name,
-						   rack=rack)
->>>>>>> 23fb529db48ad61ed16474c1083d50c18bdda201
