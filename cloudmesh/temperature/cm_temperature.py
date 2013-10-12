af4fcdfad999afc98e4ed50bc258d9348cb92c31
Gregor von Laszewski <laszewski@gmail.com>
2013-10-11 21:53:17 -0400

    format

--- cloudmesh/temperature/cm_temperature.py
+++ cloudmesh/temperature/cm_temperature.py
@@ -1,7 +1,7 @@
 from cloudmesh.config.cm_config import cm_config_server
 from cloudmesh.inventory import Inventory
 from pprint import pprint
-from sh import ssh 
+from sh import ssh
 
 # class cm_temperature:
 
@@ -13,14 +13,14 @@ inventory = Inventory()
 
 def get_ipmi_temperature(host):
 
-    (hostaddr, data) =  inventory.ipadr_cluster (hostname, "bmc")
+    (hostaddr, data) = inventory.ipadr_cluster (hostname, "bmc")
     clustername = data["cm_cluster"]
     config = config_server[clustername]['bmc']
     password = config['password']
     username = config['user']
 
-    command = "ipmitool -I lanplus -U {0} -P {1} -E -H {2} sdr type temperature".format(username,password,hostaddr)
-    
+    command = "ipmitool -c -I lanplus -U {0} -P {1} -E -H {2} sdr type temperature".format(username, password, hostaddr)
+
     proxyaddr = config['proxy']['ip']
     proxyusername = config['proxy']['user']
 
