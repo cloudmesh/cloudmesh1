106031da5863c7b4787acec4bd731cfe6713f235
Gregor von Laszewski <laszewski@gmail.com>
2013-10-22 12:52:58 -0400

    menu with icon links

--- webui/modules/menu.py
+++ webui/modules/menu.py
@@ -8,6 +8,61 @@ log = LOGGER(__file__)
 
 menu_module = Blueprint('menu_module', __name__)
 
+super_duper_sidebar_pages = [
+    ["Cloudmesh", None, None, ['all'],
+        [
+            ["Home", "/", None],
+            ["Status", "/status", None],
+            ["Profile", "/profile/", None],
+        ],
+    ],
+    ["Clouds", "cm/refresh", "365_restart", ['all'],
+        [
+            ["Refresh", "/cm/refresh", None],
+            ["VMs", "/mesh/servers", None],
+            ["Images", "/mesh/images", None],
+            ["Flavors", "/mesh/flavors/", None],
+        ],
+    ],
+    ["HPC Queues", None, None, ['all'],
+        [
+            ["Jobs", "/mesh/qstat", None],
+            ["Queueu Info", "/mesh/qinfo", None],
+            ["Rack Diagram", "/inventory/rack", None],
+        ]
+    ],
+
+    ["Admin", None, None, ['admin'],
+        [
+            ["Admin", "/admin", None],
+            ["Users - LDAP", "/users/ldap", None],
+            ["Users - Cloud", "/mesh/users/", None],
+            ["Register - Cloud", "/mesh/register/clouds", None],
+        ]
+    ],
+    ["Admin - Inventory", None, None, ['admin'],
+        [
+            ["Overview", "/inventory/", None],
+            ["Table", "/inventory/summary", None],
+            ["Images", "/inventory/images", None],
+        ],
+    ],
+    ["Admin - Provision", None, None, ['admin', 'rain'],
+        [
+            ["Policy", "/provision/policy", None],
+            ["Overview", "/provision/summary/", None],
+            ["Form", "/provision/", None],
+        ],
+    ],
+    ["Admin - Launcher", None, None, ['admin', 'rain'],
+        [
+            ["Launcher", "/cm/launch", None],
+            ["Register", "/cm/register", None],
+        ]
+    ],
+]
+
+
 super_sidebar_pages = [
     ["Cloudmesh",
         [
@@ -63,6 +118,7 @@ super_sidebar_pages = [
 ]
 
 flask.Flask.app_ctx_globals_class.super_sidebar_pages = super_sidebar_pages
+flask.Flask.app_ctx_globals_class.super_duper_sidebar_pages = super_duper_sidebar_pages
 
 app_topbar = [
     ["Home", "/"],
