ef71215292080e3abce3d7d9af69169452d86299
Gregor von Laszewski <laszewski@gmail.com>
2013-10-23 15:43:50 -0400

    sample list command shell

---
+++ cmd3local/plugins/cm_shell_list.py
@@ -0,0 +1,51 @@
+import types
+import textwrap
+from docopt import docopt
+import inspect
+import sys
+import importlib
+from cmd3.shell import command
+
+from cloudmesh.util.logger import LOGGER
+
+log = LOGGER(__file__)
+
+class cm_shell_list:
+
+    """opt_example class"""
+
+    def activate_cm_shell_list(self):
+        pass
+
+    @command
+    def do_list(self, args, arguments):
+        """
+        Usage:
+               list
+               list flavors
+               list servers
+               list images
+               
+        Options:
+
+           -v       verbose mode
+
+        """
+        log.info(args)
+
+        log.info(arguments)
+
+
+        if arguments["flavors"]:
+            log.info ("list flavors")
+            return
+
+
+        if arguments["servers"]:
+            log.info ("list servers")
+            return
+
+
+        if arguments["images "]:
+            log.info ("list images s")
+            return
