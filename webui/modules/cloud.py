c91f0b6f51851468eaca2a799223afb4d90c1e9d
Allan Streib <astreib@indiana.edu>
2013-11-11 15:22:39 -0500

    adding guard condition in case cloud is not in user's clouds

--- webui/modules/cloud.py
+++ webui/modules/cloud.py
@@ -277,7 +277,7 @@ def manage_keypairs(cloud=None):
     # currently we do the registration only for openstack
     # not yet sure if other clouds support this
     # or if we have implemented them if they also support
-    if clouds.clouds[g.user.id][cloud]['cm_type'] in ['openstack', 'ec2', 'aws']:
+    if cloud in clouds.clouds[g.user.id] and clouds.clouds[g.user.id][cloud]['cm_type'] in ['openstack', 'ec2', 'aws']:
         if request.method == 'POST':
             action = request.form['action']
             keyname = request.form["keyname"]
