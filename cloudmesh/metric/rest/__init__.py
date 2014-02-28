98b9a2687beb7d19f154abb9e673a2e8894e4908
lee212 <hroe.lee@gmail.com>
2014-02-28 10:18:28 -0500

    adding basic functions to implement REST API for cm metrics

---
+++ cloudmesh/metric/rest/__init__.py
@@ -0,0 +1,3 @@
+import os, sys
+#sys.path.insert(0, os.path.abspath("../.."))
+sys.path.append("../..")
---
+++ cloudmesh/metric/rest/cm_mimerender.py
@@ -0,0 +1,15 @@
+from mimerender import FlaskMimeRender
+from flask import jsonify
+
+render_xml = lambda message: '<message>%s</message>' % str(message)
+render_json = lambda **args: jsonify(args)
+render_html = lambda message: '<html><body>%s</body></html>' % str(message)
+render_txt = lambda message: message
+
+mimerender = FlaskMimeRender()(
+        default = "json",
+        html = render_html,
+        xml  = render_xml,
+        json = render_json,
+        txt  = render_txt
+)
