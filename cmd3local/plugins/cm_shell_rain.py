874285fce354c7676282ba4f09d3f3687c3395b7
Gregor von Laszewski <laszewski@gmail.com>
2014-04-30 16:20:16 -0400

    adding draft for rain command

--- cmd3local/plugins/cm_shell_rain.py
+++ cmd3local/plugins/cm_shell_rain.py
@@ -20,41 +20,97 @@ class cm_shell_rain:
     @command
     def do_rain(self, args, arguments):
         """
+
         Usage:
-               rain info
-               rain list NAME [IMAGE list]
-               rain add HOSTLIST IMAGE [LABEL]
-               
-        Provisioning of the images on 
+            rain -h | --help
+            rain --version
+
+
+            rain admin add [LABEL] -f FILE
+            rain admin on HOSTS
+            rain admin off HOSTS
+            rain admin [-i] delete HOSTS
+            rain admin [-i] rm HOSTS
+
+            rain admin list users
+            rain admin list projects
+            rain admin list roles
+
+
+            rain admin list hosts  
+                         [--user=USERS|--project=PROJECTS|--role=ROLE]
+                         [--start=TIME_START]
+                         [--end=TIME_END|--duration=DURATION]
+                         [--format=(json|cfg)]
+
+
+            rain list hosts  
+                         [--start=TIME_START]
+                         [--end=TIME_END|--duration=DURATION]
+                         [--format=(json|cfg)]
+
+            rain list [-p PROJECTS] [HOSTS]
+            rain list display [--type=png] --file==<file>
+
+
+            rain status [--short|--summary] [HOSTS]
+
+
+            rain admin policy   
+                         [--user=USERS|--project=PROJECTS|--role=ROLE]
+                         (-l HOSTS|-n COUNT) 
+                         [--start=TIME_START]
+                         [--end=TIME_END|--duration=DURATION]
+
+
+
+
 
         Arguments:
 
-          NAME           The name of the server
-          IMAGE          The name of the image
-          HOSTLIST       The names of hosts
-          
+
+             HOSTS     the list of hosts passed
+             LABEL     the label of a host
+             FILE      file can be  specified as .json file or .cfg file. 
+                       The ending of the file determines the format.
+
+
+
         Options:
+             -n COUNT     count of teh bare metal hosts to be provisined
+             -p           all projects
+             -p PROJECTS  --projects=PROJECTS  the projects 
+             -u USERS     --user=USERS        Specify users
 
-           -v       verbose mode
 
-        Description:
+             -f FILE      --file=FILE  file to be specified
+             -i           interactive mode adds a yes/no 
+                          question for each host specified
+             --role=ROLE                Specify predefined role
+             --start=TIME_START        Start time of the reservation, in 
+                                    YYYY/MM/DD HH:MM:SS format. 
+                                    [default: current_time]
+             --end=TIME_END              End time of the reservation, in 
+                                    YYYY/MM/DD HH:MM:SS format, 
+             --duration=DURATION   ‘+[\d}[d|d]’ format. [default: +1d]
+             --type                Type of the output -png, jpg, pdf [default:png]
+             --file                Filename of the display output
+             --format=(json|cfg}  the format [default:json]
 
-          rain info
 
-               provides information about the images and servers on
-               which rain can be applied
-
-          rain list india01
-
-               list all images that can be provisioned on the server
-               with the name india01
-
-          rain add [india01-02] precise64.aaa precise64
-
-               adding the
-               
         """
+
+
+
+
+
         log.info(arguments)
+        log.info(args)
+
+        print "==========="
+        print arguments
+        print "==========="
+        print args
 
 
         if arguments["info"]:
---
+++ cmd3local/plugins/cm_shell_rain_old.py
@@ -0,0 +1,66 @@
+import types
+import textwrap
+from docopt import docopt
+import inspect
+import sys
+import importlib
+from cmd3.shell import command
+
+from cloudmesh.util.logger import LOGGER
+
+log = LOGGER(__file__)
+
+class cm_shell_rain_old:
+
+    """The command handler for rain_old"""
+
+    def activate_cm_shell_rain_old(self):
+        pass
+
+    @command
+    def do_rain_old(self, args, arguments):
+        """
+        Usage:
+               rain_old info
+               rain_old list NAME [IMAGE list]
+               rain_old add HOSTLIST IMAGE [LABEL]
+               
+        Provisioning of the images on 
+
+        Arguments:
+
+          NAME           The name of the server
+          IMAGE          The name of the image
+          HOSTLIST       The names of hosts
+          
+        Options:
+
+           -v       verbose mode
+
+        Description:
+
+          rain_old info
+
+               provides information about the images and servers on
+               which rain_old can be applied
+
+          rain_old list india01
+
+               list all images that can be provisioned on the server
+               with the name india01
+
+          rain_old add [india01-02] precise64.aaa precise64
+
+               adding the
+               
+        """
+        log.info(arguments)
+
+
+        if arguments["info"]:
+            log.info ("rain_old info")
+            return
+
+
+
+
