7c83e1c548c9fd9bf7b9425cec3a36893f0bc788
Gregor von Laszewski <laszewski@gmail.com>
2014-05-04 15:57:39 -0400

    forgotten files

---
+++ bin/install.py
@@ -0,0 +1,101 @@
+#! /usr/bin/env python
+"""
+Usage:
+    cm-install -h | --help
+    cm-install --version
+    cm-install create OS
+    cm-install list
+    cm-install veewee    
+    
+Arguments:
+    OS     The operating system. [default: ubuntu]
+    
+Options:
+
+Description:
+
+  start the install shell with
+
+     cm-install
+
+   cm-install> vagrant create ubuntu
+
+       creates an ubuntu 14.04 image with veewee and includes it in the base box list
+
+    cm-install> vagrant list
+
+        lists the base boxes in vagrant
+
+"""
+from docopt import docopt
+import hostlist
+from datetime import datetime, timedelta
+from pytimeparse.timeparse import timeparse
+from datetime import datetime, timedelta            
+import os
+import sys
+
+try:
+    from sh import ls
+except:
+    os.system ("pip install sh")
+
+from sh import vagrant
+    
+
+def not_implemented():
+    print "ERROR: not yet implemented"
+
+def get_boxes(kind):
+    lines = vagrant("box","list")
+
+    boxes = []
+    for line in lines:
+        (OS, KIND) = line.split("(")
+        OS = OS.strip()
+        (KIND, rest) = KIND.split(")")
+        if kind == KIND:
+            boxes.append(OS)
+    return boxes
+
+ 
+def list_boxes():
+    #print vagrant("box","list")
+    print get_boxes("virtualbox")
+
+def exec_script(name):
+    if name is "veewee":
+        os.system("./bin/install-veewee.sh")
+    elif name is "ubuntu64":
+        os.system("./bin/install-ubuntu64.sh")
+        
+def cm_install_command(arguments):
+
+    print arguments
+    
+    if arguments["create"]:
+        print "cm-install create"
+        operating_system = arguments['OS']
+        boxes = get_boxes("virtualbox")
+        if operating_system not in boxes:
+            print "The OS '%s' was not found in vagrant" % (operating_system)
+            exec_script('./bin/install-ubuntu64.sh')
+        else:
+            print "The OS '%s' was found in vagrant" % (operating_system)
+            exec_script('ubuntu64')
+        
+    elif arguments["veewee"]:
+        exec_script('veewee')
+        
+    elif arguments["list"]:
+        """cm-install vagrant list"""
+        print "list"
+        list_boxes()
+        
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+
+    cm_install_command(arguments)
+    
+
+    
