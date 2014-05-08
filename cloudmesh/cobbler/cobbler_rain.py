a7e7f4e6c2f5424c8d82d265c627681b4d06a37a
Gregor von Laszewski <laszewski@gmail.com>
2014-05-08 12:18:27 -0400

    adding the function registration for the rain command

--- cloudmesh/cobbler/cobbler_rain.py
+++ cloudmesh/cobbler/cobbler_rain.py
@@ -1,60 +1,4 @@
 #! /usr/bin/env python
-"""
-Usage:
-    rain -h | --help
-    rain --version
-    rain admin add [LABEL] --file=FILE
-    rain admin on HOSTS
-    rain admin off HOSTS
-    rain admin [-i] delete HOSTS
-    rain admin [-i] rm HOSTS
-    rain admin list users
-    rain admin list projects
-    rain admin list roles
-    rain admin list hosts [--user=USERS|--project=PROJECTS|--role=ROLE]
-                          [--start=TIME_START]
-                          [--end=TIME_END]
-                          [--format=FORMAT]
-    rain admin policy [--user=USERS|--project=PROJECTS|--role=ROLE]
-                      (-l HOSTS|-n COUNT)
-                      [--start=TIME_START]
-                      [--end=TIME_END]
-    rain list [--project=PROJECTS] [HOSTS]    
-    rain list hosts [--start=TIME_START]
-                    [--end=TIME_END]
-                    [--format=FORMAT]
-    rain status [--short|--summary][--kind=KIND] [HOSTS]
-    rain provision --profile=PROFILE HOSTS
-    rain provision list (--distro=DISTRO|--kickstart=KICKSTART)
-    rain provision --distro=DITRO --kickstart=KICKSTART HOSTS
-    rain provision add (--distro=URL|--kickstart=KICk_CONTENT) NAME
-    
-Arguments:
-    HOSTS     the list of hosts passed
-    LABEL     the label of a host
-    COUNT     the count of the bare metal provisioned hosts
-    KIND      the kind
-    
-Options:
-    -n COUNT     count of teh bare metal hosts to be provisined
-    -p PROJECTS  --projects=PROJECTS  
-    -u USERS     --user=USERS        Specify users
-    -f FILE, --file=FILE  file to be specified
-    -i           interactive mode adds a yes/no 
-                 question for each host specified
-    --role=ROLE            Specify predefined role
-    --start=TIME_START     Start time of the reservation, in 
-                           YYYY/MM/DD HH:MM:SS format. [default: current_time]
-    --end=TIME_END         End time of the reservation, in 
-                           YYYY/MM/DD HH:MM:SS format. In addition a duration
-                           can be specified if the + sign is the first sign.
-                           The duration will than be added to
-                           the start time. [default: +1d]
-    --kind=KIND            Format of the output -png, jpg, pdf. [default:png]
-    --format=FORMAT        Format of the output json, cfg. [default:json]
-
-
-"""
 #from __future__ import dict
 from docopt import docopt
 import hostlist
@@ -64,29 +8,66 @@ from pytimeparse.timeparse import timeparse
 # from timestring import Date
 from datetime import datetime, timedelta            
 import sys
+from cloudmesh.util.util import parse_time_interval, not_implemented
 
-def not_implemented():
-    print "ERROR: not yet implemented"
-
-def parse_time_interval (time_start, time_end):
-    t_end = time_end
-    t_start = time_start
-
-    if t_start is not None:
-        if t_start in ["current_time","now"]:
-            t_start = str(datetime.now())
-
-    if t_end is not None:
-        if t_end.startswith("+"):
-            duration = t_end[1:]
-            delta = timeparse(duration)
-            t_start = datetime.strptime(t_start, "%Y-%m-%d %H:%M:%S.%f")
-            t_end = t_start + timedelta(seconds=delta)
-
-    return (str(t_start), str(t_end))
-    
 def rain_command(arguments):
+    """
+    Usage:
+        rain -h | --help
+        rain --version
+        rain admin add [LABEL] --file=FILE
+        rain admin on HOSTS
+        rain admin off HOSTS
+        rain admin [-i] delete HOSTS
+        rain admin [-i] rm HOSTS
+        rain admin list users
+        rain admin list projects
+        rain admin list roles
+        rain admin list hosts [--user=USERS|--project=PROJECTS|--role=ROLE]
+                              [--start=TIME_START]
+                              [--end=TIME_END]
+                              [--format=FORMAT]
+        rain admin policy [--user=USERS|--project=PROJECTS|--role=ROLE]
+                          (-l HOSTS|-n COUNT)
+                          [--start=TIME_START]
+                          [--end=TIME_END]
+        rain list [--project=PROJECTS] [HOSTS]    
+        rain list hosts [--start=TIME_START]
+                        [--end=TIME_END]
+                        [--format=FORMAT]
+        rain status [--short|--summary][--kind=KIND] [HOSTS]
+        rain provision --profile=PROFILE HOSTS
+        rain provision list (--distro=DISTRO|--kickstart=KICKSTART)
+        rain provision --distro=DITRO --kickstart=KICKSTART HOSTS
+        rain provision add (--distro=URL|--kickstart=KICk_CONTENT) NAME
 
+    Arguments:
+        HOSTS     the list of hosts passed
+        LABEL     the label of a host
+        COUNT     the count of the bare metal provisioned hosts
+        KIND      the kind
+
+    Options:
+        -n COUNT     count of teh bare metal hosts to be provisined
+        -p PROJECTS  --projects=PROJECTS  
+        -u USERS     --user=USERS        Specify users
+        -f FILE, --file=FILE  file to be specified
+        -i           interactive mode adds a yes/no 
+                     question for each host specified
+        --role=ROLE            Specify predefined role
+        --start=TIME_START     Start time of the reservation, in 
+                               YYYY/MM/DD HH:MM:SS format. [default: current_time]
+        --end=TIME_END         End time of the reservation, in 
+                               YYYY/MM/DD HH:MM:SS format. In addition a duration
+                               can be specified if the + sign is the first sign.
+                               The duration will than be added to
+                               the start time. [default: +1d]
+        --kind=KIND            Format of the output -png, jpg, pdf. [default:png]
+        --format=FORMAT        Format of the output json, cfg. [default:json]
+
+
+    """
+    arguments = docopt(self.__doc__)
     for list in ["HOSTS", "USERS", "PROJECTS","--project", "--user"]:
         try:
             expanded_list = hostlist.expand_hostlist(arguments[list])
@@ -250,8 +231,6 @@ def rain_command(arguments):
                     print "now provision host: %s" % host
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__)
-
     rain_command(arguments)
     
     """
