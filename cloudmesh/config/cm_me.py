6e680854e8411ada7c9d9207f1d7dc12ef52cd45
Gregor von Laszewski <laszewski@gmail.com>
2014-05-09 11:32:28 -0400

    adding the new init commeand with a function_command

---
+++ cloudmesh/config/cm_me.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from jinja2 import Template
+import sys
+
+def command_me (arguments):
+    
+    t = Template("Hello {{ something }}!")
+    result = t.render(something="World")
+    print result
+    
+if __name__ == "__main__":
+    command_me(sys.argv)
--- cmd3local/plugins/cm_shell_init.py
+++ cmd3local/plugins/cm_shell_init.py
@@ -6,6 +6,7 @@ import sys
 import importlib
 from cmd3.shell import command
 from cloudmesh.util.util import path_expand
+from cloudmesh.config.cm_init import init_command
 
 from cloudmesh.user.cm_template import cm_template
 from cloudmesh.util.util import yn_choice
@@ -24,6 +25,17 @@ class cm_shell_init:
     def activate_cm_shell_init(self):
         pass
 
+
+    @function_command(init_command)
+    def do_init(self, args, arguments):
+        log.info(arguments)
+        log.info(args)
+
+        init_command(arguments)
+        pass
+
+    
+'''
     @command
     def do_init(self, args, arguments):
         """
@@ -96,7 +108,7 @@ class cm_shell_init:
                 os.system ("mv {0} {1}".format(new_yaml, old_yaml))
 
             return
-
+'''
 
 
 
