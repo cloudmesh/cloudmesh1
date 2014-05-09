6f653a0e979bfc049de34b8d8d8afbbb835192cf
Gregor von Laszewski <laszewski@gmail.com>
2014-05-09 14:35:27 -0400

    fixing the cm-metric command template

--- cloudmesh/metric/cm_metric.py
+++ cloudmesh/metric/cm_metric.py
@@ -15,16 +15,13 @@ from prettytable import PrettyTable
 log = LOGGER(__file__)
 
 def cm_metric_command(arguments):
-
     """
     Usage:
-
         metric [CLOUD]
         metric [-s START] [-e END] [-u USER] [-metric (user|vm|runtime)]
-        [-period (month|day|week)] [-c CLUSTER]
+               [-period (month|day|week)] [-c CLUSTER]
 
     Arguments:
-
         CLOUD               Name of the IaaS cloud e.g. openstack, nimbus, Eucalyptus
         START               First day of filter
         END                 Last day of filter
@@ -35,51 +32,45 @@ def cm_metric_command(arguments):
         hotel, alamo, lima
 
     Options:
-
        -h                   help message
 
     Description:
-
        metric command provides usage data with filter options.
 
     Result:
-
       The result of the method is a datastructure specified in a given format.
       If no format is specified, we return a JSON string of the following format:
 
-      Basic data structure
-      --------------------
-
          {
-         "start_date"    :   start date of search    (datetime),
-         "end_date"      :   end date of search      (datetime),
-         "ownerid"       :   portal user id          (str),
-         "metric"        :   selected metric name    (str),
-         "period"        :   monthly, weekly, daily  (str),
-         "clouds"        :   set of clouds           (list)
-         [
-         { "service"     :   cloud service name  (str),
-         "hostname"     :   hostname (str),
-         "stats"        :   value (int) }
-         ...
-         ]
+            "start_date"    :   start date of search    (datetime),
+            "end_date"      :   end date of search      (datetime),
+            "ownerid"       :   portal user id          (str),
+            "metric"        :   selected metric name    (str),
+            "period"        :   monthly, weekly, daily  (str),
+            "clouds"        :   set of clouds           (list)
+            [
+               {"service"     :   cloud service name  (str),
+                "hostname"     :   hostname (str),
+                "stats"        :   value (int) }
+                ...
+            ]
          }
 
-    Example :
+    Examples:
 
-        Get user statistics
-        cm> $ metric openstack -c india -u hrlee        
+        metric openstack -c india -u hrlee        
+            Get user statistics
 
+        
     """
-    mesh = cloudmesh.mesh()
-    mongo = cm_mongo()
-    config = cm_config()
-    user = config.username()
-    dbDict = self.mongoClass.db_defaults.find_one({'cm_user_id': user})
+    #mesh = cloudmesh.mesh()
+    #mongo = cm_mongo()
+    #config = cm_config()
+    #user = config.username()
+    #dbDict = self.mongoClass.db_defaults.find_one({'cm_user_id': user})
 
-    print args, arguments
+    print arguments
     log.info(arguments)
-    log.info(args)
 
     
 if __name__ == "__main__":
@@ -90,4 +81,5 @@ if __name__ == "__main__":
 #    ls = cm_shell_metric()
 #    ls._printList('image', fieldList, clouds)
 #
-    cm_metric_command(sys.argv)
+    arguments = docopt(cm_metric_command.__doc__)
+    cm_metric_command(arguments)
