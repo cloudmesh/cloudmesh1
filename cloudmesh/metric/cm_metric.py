dba4d62cfc281b538c4f24f14c99a9b10518c172
Hyungro Lee <hroe.lee@gmail.com>
2014-05-12 15:58:04 -0400

    working on metric_command in metric. parameters are working correctly based on docstring

--- cloudmesh/metric/cm_metric.py
+++ cloudmesh/metric/cm_metric.py
@@ -14,26 +14,32 @@ from prettytable import PrettyTable
 
 log = LOGGER(__file__)
 
-def cm_metric_command(arguments):
+def metric_command(arguments):
     """
     Usage:
-        metric [CLOUD]
-        metric [-s START] [-e END] [-u USER] [-metric (user|vm|runtime)]
-               [-period (month|day|week)] [-c CLUSTER]
+	cm-metric -h | --help
+        cm-metric --version
+        cm-metric [CLOUD] [-s START] 
+                  [-e END] 
+                  [-u USER] 
+                  [-m|--metric=METRIC]
+                  [-p|--period=PERIOD] 
+                  [-c CLUSTER]
 
+   Options:
+       -h                   help message
+       -m, --metric METRIC  use either user|vm|runtime in METRIC
+       -u, --user USER      use username in USER
+       -s, --start START    use YYYYMMDD datetime in START
+       -e, --end END        use YYYYMMDD datetime in END
+       -c, --cluster CLUSTER    use cluster name e.g. india, sierra, etc
+       -p, --period PERIOD  use either month|day|week
+ 
     Arguments:
         CLOUD               Name of the IaaS cloud e.g. openstack, nimbus, Eucalyptus
-        START               First day of filter
-        END                 Last day of filter
-        USER                portal user id to filter
-        (user|vm|runtime)   Metric to view
-        (month|day|week)    Time period to view
         CLUSTER             Name of cluster e.g. india, sierra, foxtrot,
-        hotel, alamo, lima
-
-    Options:
-       -h                   help message
-
+                            hotel, alamo, lima
+    
     Description:
        metric command provides usage data with filter options.
 
@@ -57,32 +63,78 @@ def cm_metric_command(arguments):
          }
 
     Examples:
-
-        metric openstack -c india -u hrlee        
-            Get user statistics
-
+        $ cm-metric openstack -c india -u hrlee        
+        - Get user statistics
         
     """
-    #mesh = cloudmesh.mesh()
-    #mongo = cm_mongo()
-    #config = cm_config()
-    #user = config.username()
-    #dbDict = self.mongoClass.db_defaults.find_one({'cm_user_id': user})
 
-    print arguments
+    #print arguments
     log.info(arguments)
+    print "starts metric"
+
+    # stage 1
+    # ----------
+    # (make sure) all data is in database
+    # it is not real-time
+    # create api to update data into the database
+    # otherwise, cached metrics data will be loaded from the database
+
+    # stage 2
+    # ----------
+    # db access
+    # select data with search options
+    # return in table
+    m = cm_metric()
+    m.set_date(arguments["--start"], arguments["--end"])
+    m.set_period(arguments["--period"])
+    m.set_metric(arguments["--metric"])
+    m.set_user(arguments["--user"])
+    res = m.get_stats()
+
+class cm_metric:
+    from_date = None
+    to_date = None
+    period = None
+    metric = None
+    cluster = None
+    iaas = None
+    user = None
+
+    def set_date(self, from_date, to_date):
+        self.from_date = from_date
+        self.to_date = to_date
+
+    def set_period(self, period):
+        self.period = period
+
+    def set_metric(self, metric):
+        self.metric = metric
+
+    def set_cluster(self, cluster):
+        self.cluster = cluster
+
+    def set_iaas(self, cloud):
+        self.iaas = cloud
+
+    def set_cloud(self, cloud):
+        ''' link to set_iaas '''
+        self.set_iaas(cloud)
+
+    def set_user(self, user):
+        self.user = user
+
+    def get_stats(self):
+        print "get_stats called"
+        print vars(self)
+        return
+
+    def stats(self):
+        ''' link to get_stats '''
+        return self.get_stats()
 
 def main():
-    arguments = docopt(cm_metric_command.__doc__)
-    cm_metric_command(arguments)
-
+    arguments = docopt(metric_command.__doc__)
+    metric_command(arguments)
         
 if __name__ == "__main__":
-#    print "test correct"
-#    clouds = ['sierra_openstack_grizzly']
-#    fieldList = ["id", "name", "status"]
-#
-#    ls = cm_shell_metric()
-#    ls._printList('image', fieldList, clouds)
-#
     main()
