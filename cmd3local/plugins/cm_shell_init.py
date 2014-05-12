e89faf39d7103ba5d404c1934ba3db39bdcbc706
Gregor von Laszewski <laszewski@gmail.com>
2014-05-12 09:51:59 -0400

    removed redundance between init and user command

--- cmd3local/plugins/cm_shell_init.py
+++ cmd3local/plugins/cm_shell_init.py
@@ -1,28 +1,7 @@
-import types
-import textwrap
-from docopt import docopt
-import inspect
-import sys
-import importlib
 from cmd3.shell import function_command
-from cloudmesh.util.util import path_expand
 from cloudmesh.config.cm_init import init_shell_command
 
-from cloudmesh.user.cm_template import cm_template
-from cloudmesh.util.util import yn_choice
-from sh import less
-import os
-
-
-from cloudmesh.util.logger import LOGGER
-
-log = LOGGER(__file__)
-
-
 class cm_shell_init:
-
-    """opt_example class"""
-
     
     def activate_cm_shell_init(self):
         self.register_command_topic('cloud','init')
