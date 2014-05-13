1c9e25cfaa62c467a15b8e0cb11d7075e38d86f6
Hyungro Lee <hroe.lee@gmail.com>
2014-05-12 21:48:49 -0400

    function names have been changed to be called in cm

--- cmd3local/plugins/cm_shell_metric.py
+++ cmd3local/plugins/cm_shell_metric.py
@@ -1,8 +1,5 @@
 from cmd3.shell import function_command
-from cloudmesh.metric.cm_metric import metric_command
-from cloudmesh.util.logger import LOGGER
-
-log = LOGGER(__file__)
+from cloudmesh.metric.cm_metric import shell_command_metric
 
 class cm_shell_metric:
 
@@ -10,13 +7,11 @@ class cm_shell_metric:
 
     def activate_cm_shell_metric(self):
         self.register_command_topic('cloud','metric')
-        pass
 
-    @function_command(cm_metric_command)
+    @function_command(shell_command_metric)
     def do_metric(self, args, arguments):
-        log.info(arguments)
-        log.info(args)
+        #log.info(arguments)
+        #log.info(args)
 	
 	# args is not needed?
-        metric_command(arguments)
-        pass
+        shell_command_metric(arguments)
