9261ac595b683492cae362f0dbef417e88a70850
Gregor von Laszewski <laszewski@gmail.com>
2014-05-13 05:16:22 -0400

    metric command, storm, user, cobbler doc

--- cloudmesh/metric/metric-server.py
+++ cloudmesh/metric/metric-server.py
@@ -1,4 +1,14 @@
+
 from flask import Flask, url_for
+from docopt import docopt
+#from cloudmesh.metric.cm_metric import shell_command_metric
+from cm_metric import shell_command_metric
+import sh
+from cloudmesh.util.logger import LOGGER
+
+log = LOGGER(__file__)
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,31 +18,48 @@ def get_index_page():
 
 @app.route('/metric/cloud/<cloudname>/<username>/<metric>/<timestart>/<timeend>/<period>')
 def get_metric_for_cloud(cloudname,username,metric,timestart,timeend,period):
-    print "TEST"
-    arguments = {
-        'cloud': cloudname,
-        'user': username,
-        'metric': metric,
-        'time_start': timestart,
-        'time_end': timeend,
-        'period': period
-    }
-    print arguments
-    return str(arguments)
+    log.info("/metric/cloud/...")
+
+    arguments = [
+        cloudname,
+        "-s", timestart,
+        "-e", timeend,
+        "-u", username,
+        "-m", metric,
+        "-p", period
+    ]
+    log.info(arguments)
+    command = sh.Command("cm-metric")
+    result = command(arguments)
+    log.info(result)
+    print 70*"="
+    print result
+    print 70*"="    
+
+    return str(result)
 
 @app.route('/metric/cluster/<clustername>/<username>/<metric>/<timestart>/<timeend>/<period>')
 def get_metric_for_cluster(clustername,username,metric,timestart,timeend,period):
-    print "TEST"
-    arguments = {
-        'cloud': clustername,
-        'user': username,
-        'metric': metric,
-        'time_start': timestart,
-        'time_end': timeend,
-        'period': period
-    }
-    print arguments
-    return str(arguments)
+    log.info("/metric/cluster/...")
+
+    arguments = [
+        "-c", clustername,
+        "-s", timestart,
+        "-e", timeend,
+        "-u", username,
+        "-m", metric,
+        "-p", period
+    ]
+    log.info(arguments)
+    command = sh.Command("cm-metric")
+    result = command(arguments)
+    log.info(result)
+    print 70*"="
+    print result
+    print 70*"="    
+
+    return str(result)
+
 
 """
 with app.test_request_context():
---
+++ cloudmesh/shell/cm_user.py
@@ -0,0 +1,78 @@
+import types
+import textwrap
+from docopt import docopt
+import inspect
+import sys
+import importlib
+from cmd3.shell import command
+from cloudmesh.util.util import path_expand
+from cloudmesh.util.util import banner
+from cloudmesh.user.cm_user import cm_user
+from cloudmesh.config.cm_config import cm_config
+from cloudmesh.user.cm_template import cm_template
+from cloudmesh.util.util import yn_choice
+from cloudmesh.config.ConfigDict import ConfigDict
+from sh import less
+import os
+from pprint import pprint
+import yaml
+import json
+import ast
+
+from cloudmesh.util.logger import LOGGER
+
+log = LOGGER(__file__)
+
+def shell_command_user(arguments):
+    """
+    Usage:
+           user list
+           user info [ID]
+
+    Administrative command to lists the users from LDAP
+
+    Arguments:
+
+      list       list the users
+      ID         list the user with the given ID
+
+    Options:
+
+       -v       verbose mode
+
+    """
+
+    user = cm_user()
+
+    if (arguments["info"]):
+        
+        id = arguments["ID"]
+        if id is None:
+            config = cm_config()
+            id = config.username()
+        banner("User Information in Mongo for user: {0}".format(id))
+        user = cm_user()
+        result = user.info(id)
+        pprint (result)
+
+    elif (arguments["list"]):
+
+        user = cm_user()
+        list_of_users = user.list_users()
+        pprint(list_of_users)
+        print
+        print "========================="
+        num = len(list_of_users)
+        print str(num) + " users listed"
+
+    else:
+        print "WRONG PARAMETERS"
+
+    return
+
+def main():
+    arguments = docopt(shell_command_user.__doc__)
+    shell_command_user(arguments)
+
+if __name__ == '__main__':
+    main()
