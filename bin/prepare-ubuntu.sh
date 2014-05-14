4c9a06c60826dfec7084b70b695d4cb268bb6562
Gregor von Laszewski <laszewski@gmail.com>
2014-05-14 18:54:59 -0400

    adding the requirements

--- bin/prepare-ubuntu.sh
+++ bin/prepare-ubuntu.sh
@@ -1,13 +1,13 @@
 echo "# #####################################################################"
 echo "# preparing Ubuntu for cloudmesh" 
 echo "# ######################################################################"
-sudo apt-get install python-dev libldap2-dev libsasl2-dev libssl-dev
+sudo apt-get -y install python-dev libldap2-dev libsasl2-dev libssl-dev
 #sudo apt-getinstall  libnss-ldap libpam-ldap libpam-mount  winbind smbclient cifs-utils ldap-utils     
 
-sudo apt-get install emacs24
-sudo apt-get install git
-sudo apt-get install python-setuptools 
-sudo apt-get install python-dev
-sudo apt-get install python-virtualenv
+sudo apt-get -y install emacs24
+sudo apt-get -y install git
+sudo apt-get -y install python-setuptools 
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-virtualenv
 
 
