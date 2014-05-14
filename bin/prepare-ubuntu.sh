6422380a8a6fa79be31f61bbfebbc348f5096ff3
Allan Streib <astreib@indiana.edu>
2014-05-14 16:52:06 -0400

    python-virtualenv can be installed from apt-get; this also provides pip

--- bin/prepare-ubuntu.sh
+++ bin/prepare-ubuntu.sh
@@ -6,7 +6,6 @@ sudo apt-get install emacs24
 sudo apt-get install git
 sudo apt-get install python-setuptools 
 sudo apt-get install python-dev
-sudo easy_install pip
-sudo pip install virtualenv
+sudo apt-get install python-virtualenv
 
 
