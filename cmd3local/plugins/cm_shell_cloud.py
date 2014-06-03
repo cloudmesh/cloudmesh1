434962fcfe57155ec5ac182700f9d5064e4f3475
Gregor von Laszewski <laszewski@gmail.com>
2014-06-03 15:37:36 -0400

    adding the cloud list command

--- cmd3local/plugins/cm_shell_cloud.py
+++ cmd3local/plugins/cm_shell_cloud.py
@@ -5,8 +5,12 @@ from docopt import docopt
 import inspect
 import sys
 import importlib
+from cloudmesh.cm_mongo import cm_mongo
 from cmd3.shell import command
-
+from cloudmesh_common.tables import column_table
+from pprint import pprint
+import yaml
+import json
 
 log = LOGGER(__file__)
 
@@ -16,37 +20,58 @@ class cm_shell_cloud:
     """opt_example class"""
 
     def activate_cm_shell_cloud(self):
+        self.mongo_loaded = False
         self.register_command_topic('cloud','cloud')
         pass
 
+    def _load_mongo(self):
+        if not self.mongo_loaded:
+            try:
+                self.mongoClass = cm_mongo()
+                self.mongo_loaded = True
+            except:
+                print "ERROR: could not access Mongodb. " \
+                      "Have you started the mongo server?"
+
+
+    def _print_dict(self, d, format):
+        if format in ["table"]:
+            print column_table(d)
+        elif format in ["list"]:
+            print d["Clouds"]
+        elif format in ["dict"]:
+            print yaml.safe_dump(d, indent=4,default_flow_style=False)
+        elif format in ["json"]:
+            print json.dumps(d, indent=4)
+
+                     
     @command
     def do_cloud(self, args, arguments):
         """
         Usage:
-            cloud NOTIMPLEMENTED list
-            cloud NOTIMPLEMENTED info [NAME|all]
-            cloud NOTIMPLEMENTED NAME
-            cloud NOTIMPLEMENTED select
-            cloud NOTIMPLEMENTED --on | --off NAME
-            cloud NOTIMPLEMENTED on NAME
-            cloud NOTIMPLEMENTED off NAME
-            cloud NOTIMPLEMENTED add [--format=FORMAT] CLOUD
+            cloud list [--format=FORMAT]
+            cloud info [NAME]
+            cloud set NAME
+            cloud select
+            cloud on NAME
+            cloud off NAME
+            cloud add [--json | --yaml] CLOUD
 
         Manages the clouds
 
         Arguments:
 
           NAME           The name of a service or server
-          JSON           A JSON
           CLOUD          The cloud to be added
 
         Options:
 
            -v       verbose mode
-           --on     Activate the cloud
-           --off    Deactivate the cloud
-           --format=FORMAT  The format of the activation description.
-                            [default: yaml]
+           --json   The format of the activation description:jsos
+           --yaml   The format of the activation description:yaml
+           --format=FORMAT   the format of the input or output.
+                             Accepted values are table, list, json, dict. [default: table]
+                          
         Description:
 
             cloud list
@@ -58,40 +83,94 @@ class cm_shell_cloud:
                 If no name is specified the default cloud is used.
                 If the name all is used, all clouds are displayed
 
-            cloud NAME
-                setst the cloud with the name to the default
+            cloud setname NAME
+                set the cloud with the NAME to the default cloud
 
             cloud select
                 selects a cloud from the name of clouds
 
-            cloud --on | --off NAME
-            cloud on NAME
-            cloud off NAME
-                activates or deactivates a cloud with a given name
+            cloud on [NAME]
+            cloud off [NAME]
+                activates or deactivates a cloud with a given name, if name
+                is not specified, default or selected cloud will be activates
+                or deactivates
 
-            cloud add [--format=FORMAT] CLOUD
+            cloud add [--json | --yaml] CLOUD
                 adds a cloud to the list of clouds.
-                The format can either be `json` or `yaml`.
+                The format can either be `json` or `yaml`, default is yaml
+
         """
+
         log.info(arguments)
         print "<", args, ">"
 
-        if arguments["set"] or args is None:
-            log.info("set the cloud")
+        if arguments["list"]:
+            #list the cloud names
+            #
+            # Bug, this does not list all possible clouds, but just the clouds register in the passwd database
+            # it should probably come form cm_mongo().clouds() We skip this for now
+            #
+            if arguments["--format"] not in ["json","table", "dict", "list"]:
+                print "ERROR: format {0} not supported".format(arguments["--format"])
+            else:
+            
+                if not self.mongo_loaded:
+                    self._load_mongo()
+                result = self.mongoClass.userdb_passwd.find().sort("cloud")
+                cloudnames = {"Clouds":[]}
+                for result_object in result:
+                    cloudnames["Clouds"].append(result_object["cloud"])
+
+                self._print_dict(cloudnames, arguments["--format"])
             return
 
-        if arguments["--on"] and arguments["NAME"]:
+
+        if arguments["select"]:
+            #select a cloud from a list
+            pass
+            
+        if arguments["info"]:
+            if argument["NAME"]:
+                if argument["NAME"] == "all":
+                    print "---------------------->"
+                    #display all clouds
+                else:
+                    print "---------------------->"
+                    #provide information about cloud "NAME"
+            else:
+                #display default cloud
+                pass
+            return
+
+
+        if arguments["set"] and argument["NAME"]:
+
+            #set the cloud with the name to the default
+            return
+
+        if arguments["on"] and arguments["NAME"]:
+            #activate cloud NAME
             log.info("activatethe cloud")
             return
 
-        if arguments["--off"] and arguments["NAME"]:
+        if arguments["off"] and arguments["NAME"]:
+            #deactivate cloud NAME
             log.info("activatethe cloud")
             return
 
-        if arguments["info"] and arguments["NAME"]:
-            log.info("cloud info")
+        if arguments["add"] and arguments["CLOUD"]:
+            if arguments["--jason"]:
+                # adds a cloud to the list of clouds in jason format
+                pass
+            elif arguments["--yaml"]:
+                # adds a cloud to the list of clouds in yaml format
+                pass
+            else:
+                # adds a cloud to the list of clouds in yaml format
+                pass
             return
+ 
 
-        if arguments["create"] and arguments["NAME"]:
-            log.info("cloud info")
-            return
+
+if __name__ == '__main__':
+	pass
