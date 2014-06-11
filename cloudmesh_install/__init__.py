81b546c05f598d39acd7753a0231dece1aea6729
Gregor von Laszewski <laszewski@gmail.com>
2014-06-11 09:53:07 -0400

    fixing many little errors

--- cloudmesh_install/__init__.py
+++ cloudmesh_install/__init__.py
@@ -1,3 +1,4 @@
+"""Common methods and classes used at the intall time."""
 import sys
 sys.path.append("..")
 
@@ -5,5 +6,6 @@ from cloudmesh_common.bootstrap_util import path_expand
 
 __config_dir__ = path_expand("~/.futuregrid")
 
+
 def config_file(filename):
-    return __config_dir__ + filename 
+    return __config_dir__ + filename
