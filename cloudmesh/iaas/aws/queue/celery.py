7261b3ada209a485474b347f421203811a38d550
Hyungro Lee <hroe.lee@gmail.com>
2014-06-12 14:51:49 -0400

    refreshing flavors is now done by Celery task queue. For example, http://[ipaddress:port]/cm/refresh/q/aws/images calls refreshing images of aws which has 29282 registered images. The original refresh function of aws images takes 42 seconds of listing images, 8.8 seconds of storing them to mongo db which is the total runtime is about 50 seconds. To apply refreshing images by Celery Task Queue is simply adding '/q/' in the url after './refresh/' in template pages.

---
+++ cloudmesh/iaas/aws/queue/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from celery import Celery
+
+# Celery app for azure cloud
+celery = Celery('cloudmesh.iaas.aws.queue',
+                broker='amqp://guest@localhost',
+                backend='amqp',#mongodb://guest:guest@localhost:27017/jobs')#,
+                include=['cloudmesh.iaas.aws.queue.tasks'])
+
+# Optional configuration, see the application user guide.
+celery.conf.update(
+    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_DISABLE_RATE_LIMITS=True,
+    CELERY_IGNORE_RESULT=False,
+    CELERY_RESULT_PERSISTENT=True,
+    CELERY_ENABLE_UTC = True,
+    CELERY_TIMEZONE = 'US/Eastern'
+    )
+
+if __name__ == '__main__':
+    celery.start()
+
--- cloudmesh/iaas/azure/queue/tasks.py
+++ cloudmesh/iaas/azure/queue/tasks.py
@@ -3,6 +3,7 @@ from celery import current_task
 from celery.utils.log import get_task_logger
 from cloudmesh.iaas.azure.queue.celery import celery
 from cloudmesh.iaas.azure.cm_compute import azure as windows_azure
+from cloudmesh.cm_mongo import cm_mongo
 #
 #logger = get_task_logger(__name__)
 #
@@ -20,3 +21,16 @@ def vm_create(name, flavor_name, image_id, security_groups=None, key_name=None,
     obj = windows_azure()
     obj.vm_create(name, flavor_name, image_id ,security_groups, key_name, meta,
                   userdata)
+
+
+@celery.task(track_started=True)
+def refresh(cm_user_id=None, names=None, types=None):
+
+    if isinstance(names, str):
+        names = [names]
+    if isinstance(types, str):
+        types = [types]
+
+    clouds = cm_mongo()
+    clouds.activate(cm_user_id=cm_user_id)
+    clouds.refresh(cm_user_id=cm_user_id, names=names, types=types)
