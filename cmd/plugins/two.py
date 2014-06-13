27a06a16690ed17681e45b7255e972217cae4a12
Gregor von Laszewski <laszewski@gmail.com>
2014-06-13 10:57:15 -0400

    a very simple CMD shell with plugin mechanism

---
+++ cmd/plugins/two.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+
+from cmd import Cmd
+
+class CommandTwo(Cmd):
+
+    def do_two(self, args):
+        """Says hello. If you provide a name, it will greet you with it."""
+        if len(args) == 0:
+            name = 'Two'
+        else:
+            name = args
+        print "Hello, %s" % name
+
+if __name__ == '__main__':
+    prompt = CommandOne()
+    prompt.prompt = '> '
+    prompt.cmdloop('Starting prompt...')        
---
+++ cmd/shell.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+from cmd import Cmd
+from mixin import MixIn, makeWithMixins, makeWithMixinsFromString
+from plugins import *
+from plugins import get_plugins
+
+class Shell(Cmd):
+
+    def do_hello(self, args):
+        """Says hello. If you provide a name, it will greet you with it."""
+        if len(args) == 0:
+            name = 'stranger'
+        else:
+            name = args
+        print "Hello, %s" % name
+
+    def do_quit(self, args):
+        """Quits the program."""
+
+if __name__ == '__main__':
+
+    plugins = get_plugins()
+    SuperShell = makeWithMixins(Shell, plugins)
+    shell = SuperShell()
+    shell.prompt = '> '
+    shell.cmdloop('Cloudmesh Shell...')        
