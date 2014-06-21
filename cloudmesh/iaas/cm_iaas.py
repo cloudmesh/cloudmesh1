123dce70e8ecea1939db225114cb8d7c68b210d9
Hyungro Lee <hroe.lee@gmail.com>
2014-06-21 11:22:45 -0400

    adding images command

--- cloudmesh/iaas/cm_iaas.py
+++ cloudmesh/iaas/cm_iaas.py
@@ -56,7 +56,7 @@ def shell_command_flavor(arguments):
         'cm_refresh',
     ]
 
-    flavors = _select_elements(flavors_dict, your_keys)
+    flavors = _select_flavors(flavors_dict, your_keys)
 
     _display(flavors)
     
@@ -70,7 +70,7 @@ def _display(json_data, headers="firstrow", tablefmt="orgtbl"):
     print table
     print separator
 
-def _select_elements(data, selected_keys, env=[]):
+def _select_flavors(data, selected_keys, env=[]):
     '''
     {"india_openstack_havana": {"1": 
         {"cm_cloud_type": "openstack", 
@@ -104,6 +104,151 @@ def _select_elements(data, selected_keys, env=[]):
                     values.append(0)
             flavors.append(values)
     return flavors
+
+def shell_command_image(arguments):
+    """
+    Usage:
+        image 
+        image <cm_cloud>...
+	image -h | --help
+        image --version
+
+   Options:
+       -h                   help message
+ 
+    Arguments:
+        cm_cloud    Name of the IaaS cloud e.g. india_openstack_grizzly.
+    
+    Description:
+       image command provides list of available images. Image describes
+       pre-configured virtual machine image.
+       
+
+    Result:
+
+    Examples:
+        $ image india_openstack_grizzly
+        
+    """
+
+    #log.info(arguments)
+    clouds_name = arguments['<cm_cloud>']
+    # clouds in c.images treats None value as a ALL clouds
+    if not clouds_name:
+        clouds_name = None
+    config = cm_config()
+    username = config.username()
+    c = cm_mongo()
+    c.activate(cm_user_id=username)
+    images_dict = c.images(cm_user_id=username, clouds=clouds_name)
+    your_keys = {"openstack":
+                    [
+                        # [ "Metadata", "metadata"],
+                        [ "status" , "status"],
+                        [ "name" , "name"],
+                        [ "type_id" , "metadata", "instance_type_id"],
+                        [ "iname" , "metadata", "instance_type_name"],
+                        [ "location" , "metadata", "image_location"],
+                        [ "state" , "metadata", "image_state"],
+                        [ "updated" , "updated"],
+                        [ "minDisk" , "minDisk"],
+                        [ "memory_mb" , "metadata", 'instance_type_memory_mb'],
+                        [ "fid" , "metadata", "instance_type_flavorid"],
+                        [ "vcpus" , "metadata", "instance_type_vcpus"],
+                        [ "user_id" , "metadata", "user_id"],
+                        [ "owner_id" , "metadata", "owner_id"],
+                        [ "gb" , "metadata", "instance_type_root_gb"],
+                        [ "arch", ""]
+                    ],
+                  "ec2":
+                    [
+                        # [ "Metadata", "metadata"],
+                        [ "state" , "extra", "state"],
+                        [ "name" , "name"],
+                        [ "id" , "id"],
+                        [ "public" , "extra", "ispublic"],
+                        [ "ownerid" , "extra", "ownerid"],
+                        [ "imagetype" , "extra", "imagetype"]
+                    ],
+                  "azure":
+                    [
+                        [ "name", "label"],
+                        [ "category", "category"],
+                        [ "id", "id"],
+                        [ "size", "logical_size_in_gb" ],
+                        [ "os", "os" ]
+                    ],
+                  "aws":
+                    [
+                        [ "state", "extra", "state"],
+                        [ "name" , "name"],
+                        [ "id" , "id"],
+                        [ "public" , "extra", "ispublic"],
+                        [ "ownerid" , "extra", "ownerid"],
+                        [ "imagetype" , "extra", "imagetype"]
+                    ]
+                  }
+
+    images = _select_images(images_dict, your_keys)
+
+    _display(images)
+    
+def _select_images(data, selected_keys, env=[]):
+
+    """
+
+        status ACTIVE
+        updated 2013-05-26T19:29:09Z
+        name menghan/custom-utuntu-01
+        links [{u'href': u'http://198.202.120.83:8774/v1.1/1ae6813a3a6d4cebbeb1912f6d139ad0/images/502a5967-18ff-448b-830f-d6150b650d6b', u'rel': u'self'}, {u'href': u'http://198.202.120.83:8774/1ae6813a3a6d4cebbeb1912f6d139ad0/images/502a5967-18ff-448b-830f-d6150b650d6b', u'rel': u'bookmark'}, {u'href': u'http://198.202.120.83:9292/1ae6813a3a6d4cebbeb1912f6d139ad0/images/502a5967-18ff-448b-830f-d6150b650d6b', u'type': u'application/vnd.openstack.image', u'rel': u'alternate'}]
+        created 2013-05-26T19:28:09Z
+        minDisk 0
+        metadata {u'instance_uuid': u'16a5f5ac-7f39-4b01-a2c3-b2003beffb9d',
+                  u'image_location': u'snapshot',
+                  u'image_state': u'available',
+                  u'instance_type_memory_mb': u'2048',
+                  u'instance_type_swap': u'0',
+                  u'instance_type_vcpu_weight': u'None',
+                  u'image_type': u'snapshot',
+                  u'instance_type_id': u'5',
+                  u'ramdisk_id': None,
+                  u'instance_type_name': u'm1.small',
+                  u'instance_type_ephemeral_gb': u'0',
+                  u'instance_type_rxtx_factor': u'1',
+                  u'kernel_id': None,
+                  u'instance_type_flavorid': u'2',
+                  u'instance_type_vcpus': u'1',
+                  u'user_id': u'f603818711324203970ed1e3bb4b90ed',
+                  u'instance_type_root_gb': u'20',
+                  u'base_image_ref': u'1a5fd55e-79b9-4dd5-ae9b-ea10ef3156e9',
+                  u'owner_id': u'1ae6813a3a6d4cebbeb1912f6d139ad0'}
+        server {u'id': u'16a5f5ac-7f39-4b01-a2c3-b2003beffb9d', u'links': [{u'href': u'http://198.202.120.83:8774/v1.1/1ae6813a3a6d4cebbeb1912f6d139ad0/servers/16a5f5ac-7f39-4b01-a2c3-b2003beffb9d', u'rel': u'self'}, {u'href': u'http://198.202.120.83:8774/1ae6813a3a6d4cebbeb1912f6d139ad0/servers/16a5f5ac-7f39-4b01-a2c3-b2003beffb9d', u'rel': u'bookmark'}]}
+        cm_id sierra_openstack_grizzly-images-menghan/custom-utuntu-01
+        cm_refresh 2013-08-06T21-44-13Z
+        cm_cloud sierra_openstack_grizzly
+        minRam 0
+        progress 100
+        cm_kind images
+        _id 5201a66d7df38caf0fe160b5
+        cm_type openstack
+        id 502a5967-18ff-448b-830f-d6150b650d6b
+        OS-EXT-IMG-SIZE:size 876216320
+        b99fa4c8-6b92-49e6-b53f-37e56f9383b6
+    """
+    headers = ["cm_cloud"] + selected_keys['openstack']
+    images = [headers]
+    for cm_cloud, _id in data.iteritems():
+        for image_name, v in _id.iteritems():
+            values = [cm_cloud]
+            # cm_type is required to use a selected_keys for the cm_type
+            cm_type = v['cm_type']
+            for k in selected_keys[cm_type]:
+                try:
+                    values.append(v[k])
+                except:
+                    values.append(0)
+            images.append(values)
+    return images
 
 def main():
     arguments = docopt(shell_command_flavor.__doc__)
