a015d4281b2e8abd54105b8bec04a83794d4bcd8
Gregor von Laszewski <laszewski@gmail.com>
2014-07-28 22:40:12 -0400

    documenting the classes and fixing several broken methods

--- cloudmesh/management/project.py
+++ cloudmesh/management/project.py
@@ -3,18 +3,21 @@ from mongoengine.context_managers import switch_db
 from datetime import datetime
 import hashlib, uuid
 from user import User, Users
-#from comittee import Committee
+# from comittee import Committee
 from pprint import pprint
 from cloudmeshobject import CloudmeshObject
 
-port=27777
+port = 27777
 db_name = 'project'
 
 
 def IMPLEMENT():
     print "IMPLEMENT ME"
 
-STATUS = ('pending', 'approved', 'completed', 'denied')
+STATUS = ('pending',
+          'approved',
+          'completed',
+          'denied')
 
 CATEGORY = ('Database', 'FutureGrid', 'other')
 
@@ -29,7 +32,7 @@ INSTITUTE_ROLE = ('gaduate student',
                   'faculty',
                   'visitor',
                   'other')
-                  
+
 CLUSTERS = ('india',
             'bravo',
             'echo',
@@ -58,43 +61,86 @@ GRANT_ORG = ('NSF',
              'DoD',
              'NIH',
              'other', 'None')
-    
+
 class Project(CloudmeshObject):
+    '''
+    The project object with its fields. The current fields include
+    
+    Attributes:
+    
+        title     
+        abstract
+        intellectual_merit  
+        broader_impact  
+        use_of_fg  
+        scale_of_use 
+        categories 
+        keywords 
+        primary_discipline 
+        orientation  
+        contact  
+        url 
+        comment 
+        active 
+        projectid 
+        status
+        lead       
+        managers   
+        members    
+        alumnis    
+        grant_orgnization 
+        grant_id 
+        grant_url 
+        results 
+        aggreement_use 
+        aggreement_slides 
+        aggreement_support 
+        aggreement_sotfware 
+        aggreement_documentation 
+        comments
+        join_open 
+        join_notification 
+        resources_services
+        resources_software 
+        resources_clusters 
+        resources_provision
+
+    '''
 
     # -------------------------------------------------------------------
     # Project Information
     # -------------------------------------------------------------------
-    title  = StringField(required=True)    
-    abstract    = StringField(required=True) 
-    intellectual_merit  = StringField(required=True)
-    broader_impact  = StringField(required=True)
-    use_of_fg  = StringField(required=True)
-    scale_of_use  = StringField(required=True)
-    categories =  ListField(StringField(choices=CATEGORY), required=True)
+    title = StringField(required=True)
+    abstract = StringField(required=True)
+    intellectual_merit = StringField(required=True)
+    broader_impact = StringField(required=True)
+    use_of_fg = StringField(required=True)
+    scale_of_use = StringField(required=True)
+    categories = ListField(StringField(choices=CATEGORY), required=True)
     # example search in a list field
     # Project.objects(categories__contains='education')
-    keywords  = ListField(StringField(), required=True)
-    primary_discipline =  StringField(choices=DISCIPLINE, required=True)
-    orientation  = StringField(required=True)
-    contact  = StringField(required=True)
+    keywords = ListField(StringField(), required=True)
+    primary_discipline = StringField(choices=DISCIPLINE, required=True)
+    orientation = StringField(required=True)
+    contact = StringField(required=True)
     url = URLField(required=True)
     comment = StringField()
     active = BooleanField(required=True)
     projectid = UUIDField()
 
-    status =  StringField(choices=STATUS,required=True)
+    status = StringField(choices=STATUS, required=True)
     # maybe we do not need active as this may be covered in status
-    
+
     # -------------------------------------------------------------------
     # Project Comittee: contains all the information about the projects committee
     # -------------------------------------------------------------------
-    #comittee = ReferenceField(Committee)
-    
+    # comittee = ReferenceField(Committee)
+
     # -------------------------------------------------------------------
     # Member Fields
     # -------------------------------------------------------------------
     lead = ReferenceField(User)
-    #lead_institutional_role =  StringField(choices=INSTITUTE_ROLE, required=True)
+    # lead_institutional_role =  StringField(choices=INSTITUTE_ROLE, required=True)
     managers = ListField(StringField())
     members = ListField(ReferenceField(User))
     alumnis = ListField(StringField())
@@ -105,7 +151,7 @@ class Project(CloudmeshObject):
     # -------------------------------------------------------------------
     # Grant Information
     # -------------------------------------------------------------------
-    grant_orgnization =  StringField(choices=GRANT_ORG)
+    grant_orgnization = StringField(choices=GRANT_ORG)
     grant_id = StringField()
     grant_url = URLField()
 
@@ -142,16 +188,16 @@ class Project(CloudmeshObject):
     resources_software = ListField(StringField(choices=SOFTWARE), required=True)
     resources_clusters = ListField(StringField(choices=CLUSTERS), required=True)
     resources_provision = ListField(StringField(choices=PROVISIONING), required=True)
-    
+
     # BUG how can we add also arbitray info in case of other, mabe ommit choices
 
     def to_json(self):
         """prints the project as a json object"""
-         
-        d ={
+
+        d = {
              "title": self.title,
              "abstract": self.abstract,
-             "intellectual_merit": self.intellectual_merit,    
+             "intellectual_merit": self.intellectual_merit,
              "broader_impact": self.broader_impact,
              "use_of_fg": self.use_of_fg,
              "scale_of_use": self.scale_of_use,
@@ -170,14 +216,20 @@ class Project(CloudmeshObject):
              "resources_clusters": self.resources_clusters,
              "resources_provision": self.resources_provision
             }
-        return d     
-             
+        return d
+
 
     def __str__(self):
+        '''
+        printing the object as a string
+        '''
         d = self.to_json()
         return str(d)
 
 class Projects(object):
+    '''
+    convenience opbject to manage multiple prpojects
+    '''
 
     def __init__(self):
         db = connect(db_name, port=port)
@@ -185,89 +237,119 @@ class Projects(object):
         db = connect('user', port=port)
         self.users = User.objects()
 
-    
+
     def __str__(self):
+        '''
+        not implemented
+        '''
         IMPLEMENT()
 
+    def update(self):
+        '''
+        updates the project
+        '''
+        self.projects = Project.objects()
+
     def objects(self):
+        '''
+        returns the projects
+        '''
+        self.update()
         return self.projects
-    
-    def add(self, project):
-    	"""adds a project to the database but only after it has been verified"""
-        # THIS DOES NOT WORK SO I OUTCOMMENT IT: Response= Figured out the problem so I made the correction.
-        #_verify = self.verify_user(project.lead, project)
-    	#if _verify == True:
-            #*****Add a committee here before saving, especially the default committee*******
-        project.save()   	
-    	#else:
-            #print "ERROR: The user `{0}` has not registered with FutureGrid".format(project.lead)
-    
-    def add_member(self, user_name, project):
+
+    def save(self, project):
+        '''adds a project to the database but only after it has been verifie
+        
+        :param project: the project id
+        :type project: uuid
+        '''
+        project.save()
+
+    def add(self, user_name, project, role):
+        '''
+        Adds a member to the project. 
+        
+        :param role: the role of the user
+        :type role: String
+        :param user_name: the username
+        :type user_name: String
+        :param project: the project id
+        :type project: uuid
+        '''
     	"""adds members to a particular project"""
-    	__found = False
-    	for user in User.objects:
-    	    if user.username == user_name:
-    	    	project.members.append(user)
-    	    	__found == True
-    	if __found == False:
+        users = User.objects(user_name=user_name)
+        if users.count() == 1:
+            if role == "member":
+                project.members.append(user)
+            elif role == "lead":
+                project.lead.append(user)
+            elif role == "lead":
+                project.alumni.append(user)
+        else:
             print "ERROR: The user `{0}` has not registered with FutureGrid".format(user_name)
-    
-    def find_all_members(self, project):
-    	"""returns all the members of a particular project"""
-    	return project.members
-    
-    def verify_user(self, user_name, project):
-    	"""Verifies that a user has been registered or stored in the 
-    	database before adding the user to the members and project lead"""
-        __found = False
-	for user in User.objects:
-    	    if user.username == user_name:
-    	    	project.lead = user
-    	    	project.members.append(user)
-    	    	__found = True
-    	
-        return __found
-            
+
+    def find_all(self, project, role):
+        '''returns all the members of a particular project
+        
+        :param role: the role of the user
+        :type role: String
+        :param project: the project id
+        :type project: uuid
+        '''
+        if role == "member":
+            return project.members
+        elif role == "lead":
+            return project.leads
+        elif role == "lead":
+            return project.alumni
+
     def find_by_id(self, id):
+        '''
+        finds projects by if 
+        
+        :param id: the project id
+        :type id: uuid
+        '''
     	"""Finds a project by the given id"""
         found = Project.objects(projectid=id)
         if found.count() > 0:
             return found[0].to_json()
         else:
             return None
-        #User ID or project ID
+        # User ID or project ID
 
     def find_by_category(self, category):
+        '''
+        find the project by category
+        
+        :param category: the category
+        :type category: String
+        '''
     	"""Finds and returns all project in that category"""
     	found = Project.objects(categories=category)
         if found.count() > 0:
             return found[0].to_json()
         else:
-            return None   
+            return None
 
     def find_by_keyword(self, keyword):
+        '''
+        finds a projects matching a keyword
+        
+        :param keyword: a keyword
+        :type keyword: String
+        '''
     	"""Finds and returns all projects with the entered keyword"""
         found = Project.objects(keyword=keyword)
         if found.count() > 0:
             return found[0].to_json()
         else:
             return None
-            
-    def find(self, title = None):
-    	"""Finds a project by its title"""
-    	print "Projects"
-        if title == None:
-            return Project.objects()               
-    	else:
-            found = Project.objects(title = title)
-            if found.count() > 0:
-                return found[0].to_json()
-            else:
-                return None
+
 
     def clear(self):
         """removes all projects from the database"""
         for project in Project.objects:
             project.delete()
-    
-             
+
+
