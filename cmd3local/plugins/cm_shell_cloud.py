e3670863606997f5b0b98d1f34388103955ff92e
Mark Yukai Xiao <mustxyk@gmail.com>
2014-07-28 23:59:11 -0400

    updating command cloud

--- cmd3local/plugins/cm_shell_cloud.py
+++ cmd3local/plugins/cm_shell_cloud.py
@@ -1,12 +1,12 @@
 from cmd3.shell import function_command
-from cloudmesh.iaas.cm_list import shell_command_cloud
+from cloudmesh.iaas.cm_cloud import shell_command_cloud
 
-class cm_shell_list:
+class cm_shell_cloud:
     
-    def activate_cm_shell_list(self):
+    def activate_cm_shell_cloud(self):
         self.register_command_topic('cloud','cloud')
 
-    @function_command(shell_command_list)
-    def do_list(self, args, arguments):
+    @function_command(shell_command_cloud)
+    def do_cloud(self, args, arguments):
     
         shell_command_cloud(arguments)
