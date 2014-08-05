c6d6ecfc7346185894d141b47fefbcb093fe6753
Gregor von Laszewski <laszewski@gmail.com>
2014-08-05 16:02:39 -0400

    the web gui in the cm shell

---
+++ cmd3local/plugins/cm_shell_web.py
@@ -0,0 +1,17 @@
+from cmd3.shell import function_command
+from cloudmesh.shell.cm_open_web import shell_command_open_web
+
+class cm_shell_web:
+    
+    def activate_cm_shell_web(self):
+        self.register_command_topic('cloud','web')
+        pass
+
+    @function_command(shell_command_open_web)
+    def do_ggg(self, args, arguments):
+        shell_command_open_web(arguments)
+        pass
+
+if __name__ == '__main__':
+    command = cm_shell_web()
+    command.do_web("")
