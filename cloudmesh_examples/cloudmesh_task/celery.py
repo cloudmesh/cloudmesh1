dea8d70832c32cff572a772bd8b78ca0b8fa39c1
Gregor von Laszewski <laszewski@gmail.com>
2014-08-14 17:37:21 -0400

    trying a second get

--- cloudmesh_examples/cloudmesh_task/celery.py
+++ cloudmesh_examples/cloudmesh_task/celery.py
@@ -10,7 +10,7 @@ app = Celery('cloudmesh_task',
 # Optional configuration, see the application user guide.
 app.conf.update(
     CELERY_RESULT_BACKEND = 'amqp',
-    CELERY_TASK_RESULT_EXPIRES=5,     
+    CELERY_TASK_RESULT_EXPIRES=1,     
     CELERY_ACCEPT_CONTENT = ['json'],
     #    CELERY_ACCEPT_CONTENT = ['application/json'],
     CELERY_TASK_SERIALIZER = 'json',
