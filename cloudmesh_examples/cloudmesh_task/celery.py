d90d18d4a915b98a713cb267af0c097840025626
Gregor von Laszewski <laszewski@gmail.com>
2014-08-14 05:30:03 -0400

    changing the way to start async commands in celery

--- cloudmesh_examples/cloudmesh_task/celery.py
+++ cloudmesh_examples/cloudmesh_task/celery.py
@@ -9,8 +9,15 @@ app = Celery('cloudmesh_task',
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
-)
+    CELERY_RESULT_BACKEND = 'amqp',
+    CELERY_TASK_RESULT_EXPIRES=5,     
+    CELERY_ACCEPT_CONTENT = ['json'],
+    #    CELERY_ACCEPT_CONTENT = ['application/json'],
+    CELERY_TASK_SERIALIZER = 'json',
+    CELERY_RESULT_SERIALIZER = 'json',    
+    )
+
+
 
 if __name__ == '__main__':
     app.start()
--- cloudmesh_examples/cloudmesh_task/tasks.py
+++ cloudmesh_examples/cloudmesh_task/tasks.py
@@ -1,23 +1,33 @@
-from __future__ import absolute_import
+# from __future__ import absolute_import
 
 from cloudmesh_task.celery import app
 from sh import ssh 
+from pprint import pprint
+import datetime
 
 @app.task
-def cm_ssh(host, username, command):
-    print "{0}@{1}:{2}".format(username, host, command)
-    output = ""
-    error = None
-    try:
-        output = ssh("{0}@{1}".format(username, host), command)
-        banner(str(type(output)))
-        
-    except Exception, e:
-        error = dict({"host": host,
-                    "username": username,
-                    "command": command,
-                    "error": e})
-        
-    return dict({"output": str(output),
-                 "error": error})
+def cm_ssh(host, username=None, command=None):
 
+    result = dict({
+        "host": host,
+        "command": command,
+        "username": username, 
+        "output": "None",
+        "error": "None",
+        "time": "None"
+        })
+
+    now = datetime.datetime.now()
+    try:
+        
+        result["time"] = str(now)
+        result["output"] = str(ssh("{0}@{1}".format(username, host), "date; " + command))
+        # result["output"] = "DEBUG {0} {1}".format(host, str(now))
+
+    except Exception, e:
+
+        result["error"] = str(e)
+
+    return result
+
+    
