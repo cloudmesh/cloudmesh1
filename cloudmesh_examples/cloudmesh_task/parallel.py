88c44f8ac64518d17f1f417fb7181e1b6adff3c1
Gregor von Laszewski <laszewski@gmail.com>
2014-08-14 05:30:03 -0400

    changing the way to start async commands in celery

--- cloudmesh_examples/cloudmesh_task/parallel.py
+++ cloudmesh_examples/cloudmesh_task/parallel.py
@@ -1,23 +1,31 @@
-from cloudmesh_common.util import banner
 from pprint import pprint
+from cloudmesh_common.util import banner
+from datetime import timedelta, datetime
 
 def Sequential (execution_array, f, **kwargs):
+    print "ARGS", kwargs
     result = {}
     for element in execution_array:
         result[element] = f(element, **kwargs)
     return result
+
+
                 
 def Parallel (execution_array, f, **kwargs):
     task = {}
-    banner("SUBMIT")
     for element in execution_array:
-        task[element] = f.delay(element, **kwargs)
+        task[element] = f.apply_async(args=(element,),
+                                      kwargs=kwargs,
+                                      expires=1)
 
+    banner ("tasks", c=".")
     pprint (task)
-    
-    banner("COLLECT")
     result = {}
+
     for element in execution_array:
-        banner (element, c=".")      
+        
+        banner ("{0} {1}".format(element, task[element]), c=".")      
         result[element] = task[element].get(propagate=False)
+        print "OOOO", result[element]
+
     return result
