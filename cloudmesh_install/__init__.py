22bc85c745fa06e917c5f6dde30e05913e8e19c7
Hyungro Lee <hroe.lee@gmail.com>
2014-08-18 16:06:31 -0400

    config_file_raw returns ~/.futuregrid/ without path_expand()

--- cloudmesh_install/__init__.py
+++ cloudmesh_install/__init__.py
@@ -3,13 +3,16 @@ import sys
 from cloudmesh_common.bootstrap_util import path_expand
 
 
-_config_dir_prefix__ = "~/.futuregrid"
+__config_dir_prefix__ = "~/.futuregrid"
 
-__config_dir__ = path_expand(_config_dir_prefix__)
+__config_dir__ = path_expand(__config_dir_prefix__)
 
 
 def config_file(filename):
     return __config_dir__ + filename
+
+def config_file_raw(filename):
+    return __config_dir_prefix__ + filename
 
 def config_file_prefix():
     return __config_dir_prefix__ 
