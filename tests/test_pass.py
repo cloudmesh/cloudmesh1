cf8b5cb4a8e3a3591baae89fd30f2f609bb50a26
Gregor von Laszewski <laszewski@gmail.com>
2014-08-20 06:57:28 -0400

    adding a test so that travis has a dummmy test that passes and reports success

---
+++ tests/test_pass.py
@@ -0,0 +1,36 @@
+""" run with
+
+nosetests -v --nocapture
+
+or
+
+nosetests -v
+
+"""
+
+from cloudmesh.config.cm_config import cm_config_server
+from cloudmesh.config.cm_config import cm_config
+from cloudmesh.config.ConfigDict import ConfigDict
+
+import json
+import os
+import warnings
+from pprint import pprint
+
+from cloudmesh_common.util import HEADING
+from cloudmesh_common.util import path_expand
+from cloudmesh_install import config_file
+
+class Test_pass:
+
+    def setup(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_dummy(self):
+        HEADING()
+        assert True
+
+
