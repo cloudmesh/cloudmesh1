7dff9681c32a504e2fb11a17ed605743d431cc44
Gregor von Laszewski <laszewski@gmail.com>
2014-08-25 15:13:55 -0400

    adding the setup start

--- setup.py
+++ setup.py
@@ -1,23 +1,68 @@
 #!/usr/bin/env python
-# -------------------------------------------------------------------------- #
-# Copyright 2010-2011, Indiana University                                    #
-#                                                                            #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
-# not use this file except in compliance with the License. You may obtain    #
-# a copy of the License at                                                   #
-#                                                                            #
-# http://www.apache.org/licenses/LICENSE-2.0                                 #
-#                                                                            #
-# Unless required by applicable law or agreed to in writing, software        #
-# distributed under the License is distributed on an "AS IS" BASIS,          #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
-# See the License for the specific language governing permissions and        #
-# limitations under the License.                                             #
-# -------------------------------------------------------------------------- #
-import setuptools
 
-setuptools.setup(
-    setup_requires=['d2to1', 'pbr'],
-    d2to1=True)
+from distutils.core import setup
 
-    #    version='#:')
+setup(
+    name = 'cloudmesh',
+    version = __import__('cloudmesh').version(),
+    description = 'A tool to simplify managing multiple clouds including bare metal provisioning',
+    #description-file =
+    #    README.rst
+    author = 'Cloudmesh Team',
+    author_email = 'laszewski@gmail.com',
+    url = 'http://github.org/cloudmesh/cloudmesh',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Clustering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Boot',
+        'Topic :: System :: Systems Administration',
+        'Framework :: Flask',
+        'Environment :: OpenStack',
+        ],
+    packages = [
+        'cloudmesh_cmd3',
+        'cloudmesh',
+        'cloudmesh_install',
+        'cloudmesh_common'
+    ],
+  entry_points={'console_scripts': [
+    'cm-manage = cloudmesh.config.cm_manage:main',
+    'cm-init = cloudmesh.config.cm_init:main',
+    'cm-image  = cloudmesh.image.cm_image:main',
+    'cm-metric = cloudmesh.metric.cm_metric:main',
+    'cm-rain = cloudmesh.rain.cobbler.cobbler_rain:main',
+    ]},    
+
+)
+
+"""
+[install]
+install-data=$HOME
+
+
+data_files =
+    .cloudmesh = etc/FGLdapCacert.pem
+    .cloudmesh = etc/sierra-cacert.pem
+    .cloudmesh/etc/racks/diag = etc/racks/*
+    .cloudmesh/etc = etc/cloudmesh.yaml
+    .cloudmesh/etc = etc/me-none.yaml
+    .cloudmesh/etc = etc/cloudmesh.yaml
+    .cloudmesh/etc = etc/cloudmesh_server.yaml
+    .cloudmesh/etc = etc/cloudmesh_rack.yaml
+    .cloudmesh/etc = etc/cloudmesh_celery.yaml
+    .cloudmesh/etc = etc/cloudmesh_mac.yaml
+    .cloudmesh/etc = etc/cloudmesh_flavor.yaml
+    .cloudmesh = etc/cloudmesh_flavor.yaml
+
+"""
