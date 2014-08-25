cb65062af64de920bf31c93c7c97eb8a83f1c792
Hyungro Lee <hroe.lee@gmail.com>
2014-08-25 06:59:31 -0400

    updated bootstp and vagrant

--- vagrant/example1/bootstrap.sh
+++ vagrant/example1/bootstrap.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+apt-get -y install git
 git clone https://github.com/cloudmesh/cloudmesh.git
+apt-get -y install python-pip
+pip install virtualenv
+su -- vagrant
 virtualenv ~/ENV
 source ~/ENV/bin/activate
 cd cloudmesh
-./install system
+sudo ./install system
 ./install requirements
 ./install new
 #./install rc fetch
