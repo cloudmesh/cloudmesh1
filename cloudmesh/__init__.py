8d279a77cda1cf80235bd61669af282a8f87af9b
Gregor von Laszewski <laszewski@gmail.com>
2014-08-25 14:31:10 -0400

    try to identify the compile error

--- cloudmesh/__init__.py
+++ cloudmesh/__init__.py
@@ -1,12 +1,16 @@
 """
 A package to manage virtual machines on various clouds infrastructures and bare metal images.
 """
+
+"""
 from cloudmesh.config.cm_config import cm_config_server
 from cloudmesh.config.cm_config import cm_config as load
 from cloudmesh.config.cm_config import cm_config
 from cloudmesh.pbs.pbs import PBS
 from cloudmesh.cm_mesh import cloudmesh as mesh
 from pbr import version
+
+from cloudmesh.util.helper import vm_name 
 
 __version__ = version.VersionInfo('cloudmesh')
 
@@ -22,3 +26,4 @@ def version():
     return __version__
 
 
+"""
