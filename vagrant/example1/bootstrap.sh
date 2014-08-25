22b1f36ba409068f431abcf3dffd9daa280fde98
Hyungro Lee <hroe.lee@gmail.com>
2014-08-25 16:07:02 -0400

    default project and hpc username will be replaced with real values when ./install rc fetch completed

--- vagrant/example1/bootstrap.sh
+++ vagrant/example1/bootstrap.sh
@@ -3,8 +3,8 @@
 apt-get -y install git
 apt-get -y install python-pip
 pip install virtualenv
-git clone https://github.com/cloudmesh/cloudmesh.git
-git clone https://github.com/cloudmesh/cmd3.git
+git clone https://github.com/cloudmesh/cloudmesh.git ~/cloudmesh/
+git clone https://github.com/cloudmesh/cmd3.git ~/cmd3
 virtualenv ~/ENV
 source ~/ENV/bin/activate
 cd ~/cmd3
