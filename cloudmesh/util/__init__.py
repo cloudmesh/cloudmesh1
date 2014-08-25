201ff2f0c29612a5acb89bba261b09e09d8ef586
Gregor von Laszewski <laszewski@gmail.com>
2014-08-25 09:30:51 -0400

    removing the build from clean

--- cloudmesh/util/__init__.py
+++ cloudmesh/util/__init__.py
@@ -1,4 +1,9 @@
 """
 A number of simple utility functiond for cloudmesh.
 """
-__all__ = ['logger', 'util', 'menu', 'password', 'config']
+__all__ = ['logger',
+           'util',
+           'menu',
+           'password',
+           'config',
+           'console']
