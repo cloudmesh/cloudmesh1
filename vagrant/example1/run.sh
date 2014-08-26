50b18fedc36a1a84532a5d78ed75bd03aa4fb7f9
Hyungro Lee <hroe.lee@gmail.com>
2014-08-25 21:05:36 -0400

    base image selection added

--- vagrant/example1/run.sh
+++ vagrant/example1/run.sh
@@ -9,4 +9,26 @@ read userid
 echo "=================================="
 echo $userid >> .userid
 
+echo
+echo "=================================="
+echo "Select base image to launch"
+echo "=================================="
+PS3="Please choose an option: "
+select option in "Ubuntu Server 14.04 64bit" "Ubuntu Server 14.04 32bit"
+do
+	case $option in
+		"Ubuntu Server 14.04 64bit") 
+			echo $option "selected"
+			ln -sf Vagrantfile64 Vagrantfile
+			break;;
+
+		"Ubuntu Server 14.04 32bit") 
+			echo $option "selected"
+			ln -sf Vagrantfile32 Vagrantfile
+			break;;
+		quit)
+			break;;
+	esac
+done
+
 vagrant up
