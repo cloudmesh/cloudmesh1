c6f8080ccd0c97b1f4c94869b47ffc7e82b6e27e
Gregor von Laszewski <laszewski@gmail.com>
2014-08-26 16:32:16 -0400

    update the init

--- cloudmesh/__init__.py
+++ cloudmesh/__init__.py
@@ -10,21 +10,29 @@ __version__ = '1.0'
 def version():
     return __version__
 
+from cloudmesh.util.helper import vm_name 
+from cloudmesh.sh.cm import shell
+
+from cloudmesh.config.cm_config import cm_config as load
 
 
 """
 from cloudmesh.config.cm_config import cm_config_server
-from cloudmesh.config.cm_config import cm_config as load
+
+
+
 from cloudmesh.config.cm_config import cm_config
+
+
 from cloudmesh.pbs.pbs import PBS
-from cloudmesh.cm_mesh import cloudmesh as mesh
-from pbr import version
 
-from cloudmesh.util.helper import vm_name 
-
+    from cloudmesh.cm_mesh import cloudmesh as mesh
+"""
 
 
 
+
+"""
 # import sys
 # __all__= ['cloudmesh', 'profile', 'accounting', 'config', 'iaas', 'inventory', 'util', 'iaas']
 
