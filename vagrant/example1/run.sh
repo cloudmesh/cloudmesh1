eb1724e88110b543e3f0348df165ea217fffe49e
Hyungro Lee <hroe.lee@gmail.com>
2014-08-28 15:22:52 -0400

    ./run.sh in vagrant is now accepting command parameters. ./run.sh abc(portal_login_id) Ubuntu-Server-14.04-64bit can be used to skip user input.

--- vagrant/example1/run.sh
+++ vagrant/example1/run.sh
@@ -1,34 +1,71 @@
 #!/bin/bash
 
+write_userid () {
+	userid=$1
+	echo $userid > .userid
+}
+
+select_option() {
+	option=$1
+	case $option in
+		"Ubuntu-Server-14.04-64bit") 
+			echo $option "selected"
+			ln -sf Vagrantfile64 Vagrantfile
+			;;
+
+		"Ubuntu-Server-14.04-32bit") 
+			echo $option "selected"
+			ln -sf Vagrantfile32 Vagrantfile
+			;;
+		quit)
+	esac
+}
+
+images=("Ubuntu-Server-14.04-64bit" "Ubuntu-Server-14.04-32bit")
+debug=0
+
 # copy private key to synced folders
 cp ~/.ssh/id_rsa `pwd`
+if [ $# -eq 2 ]
+then
+	userid=$1
+	option=$2
+fi
 
-echo "=================================="
-echo "Futuregrid portal id? (def:$USER)"
-read userid
-echo "=================================="
-echo $userid > .userid
+if [ -z "$userid" ]
+then
+	echo "=================================="
+	echo "Futuregrid portal id? (def:$USER)"
+	echo "=================================="
+	echo -n "Login id: "; read userid
+fi
+write_userid "$userid"
+
+if [ -z "$option" ]
+then
+	echo
+	echo "=================================="
+	echo "Select base image to launch"
+	echo "=================================="
+	PS3="Please choose an option: "
+	select option in ${images[@]}
+	do
+		select_option "$option"
+		break;
+	done
+else
+	select_option "$option"
+fi
+
+# Verify
+if [ 0 -ne "$debug" ]
+then
+	cat .userid
+	ls -al Vagrantfile
+fi
 
 echo
 echo "=================================="
-echo "Select base image to launch"
+echo "Vagrant is up"
 echo "=================================="
-PS3="Please choose an option: "
-select option in "Ubuntu Server 14.04 64bit" "Ubuntu Server 14.04 32bit"
-do
-	case $option in
-		"Ubuntu Server 14.04 64bit") 
-			echo $option "selected"
-			ln -sf Vagrantfile64 Vagrantfile
-			break;;
-
-		"Ubuntu Server 14.04 32bit") 
-			echo $option "selected"
-			ln -sf Vagrantfile32 Vagrantfile
-			break;;
-		quit)
-			break;;
-	esac
-done
-
 vagrant up
