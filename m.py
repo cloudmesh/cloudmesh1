c4fd812cb84fb4f0f692dedb30e59d9c19d61cf8
Hyungro Lee <hroe.lee@gmail.com>
2014-09-04 18:23:10 -0400

    vm_create() added to m.py for testing

--- m.py
+++ m.py
@@ -22,4 +22,23 @@ mesh.refresh(username,types=['flavors'],names=["india"])
 # PROPOSAL FOR NEW MESH API
 #
 
+userinfo = cloudmesh.cm_user().info(username)
+
+cloud = "india"
+prefix = userinfo['defaults']['prefix']
+index = userinfo['defaults']['index']
+flavor = userinfo['defaults']['flavors'][cloud] 
+# or flavor = "2" small
+image = userinfo['defaults']['images'][cloud] 
+# or image = '02cf1545-dd83-493a-986e-583d53ee3728' 
+# ubuntu-14.04 
+key = "%s_%s" % (username, userinfo['defaults']['key'])
+meta = { 'cm_owner': username }
+
+# vm_create
+result = mesh.vm_create(cloud, prefix, index, flavor, image, key, meta, username)
+
+# increase index after the completion of vm_create()
+cloudmesh.cm_user().set_default_attribute(username, "index", int(index) + 1)
+
 
