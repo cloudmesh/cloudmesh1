f0d29d98ff1e74431f82648156d94cba388bd41d
Gregor von Laszewski <laszewski@gmail.com>
2014-09-04 20:49:48 -0400

    pylint

--- cloudmesh/util/debug.py
+++ cloudmesh/util/debug.py
@@ -1,15 +1,21 @@
-import sys, os
+import sys
+import os
 
-def LINE( back = 0 ):
-    return sys._getframe( back + 1 ).f_lineno
 
-def FILE( back = 0 ):
-    return sys._getframe( back + 1 ).f_code.co_filename
+def LINE(back=0):
+    return sys._getframe(back + 1).f_lineno
 
-def FUNC( back = 0):
-    return sys._getframe( back + 1 ).f_code.co_name
 
-def WHERE( back = 0 ):
-    frame = sys._getframe( back + 1 )
-    return "%s/%s %s()" % ( os.path.basename( frame.f_code.co_filename ),
-                            frame.f_lineno, frame.f_code.co_name )
+def FILE(back=0):
+    return sys._getframe(back + 1).f_code.co_filename
+
+
+def FUNC(back=0):
+    return sys._getframe(back + 1).f_code.co_name
+
+
+def WHERE(back=0):
+    frame = sys._getframe(back + 1)
+    return "{0} {1} {2}()".format(os.path.basename(frame.f_code.co_filename),
+                                  frame.f_lineno,
+                                  frame.f_code.co_name)
