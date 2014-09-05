a9a2ca19c7371184f4cc981784123e446445d59c
Gregor von Laszewski <laszewski@gmail.com>
2014-09-04 21:37:04 -0400

    pylint

--- cloudmesh/config/cm_me.py
+++ cloudmesh/config/cm_me.py
@@ -3,11 +3,12 @@
 from jinja2 import Template
 import sys
 
-def command_me (arguments):
-    
+
+def command_me(arguments):
+
     t = Template("Hello {{ something }}!")
     result = t.render(something="World")
     print result
-    
+
 if __name__ == "__main__":
     command_me(sys.argv)
