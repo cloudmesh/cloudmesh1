7513651124f5005e5f9cb899325159026c242c7f
Gregor von Laszewski <laszewski@gmail.com>
2014-09-05 06:15:41 -0400

    commented launch.py

--- cloudmesh_web/modules/launch.py
+++ cloudmesh_web/modules/launch.py
@@ -1,8 +1,6 @@
-""" Launcher Script """
 from cloudmesh_install import config_file
 from flask import Blueprint
-from flask import render_template, request #, redirect
-from flask.ext.principal import Permission
+from flask import render_template, request
 from flask.ext.login import login_required
 from cloudmesh.config.ConfigDict import ConfigDict
 from cloudmesh.launcher.cm_launcher_db import cm_launcher_db
@@ -23,6 +21,8 @@ RAIN_PERMISSION = Permission(RoleNeed('rain'))
 # list of recipies which we need to get from cm_launcher.yaml
 
 # @RAIN_PERMISSION.require(http_exception=403)
+
+
 @login_required
 @RAIN_PERMISSION.require(http_exception=403)
 @launch_module.route('/cm/launch/launch_servers/', methods=["POST", "GET"])
@@ -30,12 +30,19 @@ def launch_servers():
     """ To satisfy Pylint. Will update with proper comments """
     config = cm_config()
     cloudname = request.form['cloud']
-    usr = config["cloudmesh"]["clouds"][cloudname]["credentials"]\
-    ["OS_USERNAME"]
-    hostname = config["cloudmesh"]["clouds"][cloudname]["cm_host"] 
-    ssh_cmd = "ssh " + usr + "@" + hostname + " \"" \
-    + request.form['script'] + "\" >> /home/cloudnaut/results.txt"
-
+    #
+    # this seems wrong as the script is formulated in the yaml file, so you need to get the type
+    # from the form and read the script from the yaml file
+    #
+    data = {
+        'usr': config["cloudmesh"]["clouds"][cloudname]["credentials"]["OS_USERNAME"],
+        'hostname': config["cloudmesh"]["clouds"][cloudname]["cm_host"],
+        'script': request.form['script']
+    }
+    ssh_cmd = "ssh {usr}@{hostname}\"{script}\" >> /home/cloudnaut/results.txt".format(**data)
+    #
+    # use sh instead or use the "Sequential" API
+    #
     subprocess.Popen(ssh_cmd, shell="True")
     return ssh_cmd
 
@@ -52,10 +59,9 @@ def display_launch_table():
     else:
         print "HEY JUDE"
 
-    launcher_config = ConfigDict(filename= \
-    config_file("/cloudmesh_launcher.yaml"))
-    launch_recipies = launcher_config \
-    .get("cloudmesh.launcher.recipies") 
+    launcher_config = ConfigDict(
+        filename=config_file("/cloudmesh_launcher.yaml"))
+    launch_recipies = launcher_config.get("cloudmesh.launcher.recipies")
     return render_template('mesh/mesh_launch.html',
                            recipies=launch_recipies)
 
@@ -71,6 +77,7 @@ def launch_status():
     for item in results:
         output_list.append(item)
     return str(output_list)
+
 
 @launch_module.route('/cm/launch/db_reset')
 @login_required
@@ -93,4 +100,4 @@ def launch_clear():
 def launch_run():
     """ To satisfy Pylint. Will update with proper comments """
     LOG_MSG.error("not yet implemented")
-    #pass
+    # pass
