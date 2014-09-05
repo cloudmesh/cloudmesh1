989b502a028ae51c264e9ea6a4153196001838c5
Gregor von Laszewski <laszewski@gmail.com>
2014-09-05 16:18:32 -0400

    merging cahnges, pylint

--- cloudmesh_web/modules/profile.py
+++ cloudmesh_web/modules/profile.py
@@ -1,13 +1,8 @@
-from cloudmesh.config.cm_config import cm_config, cm_config_server
-from cloudmesh.config.cm_projects import cm_projects
-from cloudmesh_common.util import cond_decorator
 from datetime import datetime
 from flask import Blueprint, g, render_template, request
 from flask.ext.login import login_required
-import cloudmesh
-from pprint import pprint
-# from cloudmesh.user.cm_userLDAP import get_ldap_user_from_yaml
 from cloudmesh.user.cm_user import cm_user
+from cloudmesh.config.cm_config import cm_config
 
 
 from cloudmesh_common.logger import LOGGER
