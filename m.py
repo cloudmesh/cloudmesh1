aac92d7c7bd7ea54a5649529d96680e1741c95b7
Hyungro Lee <hroe.lee@gmail.com>
2014-09-09 12:22:22 -0400

    full set of m.py

--- m.py
+++ m.py
@@ -1,139 +1,123 @@
 import cloudmesh
-
 from pprint import pprint
-import sys
+
+cloudmesh.logger(False)
+username = cloudmesh.load().username()
+
+cloudmesh.banner("INIT MONGO")
+mesh = cloudmesh.mesh("mongo")
+
+#
+# authentication as a user - username is requried
+# On webgui side, this is achieved by the framework,
+# and the username is obtined from g.user.id
+#
+# On CLI side, a global user object or username variable
+# should be maintained upon the start of the shell
+# The username could be obtained from yaml file.
+
+cloudmesh.banner("ACTIVATE")
+mesh.activate(username)
+
+cloudmesh.banner("GET FLAVOR")
+mesh.refresh(username,types=['flavors'],names=["india"])
+data = mesh.flavors(cm_user_id=username, clouds=["india"])
+
+pprint (data)
+
+cloudmesh.banner("GET IMAGE")
+mesh.refresh(username,types=['images'],names=["india"])
+data = mesh.images(cm_user_id=username, clouds=["india"])
+
+pprint (data)
+
+cloudmesh.banner("LIST KEYS")
+keyobj = cloudmesh.cm_keys_mongo(username)
+print keyobj.names()
+
+cloudmesh.banner("UPDATE LABELS")
+cloudmesh.print_label(username)
+print "setting labels .... prefix->test index->1"
+cloudmesh.update_label(username, prefix="test", id=1)
+
+#
+# PROPOSAL FOR NEW MESH API
+#
+
+cloud = "india"
+
+cloudmesh.banner("LAUNCH VM INSTANCE")
+result = mesh.start(cloud, username)
+
+cloudmesh.banner("TERMINATE VM INSTANCE")
+server = result['server']['id']
+mesh.delete(cloud, server, username)
+
+cloudmesh.banner("LAUNCH 3 VM INSTANCES")
+vm = {}
+for i in range(1,3):
+    vm[i] = mesh.start(cloud, username)
 
 
-
-
-    cloudmesh.logger(False)
-    username = cloudmesh.load().username()
-    
-    cloudmesh.banner("INIT MONGO")
-    mesh = cloudmesh.mesh("mongo")
-    
-    #
-    # authentication as a user - username is requried
-    # On webgui side, this is achieved by the framework,
-    # and the username is obtined from g.user.id
-    #
-    # On CLI side, a global user object or username variable
-    # should be maintained upon the start of the shell
-    # The username could be obtained from yaml file.
-    
-    cloudmesh.banner("ACTIVATE")
-    mesh.activate(username)
-    
-    cloudmesh.banner("GET FLAVOR")
-    mesh.refresh(username,types=['flavors'],names=["india"])
-    data = mesh.flavors(cm_user_id=username, clouds=["india"])
-    
-    pprint (data)
-    
-    cloudmesh.banner("GET IMAGE")
-    mesh.refresh(username,types=['images'],names=["india"])
-    data = mesh.images(cm_user_id=username, clouds=["india"])
-    
-    pprint (data)
-    
-    cloudmesh.banner("LIST KEYS")
-    keyobj = cloudmesh.cm_keys_mongo(username)
-    print keyobj.names()
-    
-    cloudmesh.banner("UPDATE LABELS")
-    cloudmesh.print_label(username)
-    print "setting labels .... prefix->test index->1"
-    cloudmesh.update_label(username, prefix="test", id=1)
-    
-    #
-    # PROPOSAL FOR NEW MESH API
-    #
-    
-    cloud = "india"
-    
-    cloudmesh.banner("LAUNCH VM INSTANCE")
-    result = mesh.start(cloud, username)
-    
-    cloudmesh.banner("TERMINATE VM INSTANCE")
-    server = result['server']['id']
+cloudmesh.banner("TERMINATE 3 VM INSTANCES")
+for i in vm: 
+    server = vm[i]['server']['id']
     mesh.delete(cloud, server, username)
 
-    cloudmesh.banner("LAUNCH 3 VM INSTANCES")
-    vm = {}
-    for i in range(1,3):
-        vm[i] = mesh.start(cloud, username)
-    
-    
-    cloudmesh.banner("TERMINATE 3 VM INSTANCES")
-    for i in vm: 
-        server = vm[i]['server']['id']
-        mesh.delete(cloud, server, username)
-    
-    '''
-    task :: start a VM on india, before starting, do various default setup actions
-    '''
-    print "NOT IMPLEMENTED"
-    return
-    
-    flavor=mesh.flavor(cloudname="india", flavorname="m1.small") 
-    flavor=mesh.flavor("india", "m1.small") 
-    
-    
-    #set image
 
-    image=mesh.image(cloudname="india", imagename="futuregrid/ubuntu-14.04") 
-    image=mesh.image("india", "futuregrid/ubuntu-14.04") 
-    # 
-    
-    # GET CURRENT VMNAME OR BETTER GET THE NEXT VM NAME????
-    
-    vmname = mesh.vmname()
-    print vmname
-    vmname = mesh.vmname(prefix="albert", idx=10)
-    print vmname
-    vmname = mesh.vmname("James", 20)
-    print vmname
-    vmname = mesh.vmname_next()
-    print vmname
-    vmname = mesh.vmname("Brian", "+2")
-    print vmname
-    
-    #start vm1
-
-    # save default image and falvor ????
-    
-    mesh.default("india", "image", image)
-    mesh.deafult("india", "flavor", flavor)
-    
-    vm = mesh.start("india", userid)
+cloudmesh.banner("GET A FLAVOR")
+flavor=mesh.flavor(cloudname="india", flavorname="m1.small") 
+flavor=mesh.flavor("india", "m1.small") 
 
 
-    #start vm2
-
-    # save default image and falvor ????
-    
-    
-    cloud = "india"
-    prefix = "gregor"
-    index = "10000"
-    flavor = mesh.flavor("india", "m1.small") 
-    image = mesh.image("india", "futuregrid/ubuntu-14.04") 
-    
-    vm = mesh.start(cloud, username, prefix=prefix, index=index, flavor=flavor,
-                    image=image)
-
-    
-    vm = mesh.start("india", username, image=image, flavor=flavor)
-    vm = mesh.assign_public_ip(...)
-    
-    vmname = vm['lable'/'name'] # prefix-id
-
-    result = mesh.ssh(vmname, "ls", ...)
+cloudmesh.banner("GET AN IMAGE")
+image=mesh.image(cloudname="india", imagename="futuregrid/ubuntu-14.04") 
+image=mesh.image("india", "futuregrid/ubuntu-14.04") 
 
 
-    mesh.delete(vmname)
+cloudmesh.banner("GET A VM NAME")
+vmname = mesh.vmname()
+print vmname
 
 
+cloudmesh.banner("SET A VM NAME")
+vmname = mesh.vmname(prefix="albert", idx=10)
+print vmname
+vmname = mesh.vmname("James", 20)
+print vmname
 
 
+cloudmesh.banner("GET A NEXT VM NAME")
+vmname = mesh.vmname_next()
+print vmname
+vmname = mesh.vmname("Brian", "+2")
+print vmname
 
+
+cloudmesh.banner("SET A DEFAULT IMAGE OR A DEFAULT FLAVOR")
+mesh.default("india", "image", image)
+mesh.deafult("india", "flavor", flavor)
+
+
+cloudmesh.banner("START A VM WITH OPTIONS")
+cloud = "india"
+prefix = "gregor"
+index = "10000"
+flavor = mesh.flavor("india", "m1.small") 
+image = mesh.image("india", "futuregrid/ubuntu-14.04") 
+
+vm = mesh.start(cloud, username, prefix=prefix, index=index, flavor=flavor,
+                image=image)
+
+
+vm = mesh.start("india", username, image=image, flavor=flavor)
+server = vm['server']['id']
+
+
+cloudmesh.banner("ASSIGN PUBLIC IP ADDRESS TO THE VM")
+ip = mesh.assign_public_ip(cloud, server, username)
+
+cloudmesh.banner("RUN A COMMAND VIA SSH TO THE VM")
+result = mesh.ssh(cloud, vmname, "ls")
+
+mesh.delete(cloud, server, username)
