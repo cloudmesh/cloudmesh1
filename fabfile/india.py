bd49ab0177f9211e628876f74369221fa66400de
Gregor von Laszewski <laszewski@gmail.com>
2014-09-12 17:32:57 -0400

    move ipython fabfiles to ipython.py

--- fabfile/india.py
+++ fabfile/india.py
@@ -13,26 +13,3 @@ def configure():
                       )
     ec2secgroup_openport('india', 5000)
     print "Configuration changes have been made successfully"
-
-@task
-def ipython():
-    IPython.lib import passwd
-    from fabfile.util import yaml_file_replace
-
-
-    local("ipython profile create nbserver")
-    local('cp etc/ipython_notebook_config.py ~/.ipython/profile_nbserver')
-    result = passwd()
-
-    
-    yaml_file_replace(filename='~/.ipython/profile_nbserver/ipython_notebook_config.py',
-                      replacements={'SHAPASSWORD': result}
-                      )
-    
-    local("openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out ~/.ipython/profile_nbserver/mycert.pem")
-    local("chmod go-rw ~/.ipython/profile_nbserver/mycert.pem")
-
-
-@task
-def start():
-    local("ipython notebook --certfile=~/.ipython/profile_nbserver/mycert.pem --profile=nbserver")
