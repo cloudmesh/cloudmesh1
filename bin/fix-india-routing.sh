dec506f8ec0422b17060d8bd773ef06ede47c1e7
Fugang Wang <kevinwangfg@gmail.com>
2014-09-16 16:00:54 -0400

    openstack keystone's internal ip

--- bin/fix-india-routing.sh
+++ bin/fix-india-routing.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo -e "\n# Added by Cloudmesh\n# Fixing india routing problem\n172.29.200.136\tindia\tindia.futuregrid.org" | sudo tee -a /etc/hosts
+echo -e "172.29.200.57\t\ti57r.idp.iu.futuregrid.org" | sudo tee -a /etc/hosts
 
