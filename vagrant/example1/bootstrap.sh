74a04e6e98385278e953947ca16e592f752e3ee2
Hyungro Lee <hroe.lee@gmail.com>
2014-09-16 20:05:34 -0400

    reflected latest installation to vagrant

--- vagrant/example1/bootstrap.sh
+++ vagrant/example1/bootstrap.sh
@@ -6,7 +6,8 @@
 apt-get update
 apt-get -y install git
 apt-get -y install python-pip
-pip install virtualenv
+#pip install virtualenv
+apt-get -y install python-virtualenv
 
 # Cloudmesh
 git clone https://github.com/cloudmesh/cloudmesh.git ~/cloudmesh/
@@ -28,18 +29,11 @@ ssh-keygen -b 2048 -t rsa -f ~/.ssh/cloudmesh-default -q -N ""
 ./install cloudmesh
 cm-iu user fetch --username=`cat /vagrant/.userid`
 cm-iu user create
-fab mongo.start
-# Waiting may not needed
-sleep 5
-#fab mongo.boot
-#fab mongo.boot
-sleep 5
-#fab user.mongo
-#fab mongo.simple
+fab mongo.boot
+fab user.mongo:cloudmesh
+fab mongo.simple
 # fab mongo.reset is same as fab mongo.boot, user.mongo, mongo.simple
-fab mongo.reset
+#fab mongo.reset
 fab server.start
-cm cloud on sierra
 cm cloud on india
-cm flavor sierra --refresh
 cm flavor india --refresh
