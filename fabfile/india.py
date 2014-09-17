92d8518dfa8792b7aae346a7a438321b7dd818fb
fugangwang <kevinwangfg@gmail.com>
2014-09-17 04:46:57 +0000

    changing to internal url mode when configuring VM on india

--- fabfile/india.py
+++ fabfile/india.py
@@ -1,15 +1,28 @@
 from fabric.api import task
 from util import ec2secgroup_openport, yaml_file_replace
-
+from cloudmesh_install import config_file
+from cloudmesh.config.cm_config import yaml_attribute_replace
 
 @task
 def configure():
     """configure india environment for cloudmesh rapid deployment"""
+    
+    # running on server mode with external port listening
     yaml_file_replace(filename='/cloudmesh_server.yaml',
                       replacements={
                                     'browser: True': 'browser: False',
                                     'host: 127.0.0.1': 'host: 0.0.0.0'
                                     }
                       )
-    ec2secgroup_openport('india', 5000)
+    # port 5000 needs to be open
+    # ec2secgroup_openport('india', 5000)
+    # now managed via nova before vm is started.
+    
+    # new way to replace an attribute in yaml
+    filename = config_file("/cloudmesh.yaml")
+    replacements = {
+        "cloudmesh.clouds.india.cm_service_url_type": "internalURL",
+    }
+    yaml_attribute_replace(filename,replacements)
+    
     print "Configuration changes have been made successfully"
