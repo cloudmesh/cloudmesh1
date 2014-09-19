16a558e24ff5ba5b3449bd04362553e29d2845ba
Aravindh Varadharaju <varadharaju.aravindh@gmail.com>
2014-09-19 16:09:19 -0400

    Fix for OSX install

--- cloudmesh_install/util.py
+++ cloudmesh_install/util.py
@@ -10,7 +10,7 @@ import platform
 
 from string import Template
 import os
-
+import sys
 
 def get_system():
     if is_ubuntu():
@@ -27,7 +27,7 @@ def is_ubuntu():
     """test sif the platform is ubuntu"""
     (dist, version, release) = platform.dist()
     if dist == "ubuntu" and version not in ["14.04"]:
-        log.error("ERROR: %s %s is not tested" % (dist, version))
+        print("ERROR: %s %s is not tested" % (dist, version))
     return dist == 'Ubuntu'
 
 
@@ -35,7 +35,7 @@ def is_centos():
     """test if the platform is centos"""
     (dist, version, release) = platform.dist()
     if dist == "centos" and version not in ["6.5"]:
-        log.error("WARNING: %s %s is not tested" % (dist, version))
+        print("WARNING: %s %s is not tested" % (dist, version))
     return dist == "centos"
 
 
@@ -43,10 +43,10 @@ def is_osx():
     osx = platform.system().lower() == 'darwin'
     if osx:
         os_version = platform.mac_ver()[0]
-        if os_version not in ['10.9.4']:
+        if os_version not in ['10.9.5']:
 
             osx = False
-            log.error("WARNING: %s %s is not tested" % ('OSX', os_version))
+            print("WARNING: %s %s is not tested" % ('OSX', os_version))
     return osx
 
 
