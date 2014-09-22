89fb1cec25cb0bacb7cd571eef3dedf224e1b7a3
Gregor von Laszewski <laszewski@gmail.com>
2014-09-21 22:51:48 -0400

    doing strings the python way

--- tests/test_cm_console_ext.py
+++ tests/test_cm_console_ext.py
@@ -9,12 +9,17 @@ import sys
 import os
 import cloudmesh
 import unittest
-
+import random
+        
 log = LOGGER(__file__)
 
 class Test(unittest.TestCase):
     vm = []
 
+    def setup(self):    
+        self.config = cloudmesh.load("user")
+        self.username = config.get("cloudmesh.hpc.username")
+    
     def test_01_activate_cloud(self):
         HEADING()
         os.system("cm cloud on india")
@@ -27,11 +32,17 @@ class Test(unittest.TestCase):
     @classmethod
     def test_03_start_a_vm(cls):
         HEADING()
-        import random
-        vm_name = "nosetests_"+str(random.randint(1,100))
-        cmd = "cm \"vm start --name={0} --cloud=india \
-                        --image=futuregrid/ubuntu-14.04 \
-                        --flavor=m1.small\"".format(vm_name)
+
+        config = cloudmesh.load("user")
+        username = config.get("cloudmesh.hpc.username")
+
+        vm_name = "{0}_{1}_{2}".format(username, "test",str(random.randint(1,100)))
+        cmd = ('cm "vm start'
+               ' --name={0}'
+               ' --cloud=india'
+               ' --image=futuregrid/ubuntu-14.04'
+               ' --flavor=m1.small"'.format(vm_name))
+
         print cmd
         os.system(cmd)
         cls.vm.append(vm_name)
@@ -39,37 +50,38 @@ class Test(unittest.TestCase):
     def test_04_validate_vm_running(self):
         HEADING()
         for vmname in self.vm:
-            res = os.popen("cm \"list vm india --refresh\"|grep " + \
-                           " {0} | wc -l".format(vmname)).read()
+            res = os.popen(
+                'cm "list vm india --refresh" '
+                ' | grep {0} | wc -l'.format(vmname)).read()
             assert res.strip() == "1"
 
     def test_05_delete_vms(self):
         HEADING()
         for vm_name in self.vm:
-            cmd = ("cm \"vm delete {0} --cloud=india --force\"".format(vm_name))
+            cmd = ('cm "vm delete {0} --cloud=india --force"'.format(vm_name))
             os.system(cmd)
     
     @classmethod
     def test_06_validate_vm_deleted(cls):
         HEADING()
         for vmname in cls.vm:
-            res = os.popen("cm \"list vm india --refresh\"|grep {0}|wc " + \
-                           " -l".format(vmname)).read()
+            res = os.popen('cm "list vm india --refresh" '
+                           '| grep {0}|wc -l'.format(vmname)).read()
             assert res.strip() == "0"
             cls.vm.remove(vmname)
 
     def test_07_default_flavor(self):
         HEADING()
-        os.system("cm \"cloud set flavor india --flavorid=2\"")
+        os.system('cm "cloud set flavor india --flavorid=2"')
 
     def test_08_validate_default_flavor(self):
         HEADING()
-        res = os.popen("cm cloud default|grep m1.small|wc -l").read()
+        res = os.popen("cm cloud default | grep m1.small | wc -l").read()
         assert res.strip() == "1"
 
     def test_09_default_image(self):
         HEADING()
-        os.system("cm \"cloud set image india --image=futuregrid/ubuntu-14.04\"")
+        os.system('cm "cloud set image india --image=futuregrid/ubuntu-14.04"')
 
     def test_10_validate_default_image(self):
         HEADING()
@@ -81,20 +93,20 @@ class Test(unittest.TestCase):
         HEADING()
         import random
         vm_name = "nosetests_"+str(random.randint(1,100))
-        os.system("cm \"vm start --name={0}\"".format(vm_name))
+        os.system('cm "vm start --name={0}"'.format(vm_name))
         cls.vm.append(vm_name)
 
     def test_12_validate_vm_running(self):
         HEADING()
         for vmname in self.vm:
-            res = os.popen("cm \"list vm india --refresh\"|grep " + \
-                           " {0} | wc -l".format(vmname)).read()
+            res = os.popen('cm "list vm india --refresh" '
+                           '| grep {0} | wc -l'.format(vmname)).read()
             assert res.strip() == "1"
 
     def test_13_delete_vms(self):
         HEADING()
         for vm_name in self.vm:
-            cmd = ("cm \"vm delete {0} --cloud=india --force\"".format(vm_name))
+            cmd = ('cm "vm delete {0} --cloud=india --force"'.format(vm_name))
             os.system(cmd)
             #cls.vm.remove(vm_name)
 
@@ -102,7 +114,7 @@ class Test(unittest.TestCase):
     def test_14_validate_vm_deleted(cls):
         HEADING()
         for vmname in cls.vm:
-            res = os.popen("cm \"list vm india --refresh\"|grep {0}|wc " + \
-                           " -l".format(vmname)).read()
+            res = os.popen('cm "list vm india --refresh" '
+                           ' | grep {0} | wc -l'.format(vmname)).read()
             assert res.strip() == "0"
             cls.vm.remove(vmname)
