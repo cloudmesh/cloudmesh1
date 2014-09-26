693b0cb09e42e42cfbf170fb23dbe8ec19f64826
Gregor von Laszewski <laszewski@gmail.com>
2014-09-26 15:09:16 -0400

    testcookbook

---
+++ etc/cookbooks/cm_test/README.md
@@ -0,0 +1,4 @@
+# cm_test
+
+TODO: Enter the cookbook description here.
+
---
+++ etc/cookbooks/cm_test/metadata.rb
@@ -0,0 +1,8 @@
+name             'cm_test'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures cm_test'
+long_description 'Installs/Configures cm_test'
+version          '0.1.0'
+
