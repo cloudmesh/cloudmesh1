e526aed7dbe8d2c678c4544e2fd5883a111db7e3
Hyungro Lee <hroe.lee@gmail.com>
2014-09-25 22:48:35 -0400

    cleaning

--- cloudmesh/cm_mongo.py
+++ cloudmesh/cm_mongo.py
@@ -707,14 +707,9 @@ class cm_mongo:
                                 cm_user_id)
 
         # increase index after the completion of vm_create()
-        #<<<<<<< HEAD
-        #self.cm_user.set_default_attribute(cm_user_id, "index", int(index) + 1)
-        # self.vmname_next()
-        #=======
         next_index = int(index) + 1
         self.cm_user.set_default_attribute(cm_user_id, "index", next_index)
         self.userinfo['defaults']['index'] = str(next_index)
-        #>>>>>>> b086124cc66673eefee5716cb9a748cbcbac437d
         #
         # BUG flavor name needs to be returned
         #
