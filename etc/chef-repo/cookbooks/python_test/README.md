f881a812a5f422cb7c699d788390a1979acc7324
Gregor von Laszewski <laszewski@gmail.com>
2014-09-27 06:56:26 -0400

    add the new exmples

---
+++ etc/chef-repo/cookbooks/python_test/README.md
@@ -0,0 +1,4 @@
+# python_test
+
+TODO: Enter the cookbook description here.
+
---
+++ etc/chef-repo/cookbooks/python_test/metadata.rb
@@ -0,0 +1,8 @@
+name             'python_test'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures python_test'
+long_description 'Installs/Configures python_test'
+version          '0.1.0'
+
