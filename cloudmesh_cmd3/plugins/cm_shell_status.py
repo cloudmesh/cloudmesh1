d83a60f8951311c3aafe5199d7b2c25219dff1db
Mark Yukai Xiao <mustxyk@gmail.com>
2014-10-03 15:55:13 -0400

    update command status

--- cloudmesh_cmd3/plugins/cm_shell_status.py
+++ cloudmesh_cmd3/plugins/cm_shell_status.py
@@ -3,6 +3,8 @@ from cloudmesh_common.logger import LOGGER
 from pprint import pprint
 from cloudmesh.cm_mongo import cm_mongo_status
 from cloudmesh_common.tables import two_column_table
+import json
+from subprocess import call
 
 log = LOGGER(__file__)
 
@@ -15,16 +17,22 @@ class cm_shell_status:
     def do_status(self, args, arguments):
         """
         Usage:
-            status mongo [--format=FORMAT]
-            status celery [--format=FORMAT] [-s|--simple]
+            status mongo 
+            status celery 
+            status rabbitmq
 
             Shows system status
         """
+        # TODO: temp design
+        
         if arguments['mongo']:
             stat = cm_mongo_status()
-            func = getattr(self, "_print_" + str(arguments['--format']))
-            func(stat.serverStatus())
+            pprint(stat.serverStatus())
+            #func = getattr(self, "_print_" + str(arguments['--format']))
+            #func(stat.serverStatus())
         elif arguments['celery']:
+            call(['celery','inspect','ping'])
+            '''
             import celery
             if arguments['--simple'] or arguments['-s']:
                 res = celery.current_app.control.inspect().ping()
@@ -32,8 +40,11 @@ class cm_shell_status:
                 res = celery.current_app.control.inspect().stats()
             func = getattr(self, "_print_" + str(arguments['--format']))
             func(res)
+            '''
+        elif arguments['rabbitmq']:
+            call(['sudo','rabbitmqctl','status'])
         
-
+    '''
     def _print_None(self, data):
         self._print_table(data)
 
@@ -42,3 +53,4 @@ class cm_shell_status:
       
     def _print_table(self, data):
         print two_column_table(data)
+    '''
