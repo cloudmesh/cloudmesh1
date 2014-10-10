943cbe5e60493751e7ef894b9197941fc2054c11
Gregor von Laszewski <laszewski@gmail.com>
2014-10-10 10:05:27 -0400

    adding cm-admin to deployed commands

--- cloudmesh_admin/admin.py
+++ cloudmesh_admin/admin.py
@@ -1,26 +1,34 @@
-"""cm-admin.
-
-Usage:
-  cm-admin server start
-  cm-admin server stop
-  cm-admin server status
-  cm-admin version
-  
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
- 
-"""
+#!/usr/bin/env python
 
 from fabfile.server import start, stop, kill
 from fabfile.queue import ls as queue_ls
 from fabfile.mongo import info as mongo_info
+from fabfile.user import mongo as set_mongo_password
 
 from docopt import docopt
 import cloudmesh
+import sys
 
-if __name__ == '__main__':
-    arguments = docopt(__doc__, version=cloudmesh.__version__)
+def main():
+    admin_command(sys.argv)
+        
+def admin_command(args):
+    """cm-admin.
+
+    Usage:
+      cm-admin server start
+      cm-admin server stop
+      cm-admin server status
+      cm-admin mongo password
+      cm-admin version
+
+    Options:
+      -h --help     Show this screen.
+      --version     Show version.
+
+    """
+    print args
+    arguments = docopt(admin_command.__doc__, args[1:])
     print(arguments)
 
     if arguments['version']:
@@ -35,3 +43,9 @@ if __name__ == '__main__':
         print "status"
         queue_ls()
         mongo_info()
+    elif arguments['mongo'] and arguments['password']:
+        set_mongo_password()
+
+
+if __name__ == '__main__':
+    admin_command(sys.argv)
