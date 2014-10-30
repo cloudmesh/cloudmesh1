28209d02b7c5a67e31996a99c15c114ff393f879
Hyungro Lee <hroe.lee@gmail.com>
2014-10-30 11:38:58 -0400

    usage updated

--- cloudmesh_cmd3/plugins/cm_shell_usage.py
+++ cloudmesh_cmd3/plugins/cm_shell_usage.py
@@ -62,6 +62,19 @@ class cm_shell_usage:
             def_cloud = self.get_cloud_name(userid)
             self.cm_mongo.activate(userid)
             usage = self.cm_mongo.usage(def_cloud, userid)
+            # server usages need to be supressed.
+            # e.g. {u'hours': 24.00000006388889, u'uptime': 1960234,
+            # u'started_at': u'2014-10-07T23:03:57.000000', u'ended_at': None,
+            # u'name': u'hrlee-server-2zuvke4wujud', u'tenant_id':
+            # u'3e6eaf1d913a48f694a7bc0fbb027507', u'instance_id':
+            # u'2c9d24e0-7453-4f83-84b7-f8c0254a574f', u'state':
+            # u'active', u'memory_mb': 2048, u'vcpus': 1, u'flavor':
+            # u'm1.small', u'local_gb': 20} 
+            try:
+                usage['server_usages'] = str(len(usage['server_usages'])) + " vms"
+            except:
+                pass
+
             print(row_table(usage, order=None, labels=["Variable", "Value"]))
 
             return usage
