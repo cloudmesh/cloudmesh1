5f2fed0a8421f4d8bc318bae9dda9bb32061369c
Hyungro Lee <hroe.lee@gmail.com>
2014-11-05 15:27:34 -0500

    updated with fab security.check. openstack, aws checked

--- fabfile/security.py
+++ fabfile/security.py
@@ -4,14 +4,25 @@ import os
 
 @task
 def check(search=""):
+    check_list = [ ("openstack", "'OS_PASSWORD': '[a-zA-Z0-9]+'"),
+                  ("aws", "'EC2_SECRET_KEY': '[a-zA-Z0-9]+'") ]
+    for pair in check_list:
+        platform = pair[0]
+        search = pair[1]
+        _grep(search, platform)
+
+def _grep(search, platform):
     if not search:
-        search = "OS_PASSWORD': '[a-zA-Z0-9]+'"
+        search = "'OS_PASSWORD': '[a-zA-Z0-9]+'"
     cmd = "egrep -ri \"{0}\" * | cut -d\":\" -f1".format(search)
-    print(cmd)
+    print("[{0}]:{1}".format(platform, cmd))
     res = local(cmd, capture=True)
     if res:
-        print ("ERROR: OS_PASSWORD DETECTED, SEE FILES BELOW")
+        print ('[{0}]: [ERROR] PASSWORD(OR SECRET KEY) DETECTED, SEE FILES '
+               'BELOW'.format(platform))
         print ("")
         print (res)
     else:
-        print ("NO PASSWORD DETECTED")
+        print ("[{0}]: NO PASSWORD DETECTED".format(platform))
+
+    print ("")
