12cb97ab1dad6fc98c0db5939cba4524efccabd2
Gregor von Laszewski <laszewski@gmail.com>
2014-11-10 15:58:15 -0500

    added more experiment stuff

--- cloudmesh/shell/cm_experiment_group.py
+++ cloudmesh/shell/cm_experiment_group.py
@@ -11,11 +11,12 @@ def shell_command_experiment_group(arguments):
     """
     Usage:
         group info
-        group list [NAME]
+        group list [NAME] [ATTRIBUTES] [--foramt=TABLEFORMAT]
         group set NAME
-        group add NAME
+        group create NAME
         group [-i] delete NAME
-
+        group add [--name=NAME] KIND LABEL         
+        
     Arguments:
 
         NAME   the name of the group
@@ -28,6 +29,64 @@ def shell_command_experiment_group(arguments):
 
        group NAME  lists in formation about the group
 
+    Example:
+
+       group create experiment_1
+       vm start
+       last = vm label
+       group add experiment_1 vm last
+
+       group create experiment_2
+       vm start
+       last = vm info label  # prints the vm label /prefix + number
+       ipno = vm info ip # prints the ip of the last vm
+       ipno = vm info ip gvonlasz_1  # get ip of vm with label gvonlasz_1
+
+       group add expermiment_2 ip ipno
+
+       groups are just tuples
+
+       i can have multiple Kinds in the tuple
+
+    mongoengine
+
+    class groupObject
+
+        def add (... name, kind, attribute ...)
+        def printer ( ... kind, printfunction, name...)
+        def getter ( .... kind, name)
+        
+    def getter ( .... kind, name ...)
+
+       if kind == "vm":
+          vm = get vm from mongo
+          return vm
+       elif kind = "image"
+          iamge = get image from mongo
+          return iamge
+       ....
+
+    def vmPrinter ( .... vm ...)
+
+       print vm.ip
+       print vm.name
+       ....
+
+    def imagePrinter ( .... image ...)
+
+       print image.size
+       print image.name
+       ....
+
+       
+
+    g = groupObject()
+    g.printer("vm", cmPrinter)
+    g.printer("image", imagePrinter)
+
+    
+       
+       
     """
 
     name = arguments["NAME"]
