0299ec40746fbd9d42b234bfa63485e17fad7c7f
Gregor von Laszewski <laszewski@gmail.com>
2015-03-20 06:42:00 -0400

    add requirements to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,145 @@
 #!/usr/bin/env python
+requirements = """
+    # ######################################################################
+    # Basic environment
+    # ######################################################################
+    #
+    future
+    pip
+    #
+    # ######################################################################
+    # Sphinx
+    # ######################################################################
+    #
+    sphinx
+    sphinx_bootstrap_theme
+    sphinxcontrib-autorun
+    sphinxcontrib-blockdiag
+    sphinxcontrib-exceltable
+    sphinxcontrib-webmocks
+    actdiag
+    blockdiag
+    blockdiagcontrib-square
+    blockdiagcontrib-qb
+    blockdiagcontrib-class
+    blockdiagcontrib-cisco
+    nwdiag
+    seqdiag
+    sphinxcontrib-httpdomain 
+    #
+    # ######################################################################
+    # Packages which must be installed first
+    # ######################################################################
+    #
+    paramiko
+    pycrypto
+    fabric
+    #
+    # ######################################################################
+    # Console and commandline Related
+    # ######################################################################
+    #
+    blessings
+    progress
+    sh
+    console
+    python-hostlist
+    docopt
+    cmd3>=1.1.0
+    #
+    # ######################################################################
+    # Databases and directories
+    # ######################################################################
+    #
+    python-ldap
+    pyaml
+    pyyaml
+    pymongo
+    mongoengine
+    #
+    # ######################################################################
+    # Rabbitmq
+    # ######################################################################
+    #
+    librabbitmq
+    #
+    # ######################################################################
+    # Flask
+    # ######################################################################
+    #
+    WTForms
+    Flask>=0.10.1
+    Flask-WTF>=0.9.3
+    Flask-AutoIndex
+    Flask-Silk
+    flask_login
+    Flask-OpenID
+    flask-principal
+    Flask-RSTPages
+    flask-restful
+    flask-restful-swagger
+    #
+    # ######################################################################
+    # Testing and formatting
+    # ######################################################################
+    #
+    nose
+    autopep8
+    #
+    # ######################################################################
+    # OpenStack and cloud
+    # ######################################################################
+    #
+    apache-libcloud==0.14.1
+    python-novaclient
+    python-keystoneclient
+    azure
+    #
+    # ######################################################################
+    # Parallel tools
+    # ######################################################################
+    #
+    celery
+    flower
+    psutil
+    #
+    # ######################################################################
+    # General Libraries
+    # ######################################################################
+    #
+    simple-json
+    pytimeparse
+    timestring
+    prettytable
+    tabulate
+    passlib
+    # ######################################################################
+    # ipython
+    # ######################################################################
+    #
+    ipython
+    pyzmq
+    tornado
+    # ######################################################################
+    # General Libraries
+    # ######################################################################
+    #
+    fake-factory
+    ecdsa
+    #
+"""
+
+def auto_install_requirements_from_string(requirements):
+    lines = requirements.split("\n")
+
+    r = []
+    for line in lines:
+        line = line.strip()
+        if not line.startswith("#") and line != '':
+            r.append(line)
+    return r
+
+requirements = auto_install_requirements_from_string(requirements)
 
 #from distutils.core import setup
 
@@ -84,4 +225,5 @@ setup(
     cmdclass={
         'custom': InstallTest,
         },
+    install_requires=requirements,
 )
