6155685f4af0c1c3be63ed0a43c30762695f787d
Gregor von Laszewski <laszewski@gmail.com>
2015-03-26 09:09:03 -0400

    add the cert

--- setup.py
+++ setup.py
@@ -98,6 +98,7 @@ setup(
         (home + '/.cloudmesh', [
             'etc/FGLdapCacert.pem',
             'etc/india-havana-cacert.pem',
+            'etc/india-juno-cacert.pem',
             'etc/cloudmesh_flavor.yaml']),
         (home + '/.cloudmesh/etc', [
             'etc/cloudmesh.yaml',
