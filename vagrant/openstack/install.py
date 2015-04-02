94daadcb0ce56977f26cac26b0f796f5c7c9e46a
Gregor von Laszewski <laszewski@gmail.com>
2015-04-02 12:39:30 -0400

    add configdict read

--- vagrant/openstack/install.py
+++ vagrant/openstack/install.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
+
+from cloudmesh_base.ConfigDict import ConfigDict
+
 os.system("vagrant plugin install vagrant-openstack-provider")
+
+
+config = ConfigDict()
+
+print config
